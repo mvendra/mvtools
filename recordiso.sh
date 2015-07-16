6f65f9918f586b8b6f7fdc5da7ca62afb1c56a55
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-16 13:28:36 -0300

    morpinh recordiso.txt into a proper script

--- recordiso.sh
+++ recordiso.sh
@@ -1,1 +1,21 @@
-cdrecord -v -dao dev=/dev/sr0 $1
+#!/bin/bash
+
+puaq(){ # puaq stands for Print Usage And Quit
+  echo "Usage: `basename $0` isoimage.iso"
+  exit 1
+}
+
+if [ -z $1 ]; then
+  puaq
+fi
+
+DEVICEFILE=/dev/sr0 # mvtodo: detect the system's device file (create another script for that!)
+IMAGEFILE=$1
+
+if [ ! -f $IMAGEFILE ]; then
+  echo "$IMAGEFILE does not exist. Aborted."
+  exit 2
+fi
+
+cdrecord -v -dao dev=$DEVICEFILE $IMAGEFILE
+
