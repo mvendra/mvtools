b7374f2993b5504cb29f55cf5268b3ebf0f5a8a7
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-17 12:48:02 -0300

    halfway impl of second req. automated mousefix

--- mousefixes/mousefix5.sh
+++ mousefixes/mousefix5.sh
@@ -6,9 +6,10 @@ xset m 0 0
 DEVICE_ID=("`xinput --list | grep -i mouse | parseid.py`")
 
 # xinput --list-props DEVICE_ID (and find Device Accel Profile)
-DEVICE_ACCEL_PROFILE=265
+DEVICE_ACCEL_PROFILE=("`xinput --list-props $DEVICE_ID | grep -i \"device accel profile\" | parsedap.py`")
 
 echo "mvtodo: automate DEVICE_ACCEL_PROFILE detection: $DEVICE_ACCEL_PROFILE $DEVICE"
+exit 1
 
 xinput set-prop $DEVICE_ID $DEVICE_ACCEL_PROFILE -1
 
