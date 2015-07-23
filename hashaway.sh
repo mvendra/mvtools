aea9c730da4ff0ec527400a1b86d295635966207
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-23 10:40:39 -0300

    introducing hash check and gen scripts

---
+++ hashaway.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+puaq(){ # puaq stands for Print Usage And Quit
+  echo "Usage: `basename $0` archive-to-hash"
+  exit 1
+}
+
+if [ -z $1 ]; then
+  puaq
+fi
+
+if [ ! -f $1 ]; then
+  echo "$1 does not exist."
+  exit 2
+fi
+
+HASHFNAME=$1.sha256
+
+if [ -f $HASHFNAME ]; then
+  echo "$HASHFNAME exists. Refusing to overwrite."
+  exit 3
+fi
+
+sha256sum $1 > $HASHFNAME
+
---
+++ hasheck.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+puaq(){ # puaq stands for Print Usage And Quit
+  echo "Usage: `basename $0` archive-to-check"
+  exit 1
+}
+
+if [ -z $1 ]; then
+  puaq
+fi
+
+if [ ! -f $1 ]; then
+  echo "$1 does not exist."
+  exit 2
+fi
+
+HASHFNAME=$1.sha256
+
+if [ ! -f $HASHFNAME ]; then
+  echo "ERROR: $HASHFNAME is not present. Aborting"
+  exit 3
+fi
+
+TMPFNAME=(`randomfilename.sh`)
+TMPFNAME=${TMPFNAME}.tmpfile
+
+if [ -f $TMPFNAME ]; then
+  echo "ERROR: Temporary filename $TMPFNAME conflict! Aborting."
+  exit 4
+fi
+
+sha256sum $1 > $TMPFNAME
+diff $HASHFNAME $TMPFNAME 
+
+if [ $? -eq 0 ]; then
+  echo "Match!"
+else
+  echo "No match"
+fi
+
+if [ ! -z $TMPFNAME ]; then
+  rm $TMPFNAME
+fi
+
