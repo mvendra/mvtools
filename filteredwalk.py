ce3f3d8a211d2d70ca89b4acbea8b6159d3ae7a9
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-24 15:12:59 -0300

    adding non recursive option

--- filteredwalk.py
+++ filteredwalk.py
@@ -3,12 +3,13 @@
 import sys
 import os
 
-def makefilelist(path, exts):
+def makefilelist(path, exts, subs):
 
   """
   Makes a file list by walking the provided path
   path = a path string
   exts = a list of extensions to consider, without the dot
+  subs = a boolean, whether it should include subfolders (recursively)
   returns a dictionary with extensions as keys, and each dict entry is a list of filenames with full path
   """
 
@@ -17,6 +18,9 @@ def makefilelist(path, exts):
     ret_lists[x] = []
 
   for dirpath, dirnames, filenames in os.walk(path):
+    if not subs and dirpath != ".":
+        break
+
     for f in filenames:
       for x in exts:
         if f.endswith(x):
@@ -26,9 +30,19 @@ def makefilelist(path, exts):
 
 if __name__ == "__main__":
   if len(sys.argv) < 3:
-    print("Usage: %s path extensions " % os.path.basename(__file__))
+    print("Usage: %s [-R] path extensions" % os.path.basename(__file__))
     exit(1)
-  ret = makefilelist(sys.argv[1], sys.argv[2:])
+
+  inc_sub = False
+  if '-R' in sys.argv or '-r' in sys.argv:
+    inc_sub = True
+    path = sys.argv[2]
+    exts = sys.argv[3:]
+  else:
+    path = sys.argv[1]
+    exts = sys.argv[2:]
+
+  ret = makefilelist(path, exts, inc_sub)
   for k in ret:
     for r in ret[k]:
       print(r)
