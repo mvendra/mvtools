7bf4beac438a510436eb09db06137d5d08a19b28
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-24 17:32:36 -0300

    started env snaity check tool

--- env_sanity_check.py
+++ env_sanity_check.py
@@ -5,6 +5,8 @@
 import os
 
 if __name__ == "__main__":
-    for i in os.environ:
-        print(os.environ[i])
+  for i in os.environ.keys():
+    # mvtodo: check duplicate paths
+    # mvtodo: check whether envvar pointing to paths are actually valid paths (path starts with / at least. must have two
+    #print(os.environ[i])
 
