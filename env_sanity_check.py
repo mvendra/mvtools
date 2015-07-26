34bd3b3335d5b273b6153c59db54101ddf35827c
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-26 14:44:01 -0300

    introducing env sanity check tool

--- env_sanity_check.py
+++ env_sanity_check.py
@@ -4,10 +4,33 @@
 
 import os
 
+#VARS_EXCEPTIONS = ["MANDATORY_PATH", "GPG_AGENT_INFO", "XDG_CONFIG_DIRS", "DEFAULTS_PATH", "XDG_DATA_DIRS"] # my mint 17 had these undefined in mid-2015 (mv)
+VARS_EXCEPTIONS = []
+
+def is_sane(var, val):
+
+    if var in VARS_EXCEPTIONS:
+        return True
+
+    if val == "":
+        return True
+
+    if var == "PATH": # special case
+        paths_in_path = val.split(":")
+        for p in paths_in_path:
+            if not os.path.exists(p):
+                print("%s inside PATH does not exist")
+                return False
+
+    elif val[0] == '/': # generic case
+        return os.path.exists(val)
+
+    return True
+
 if __name__ == "__main__":
     for i in os.environ.keys():
-        print(i)
-    # mvtodo: check duplicate paths
-    # mvtodo: check whether envvar pointing to paths are actually valid paths (path starts with / at least. must have two
-    #print(os.environ[i])
+        thevar = i
+        theval = os.environ[i]
+        if not is_sane(thevar, theval):
+            print("WARNING: %s has a suspicious value" % thevar)
 
