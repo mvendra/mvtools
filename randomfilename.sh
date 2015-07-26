b4c8ae6d08bfdb94f18a6277ffab45843d68d76c
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-26 20:10:59 -0300

    adding some extra defense to randomfilename generator

--- randomfilename.sh
+++ randomfilename.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # first method
-date +%s | sha256sum | base64 | head -c 32 ; echo
+RFN=(`date +%s | sha256sum | base64 | head -c 48`)
+RFN=${RFN}.tmp
+
+if [ -e $RFN ]; then
+  echo "Temporary file $RFN already exists! Race hazard is abound. Aborting."
+  exit 1
+fi
+
+echo $RFN
 
 # second method
 #cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32
