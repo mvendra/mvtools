f61a2d68a1f26e61aa8fa40fb31b31b307c15ed3
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-26 17:42:12 -0300

    forgot to get rid of a print

--- git/meldiff.py
+++ git/meldiff.py
@@ -7,6 +7,5 @@ if __name__ == "__main__":
     if (len(sys.argv) < 8):
         print("This script is not intended for standalone use - integrate with meld instead.")
         sys.exit(1)
-    print(sys.argv)
     os.system("meld %s %s" % (sys.argv[2], sys.argv[5]))
 
