6be4006017662036f9c422fa9313e60d1d7a1039
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-26 18:51:42 -0300

    new boot script for cpp

---
+++ cppboot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CPPBOOTFILE=testforecho.cpp
+if [ ! -z $1 ]; then
+  CPPBOOTFILE=$1
+fi
+
+if [ -f ./$CPPBOOTFILE ]; then
+  echo "There's already a ./$CPPBOOTFILE in the CWD (`pwd -P`), so this script is aborted."
+  exit 1
+fi
+
+touch ./$CPPBOOTFILE
+echo "\n#include <iostream>\n\nint main(int argc, char *argv[]){\n\n    std::cout << \"test for echo\" << std::endl;\n\n    return 0;\n\n}" > ./$CPPBOOTFILE
+
