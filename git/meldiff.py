483e45f78adf355f081b07bc5d3b91f86d0f2d94
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-26 17:41:24 -0300

    improving tact of meldiff helper tool

--- git/meldiff.py
+++ git/meldiff.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
+
 import sys
 import os
 
-os.system("meld %s %s" % (sys.argv[2], sys.argv[5]))
+if __name__ == "__main__":
+    if (len(sys.argv) < 8):
+        print("This script is not intended for standalone use - integrate with meld instead.")
+        sys.exit(1)
+    print(sys.argv)
+    os.system("meld %s %s" % (sys.argv[2], sys.argv[5]))
+
