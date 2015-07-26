84a5f6b29f862a8a6c6d4bac3744132727571ee1
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-26 14:07:20 -0300

    first version of clementine playlist base path rebasing tool is done

--- audio/clementine_playlist_rebase.py
+++ audio/clementine_playlist_rebase.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 
-if __name__ == "__main__":
-  print("elo le monde!")
+import sys
+import os
+from subprocess import call
 
+def puaq(): # print usage and quit
+    print("Usage: %s path_with_playlists string_to_find string_to_replace_with" % os.path.basename(__file__))
+    sys.exit(1)
+
+def proc(plfile, str_find, str_rep):
+
+    str_find = str_find.replace("/", "\\/")
+    str_rep = str_rep.replace("/", "\\/")
+    sed_opts = "s/%s/%s/g" % (str_find, str_rep)
+    the_cmd = ["sed", "-i", sed_opts, plfile]
+
+    call(the_cmd)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        puaq()
+
+    path_playlists = os.path.abspath(sys.argv[1])
+    str_find = sys.argv[2]
+    str_rep = sys.argv[3]
+    
+    playlists = os.listdir(path_playlists)
+    for p in playlists:
+        proc(os.path.join(path_playlists, p), str_find, str_rep)
+ 
