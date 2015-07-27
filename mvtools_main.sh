63e9b233ef9175115ccbb94ebd48eaf58173f6e4
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-27 19:55:49 -0300

    important improvement for mvtools-main: now also adding tools to the PYTHONPATH, so scripts can be imported everywhere. nice.

--- mvtools_main.sh
+++ mvtools_main.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 add2path(){
-  PATH=${PATH}:${1}
+  if [ -z $PATH ]; then
+    # first use. dont add the comma at the beginning
+    PATH=${1}
+  else
+    PATH=${PATH}:${1}
+  fi
+}
+
+add2pythonpath(){
+  if [ -z $PYTHONPATH ]; then
+    # first use. dont add the comma at the beginning
+    export PYTHONPATH=${1}
+  else
+    export PYTHONPATH=${PYTHONPATH}:${1}
+  fi
 }
 
 # this requires the MVTOOLS envvar to be defined somewhere else (.bashrc, likely).
@@ -9,12 +23,24 @@ add2path(){
 if [[ ! -z $MVTOOLS && -d $MVTOOLS ]]; then
 
   add2path $MVTOOLS
+  add2pythonpath $MVTOOLS
+
   add2path $MVTOOLS/audio
+  add2pythonpath $MVTOOLS/audio
+
   add2path $MVTOOLS/git
+  add2pythonpath $MVTOOLS/git
+
   add2path $MVTOOLS/inputfixes
-  add2path $MVTOOLS/startservices
+  add2pythonpath $MVTOOLS/inputfixes
+
   add2path $MVTOOLS/codegen
+  add2pythonpath $MVTOOLS/codegen
+
   add2path $MVTOOLS/security
+  add2pythonpath $MVTOOLS/security
+
+  add2path $MVTOOLS/startservices
 
   # for a nuclear-recursive solution, use this:
   #PATH=${PATH}:$(find ~/the_base_path -type d | tr '\n' ':' | sed 's/:$//')
