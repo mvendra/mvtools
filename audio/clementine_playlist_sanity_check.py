bdc306977e2359bce63bfa8adc9d9fe17e743d39
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-28 19:58:45 -0300

    implemented the crux of the clementine playlist sanity check tool

--- audio/clementine_playlist_sanity_check.py
+++ audio/clementine_playlist_sanity_check.py
@@ -8,8 +8,43 @@ def puaq(): # print usage and quit
     print("Usage: %s path_with_playlists" % os.path.basename(__file__))
     sys.exit(1)
 
+def getcontents(thefile):
+    contents = ""
+    with open(thefile, "r") as f:
+        contents = f.read()
+    return contents
+
+def convert_clementine_playlist_to_python_list(filename):
+
+    contents = getcontents(filename)
+    if len(contents) == 0:
+        print("%s is empty! Aborting." % filename)
+        sys.exit(1)
+
+    tracks = []
+
+    ls = 0
+    le = 0
+    while True:
+        ls = contents.find("<location>", ls)
+        if ls == -1:
+            # we are done
+            break
+
+        le = contents.find("</location>", ls+1)
+        if le == -1:
+            print("Malformatted playlist contents detected. Check starting at offset %s, playlist %s." % (filename, ls))
+
+        location = contents[ls+len("<location>"):le]
+        tracks.append(location)
+
+        ls = le # advance to the next location
+
+    return tracks
+
 def proc(plfile):
-    pass
+    tracks = convert_clementine_playlist_to_python_list(plfile)
+    print(tracks)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
