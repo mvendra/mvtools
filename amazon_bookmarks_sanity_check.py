cfdf16f62c989e67b9b29cef409d941e563335e8
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-29 19:31:37 -0300

    adding TODOs

--- amazon_bookmarks_sanity_check.py
+++ amazon_bookmarks_sanity_check.py
@@ -4,6 +4,9 @@ import urllib
 
 if __name__ == "__main__":
     cont = urllib.urlopen("http://www.amazon.com/Templates-Complete-Guide-David-Vandevoorde/dp/0201734842/ref=sr_1_1?ie=UTF8&qid=1438170638&sr=8-1&keywords=c%2B%2B+templates")
+    # mvtodo: extract all links from a bookmarks dump
+    # mvtodo: extract isbn13 from each link
+    # mvtodo: iterate and compare all the isbn13's looking for dupes
 
     #print(cont)
 
