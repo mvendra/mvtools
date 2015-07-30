5c1dfd712e201d4ad9190177736f3cfb63651462
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-30 16:14:02 -0300

    implementing basics of gvsta

--- git/git-visitor-status.py
+++ git/git-visitor-status.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python
 
+import sys
+import os
+import fsquery
+
+def puaq():
+    print("Usage: %s base_path." % os.path.basename(__file))
+    sys.exit(1)
+
+def filter_git_only(thelist):
+    ret = []
+    for d in thelist:
+        if os.path.basename(d) == ".git":
+            ret.append(d)
+    return ret
+
+def run_visitor_status(list_repos):
+    print(list_repos)
+
 if __name__ == "__main__":
-    print("elo")
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    basepath = sys.argv[1]
+    if not os.path.exists(basepath):
+        print("%s does not exist. Aborting." % basepath)
+        sys.exit(1)
+
+    ret = fsquery.makecontentlist(basepath, True, False, True, False, True, [])
+    ret = filter_git_only(ret)
+    run_visitor_status(ret)
 
