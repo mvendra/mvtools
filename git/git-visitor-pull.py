d4938889e203e6fccdd7821ff1df45ffda0ca603
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-30 15:43:24 -0300

    improved git visitor ideas alittle

--- git/git-visitor-pull.py
+++ git/git-visitor-pull.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# mvtodo: recursively check status of repos and report back
-
 if __name__ == "__main__":
     print("elo")
 
--- git/gvpus.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-# mvtodo: recursively check repos and issues a push command, but without force. report back on the failed ones.
-
-if __name__ == "__main__":
-  print("elo")
-
