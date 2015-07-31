1a62ee05f1717306129af37eef637e717fce8d10
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-07-31 11:46:53 -0300

    adding prototypes for git_visitor_pull and git_visitor_push

--- git/git_visitor_push.py
+++ git/git_visitor_push.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+import sys
+import os
+import git_visitor_base
+from subprocess import check_output
+
+def puaq():
+    print("Usage: %s base_path." % os.path.basename(__file__))
+    sys.exit(1)
+
+def run_visitor_push(list_repos):
+    for r in list_repos:
+        base_repo_path = os.path.dirname(r)
+        repo_git_dir = r
+        out = check_output(["git", "--git-dir=%s" % repo_git_dir, "--work-tree=%s" % base_repo_path, "push"])
+
 if __name__ == "__main__":
-    print("elo")
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    basepath = sys.argv[1]
+    if not os.path.exists(basepath):
+        print("%s does not exist. Aborting." % basepath)
+        sys.exit(1)
+
+    repos = git_visitor_base.make_repo_list(basepath)
+    run_visitor_push(repos)
 
