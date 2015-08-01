c90fe18d2c0039934ca347ce24b8c1fe507f1a29
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-01 18:29:03 -0300

    factored out some code from visitor base to new repo query tool

---
+++ git/git_repo_query.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from subprocess import check_output
+
+def puaq():
+    print("Usage: %s repo_path (.git folder included)." % os.path.basename(__file__))
+    sys.exit(1)
+
+def get_remotes(repo):
+
+    """ get_remotes
+    returns a list with a repo's remotes.
+    """
+
+    if not os.path.exists(repo):
+        print("%s does not exist." % repo)
+        return None
+    if not (repo.endswith(".git") or repo.endswith(".git" + os.sep)):
+        print("%s does not point to a .git repository." % repo)
+        return None
+
+    out = check_output(["git", "--git-dir=%s" % repo, "--work-tree=%s" % os.path.dirname(repo), "remote"])
+    ret_list = out.split()
+    if len(ret_list) > 0:
+        return ret_list
+    else:
+        return None
+
+def get_branches(repo):
+
+    """ get branches
+    returns a list with a repo's branches. the first item in the list is the checked out branch
+    """
+
+    if not os.path.exists(repo):
+        print("%s does not exist." % repo)
+        return None
+    if not (repo.endswith(".git") or repo.endswith(".git" + os.sep)):
+        print("%s does not point to a .git repository." % repo)
+        return None
+
+    out = check_output(["git", "--git-dir=%s" % repo, "--work-tree=%s" % os.path.dirname(repo), "branch"])
+    branch_list = out.split("\n")
+
+    # move the checked out branch to the front
+    for i in branch_list:
+        if i.startswith("*"):
+            branch_list.remove(i)
+            branch_list = [i[2:]] + branch_list
+            break;
+
+    # remove blank spaces
+    if "" in branch_list:
+        branch_list.remove("")
+
+    # trim branch strings
+    ret_list = []
+    for i in branch_list:
+        ret_list.append(i.strip())
+
+    if len(ret_list) > 0:
+        return ret_list
+    else:
+        return None
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    repopath = sys.argv[1]
+    if not os.path.exists(repopath):
+        print("%s does not exist. Aborting." % repopath)
+        sys.exit(1)
+
+    print("branches: %s" % get_branches(repopath))
+    print("remotes: %s" % get_remotes(repopath))
+
--- git/git_visitor_base.py
+++ git/git_visitor_base.py
@@ -3,7 +3,6 @@
 import sys
 import os
 import fsquery
-from subprocess import check_output
 
 def puaq():
     print("Usage: %s base_path." % os.path.basename(__file__))
@@ -12,7 +11,7 @@ def puaq():
 def __filter_git_only(thelist):
     ret = []
     for d in thelist:
-        if os.path.basename(d) == ".git":
+        if d.endswith(".git") or d.endswith(".git" + os.sep):
             ret.append(d)
     return ret
 
@@ -24,49 +23,6 @@ def make_repo_list(path):
 
     ret_list = fsquery.makecontentlist(path, True, False, True, False, True, [])
     ret_list = __filter_git_only(ret_list)
-    if len(ret_list) > 0:
-        return ret_list
-    else:
-        return None
-
-def get_remotes(repo):
-
-    """ get_remotes
-    returns a list with a repo's remotes.
-    """
-
-    out = check_output(["git", "--git-dir=%s" % repo, "--work-tree=%s" % os.path.dirname(repo), "remote"])
-    ret_list = out.split()
-    if len(ret_list) > 0:
-        return ret_list
-    else:
-        return None
-
-def get_branches(repo):
-
-    """ get branches
-    returns a list with a repo's branches. the first item in the list is the checked out branch
-    """
-
-    out = check_output(["git", "--git-dir=%s" % repo, "--work-tree=%s" % os.path.dirname(repo), "branch"])
-    branch_list = out.split("\n")
-
-    # move the checked out branch to the front
-    for i in branch_list:
-        if i.startswith("*"):
-            branch_list.remove(i)
-            branch_list = [i[2:]] + branch_list
-            break;
-
-    # remove blank spaces
-    if "" in branch_list:
-        branch_list.remove("")
-
-    # trim branch strings
-    ret_list = []
-    for i in branch_list:
-        ret_list.append(i.strip())
-
     if len(ret_list) > 0:
         return ret_list
     else:
@@ -84,6 +40,4 @@ if __name__ == "__main__":
 
     for r in make_repo_list(basepath):
         print("repo: %s" % r)
-        print("branches: %s" % get_branches(r))
-        print("remotes: %s" % get_remotes(r))
 
