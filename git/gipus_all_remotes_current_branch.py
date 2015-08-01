60e7124e679928ccf62a2a9ec780c3270bacced2
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-01 19:05:44 -0300

    introducing new intermediate tools

---
+++ git/gipus_all_remotes_current_branch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import git_repo_query
+from subprocess import check_output
+
+def puaq(): # print usage and quit
+    print("Usage: %s repo_path (.git folder included)." % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    repo_path = sys.argv[1]
+
+    remotes = git_repo_query.get_remotes(repo_path)
+    branches = git_repo_query.get_branches(repo_path)
+
+    if branches is None:
+        print("No branches detected in %s. Aborting." % repo_path)
+        sys.exit(1)
+
+    if remotes is None:
+        print("No remotes detected in %s. Aborting." % repo_path)
+        sys.exit(1)
+
+    current_branch = branches[0]
+
+    for r in remotes:
+        print("Pushing to %s..." % r)
+        out = check_output(["git", "--git-dir=%s" % repo_path, "--work-tree=%s" % os.path.dirname(repo_path), "push", r, current_branch])
+
