b97be4522ac4a963dd4652264bc7417de8e5e311
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-02 19:48:55 -0300

    better naming convention

--- git/git_visitor_status.py
+++ git/git_visitor_status.py
@@ -5,7 +5,7 @@ import os
 import git_visitor_base
 from subprocess import check_output
 
-def run_visitor_status(list_repos):
+def visitor_status(list_repos):
     for r in list_repos:
         out = check_output(["git", "--git-dir=%s" % r, "--work-tree=%s" % os.path.dirname(r), "status", "-s"])
         if len(out) == 0:
@@ -14,5 +14,5 @@ def run_visitor_status(list_repos):
             print("%s is dirty." % os.path.dirname(r))
 
 if __name__ == "__main__":
-    git_visitor_base.do_visit(sys.argv, run_visitor_status)
+    git_visitor_base.do_visit(sys.argv, visitor_status)
 
