f6cd9ca82e3088daa50a7b049de911b5d44a9e2a
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-02 20:10:42 -0300

    git visitor push with bugfixes - it mostly works now

--- git/git_visitor_push.py
+++ git/git_visitor_push.py
@@ -8,12 +8,13 @@ from subprocess import check_output
 
 def visitor_push(repos):
     for rp in repos:
-        print("* Pushing to %s ..." % rp)
+        print("\n* Pushing to %s ..." % rp)
         remotes = git_repo_query.get_remotes(rp)
         for rm in remotes:
             out = check_output(["git", "--git-dir=%s" % rp, "--work-tree=%s" % os.path.dirname(rp), "push", rm])
+            # mvtodo: if any of these calls fail, the show is stopped. this is obviously wrong. dont stop when any repo fails because local and remote are out of sync
             # mvtodo: I could parse out and print more informative stuff
-    print("   \n")
+    # mvtodo: should also print a shorter report in the end (no failures vs. this and that failed, instead of wanting the user to read thru all the push messages)
 
 if __name__ == "__main__":
     git_visitor_base.do_visit(sys.argv, visitor_push)
