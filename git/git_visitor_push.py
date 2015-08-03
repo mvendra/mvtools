488be33b13a7b6b5dff7214a65b0cf3e13367e52
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-03 11:03:05 -0300

    multibranch visitor prototype

--- git/git_visitor_push.py
+++ git/git_visitor_push.py
@@ -14,17 +14,20 @@ def visitor_push(repos):
         print("\n* Pushing to %s ..." % rp)
         remotes = git_repo_query.get_remotes(rp)
         for rm in remotes:
+            branches = git_repo_query.get_branches(rp)
+            for bn in branches:
 
-            try:
-                out = subprocess.check_output(["git", "--git-dir=%s" % rp, "--work-tree=%s" % os.path.dirname(rp), "push", rm])
-                out = "OK."
-            except OSError as oser:
-                out = "Failed."
-            except subprocess.CalledProcessError as cper:
-                out = "Failed."
-            
-            bn = "mvtodo"
-            report.append("%s (remote=%s, branch=%s): %s" % (rp, rm, bn, out))
+                try:
+                    mvdebugl = ["git", "--git-dir=%s" % rp, "--work-tree=%s" % os.path.dirname(rp), "push", rm, bn]
+                    print("mvdebug %s" % mvdebugl)
+                    out = subprocess.check_output(["git", "--git-dir=%s" % rp, "--work-tree=%s" % os.path.dirname(rp), "push", rm, bn])
+                    out = "OK."
+                except OSError as oser:
+                    out = "Failed."
+                except subprocess.CalledProcessError as cper:
+                    out = "Failed."
+                
+                report.append("%s (remote=%s, branch=%s): %s" % (rp, rm, bn, out))
 
     print("\nRESULTS:")
     for p in report:
