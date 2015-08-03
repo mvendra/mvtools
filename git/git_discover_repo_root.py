5776642656bd0dff34544735d10c722efcbf1a6c
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-03 13:56:06 -0300

    introducing new tool to discover the full path to the root directory, of your CWD happens to be a repo's subfolder

---
+++ git/git_discover_repo_root.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import path_utils
+
+def is_repo_root(path):
+    if not os.path.exists(path):
+        return False
+    if path.endswith(".git") or path.endswith(".git" + os.sep):
+        return True
+
+if __name__ == "__main__":
+
+    curpath = os.getcwd()
+    while not is_repo_root(os.path.join(curpath, ".git")):
+        curpath = path_utils.backpedal_path(curpath)
+        if curpath is None:
+            break
+        print(os.path.join(curpath, ".git"))
+
