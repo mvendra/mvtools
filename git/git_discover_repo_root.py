a747a4f42d0642688eb98d1fecdb878e49a06f22
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-03 14:02:57 -0300

    allowing git discover repo root tool to be overriden with cmdline args

--- git/git_discover_repo_root.py
+++ git/git_discover_repo_root.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import path_utils
 
 def is_repo_root(path):
@@ -11,7 +12,11 @@ def is_repo_root(path):
 
 if __name__ == "__main__":
 
-    curpath = os.getcwd()
+    curpath = os.getcwd() # uses current working dir by default
+    if len(sys.argv) > 1:
+        # override with cmdline arg
+        curpath = sys.argv[1]
+
     while not is_repo_root(os.path.join(curpath, ".git")):
         curpath = path_utils.backpedal_path(curpath)
         if curpath is None:
