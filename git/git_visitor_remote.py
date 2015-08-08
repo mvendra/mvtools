a319a9cd6ffeca25150f011f2a15ce40209666e3
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-08 18:40:20 -0300

    introducing git visitor remote

---
+++ git/git_visitor_remote.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+
+import git_visitor_base
+import git_repo_query
+
+def visitor_remote(repos):
+
+    for rp in repos:
+        print("\n* Listing remotes of %s ..." % rp)
+        remotes = git_repo_query.get_remotes(rp)
+        for rm in remotes:
+            print(rm)
+
+if __name__ == "__main__":
+    git_visitor_base.do_visit(sys.argv, visitor_remote)
+
