9111d987f620737c316b6a82fe439f4ef064b92b
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-08-08 20:01:53 -0300

    decoupled password detection+copying and temporary decryption

--- security/dec_and_copy_pass.py
+++ security/dec_and_copy_pass.py
@@ -7,61 +7,31 @@ import path_utils
 import getpass
 
 def puaq():
-    print("Usage: %s password_file" % os.path.basename(__file__))
+    print("Usage: %s enc_password_file" % os.path.basename(__file__))
     sys.exit(1)
-
-def getpasswordfromcontents(contents):
-
-    ID_PATTERN_S="pw=["
-    ID_PATTERN_E="]"
-
-    of_s = contents.find(ID_PATTERN_S)
-
-    if of_s == -1:
-        return None
-
-    of_s += len(ID_PATTERN_S)
-    of_e = contents.find(ID_PATTERN_E, of_s)
-
-    if of_e == -1:
-        return None
-
-    return contents[of_s:of_e]
 
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         puaq()
 
-    passfile = sys.argv[1]
-    if not os.path.exists(passfile):
-        print("%s does not exist. Aborting." % passfile)
+    encpassfile = sys.argv[1]
+    if not os.path.exists(encpassfile):
+        print("%s does not exist. Aborting." % encpassfile)
         sys.exit(1)
 
     random_fn = subprocess.check_output(["randomfilenamegen.sh"])
     passphrase = getpass.getpass("Type in...\n")
-    contents=""
 
     try:
-        subprocess.check_output(["decrypt.sh", passfile, random_fn, passphrase])
-        with open(random_fn) as f:
-            contents=f.read()
+        subprocess.check_output(["decrypt.sh", encpassfile, random_fn, passphrase])
     except:
         print("Failed decrypting file.")
         path_utils.deletefile_ignoreerrors(random_fn)
         sys.exit(1)
 
-    password = getpasswordfromcontents(contents)
-    content_to_copy = password
-    if password == None:
-        opt = raw_input("Unable to find predefined text pattern. Press enter and I will copy entire file. This is your chance to abort.")
-        content_to_copy = contents
-        #print("Unable to extract password from file - cant find the password text pattern.")
-        #path_utils.deletefile_ignoreerrors(random_fn)
-        #sys.exit(1)
-
     try:
-        subprocess.call(["sendtoclipboard.py", content_to_copy])
+        subprocess.call(["copypass.py", random_fn])
         path_utils.deletefile_ignoreerrors(random_fn)
     except:
         print("Unable to send password to clipboard.")
