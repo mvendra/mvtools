0136d555356a7571d3a5bcb98ebc9d0d28323bd8
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-09-02 19:52:47 -0300

    new c boot codegen script

---
+++ codegen/cboot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CBOOTFILE=testforecho.c
+if [ ! -z $1 ]; then
+  CBOOTFILE=$1
+fi
+
+if [ -e ./$CBOOTFILE ]; then
+  echo "There's already a ./$CBOOTFILE in the CWD (`pwd -P`), so this script is aborted."
+  exit 1
+fi
+
+touch ./$CBOOTFILE
+echo "\n#include <stdio.h>\n\nint main(int argc, char *argv[]){\n\n    printf(\"test for echo\\\n\");\n\n    return 0;\n\n}" > ./$CBOOTFILE
+
