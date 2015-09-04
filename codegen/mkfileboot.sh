83dcf1fc558cde59fa573698d880d29c2411aa18
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-09-04 18:00:40 -0300

    securing Makefile booter

--- codegen/mkfileboot.sh
+++ codegen/mkfileboot.sh
@@ -10,6 +10,12 @@ if [ -e ./$MKFILEBOOTFILE ]; then
   exit 1
 fi
 
+MKFILE_TEMPLATE=$BUILDING/makefiletemplates/Makefile_singlefolder_apponly
+if [ ! -e $MKFILE_TEMPLATE ]; then
+  echo "The makefile template $MKFILE_TEMPLATE does not exist. This script has no hope to succeed."
+  exit 1
+fi
+
 touch ./$MKFILEBOOTFILE
-cat $BUILDING/makefiletemplates/Makefile_singlefolder_apponly > ./$MKFILEBOOTFILE
+cat $MKFILE_TEMPLATE > ./$MKFILEBOOTFILE
 
