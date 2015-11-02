24761757263aa3d54791eae93c44f5ceb75e5069
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-02 18:00:42 -0200

    some refactors. less intrusion on policing by visitor base

--- git/git_visitor_base.py
+++ git/git_visitor_base.py
@@ -5,37 +5,49 @@ import os
 import fsquery
 import path_utils
 
-def puaq():
-    print("Usage: %s base_path." % os.path.basename(__file__))
-    sys.exit(1)
+def filter_git_only(path_list):
 
-def __filter_git_only(thelist):
+    """ filter_git_only
+    given path_list, will filter elements if they point to a git repository (.git folder)
+    returns a subset of path_list that do point to a git repository's git folder
+    """
+
     ret = []
-    for d in thelist:
+    for d in path_list:
         if d.endswith(".git") or d.endswith(".git" + os.sep):
             ret.append(d)
     return ret
 
-def make_paths_list(argv):
+def make_path_list(paths_to_consider):
 
-    """ make_paths_list
-    returns a list of paths, based on argv or envvars
+    """ make_path_list
+    assembles a list of valid paths, based on function parameter and envvar
     """
 
     paths = []
-    if len(argv) > 1:
-        # paths specified by cmdline arg. use it.
-        paths = argv[1:]
-    else:
-        # no paths specified. try to get paths from envvars
-        try:
-            paths.append(os.environ["MVBASE"])
-        except KeyError:
-            print("No paths specified by argv, and MVBASE is undefined.")
-            return None
 
+    if paths_to_consider is not None:
+        for pc in paths_to_consider:
+            paths.append(pc)
+
+    path_env = get_path_from_env()
+    if path_env is not None:
+        paths.append(path_env)
     paths = path_utils.filter_path_list_no_same_branch(paths) # sanitises paths list
     return paths
+
+def get_path_from_env():
+
+    """ get_path_from_env
+    will return the path defined by the environment. returns None if its undefined.
+    """
+
+    path = None
+    try:
+        path = os.environ["MVBASE"]
+    except KeyError:
+        pass
+    return path
 
 def make_repo_list(path):
 
@@ -47,15 +59,15 @@ def make_repo_list(path):
         return None
 
     ret_list = fsquery.makecontentlist(path, True, False, True, False, True, [])
-    ret_list = __filter_git_only(ret_list)
+    ret_list = filter_git_only(ret_list)
     if len(ret_list) > 0:
         return ret_list
     else:
         return None
 
-def do_visit(argv, func):
+def do_visit(path_list, func):
 
-    paths = make_paths_list(argv)
+    paths = make_path_list(path_list)
     if paths is None:
         print("No paths to visit.")
         return
@@ -66,17 +78,4 @@ def do_visit(argv, func):
             print("Warning: %s has no repositories." % p)
             continue
         func(repos)
-
-if __name__ == "__main__":
-
-    if len(sys.argv) < 2:
-        puaq()
-
-    basepath = sys.argv[1]
-    if not os.path.exists(basepath):
-        print("%s does not exist. Aborting." % basepath)
-        sys.exit(1)
-
-    for r in make_repo_list(basepath):
-        print("repo: %s" % r)
 
