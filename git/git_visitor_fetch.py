9cadd436509dde9a60726e2ddd5878823a5f4377
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-08 12:12:59 -0200

    adapting visitor fetch to use remotes filtering

--- git/git_visitor_fetch.py
+++ git/git_visitor_fetch.py
@@ -4,6 +4,8 @@ import git_visitor_base
 import git_repo_query
 import git_fetch
 
+import sys
+
 def visitor_fetch(repos, options):
 
     ORIGINAL_COLOR = "\033[0m" # mvtodo: would be better to try to detect the terminal's current standard color
@@ -11,6 +13,10 @@ def visitor_fetch(repos, options):
     report = []
     for rp in repos:
         remotes = git_repo_query.get_remotes(rp)
+        remotes = git_visitor_base.filter_remotes(remotes, options)
+        if remotes is None:
+            report.append("\033[31m%s: Failed filtering remotes\033[0m" % rp) # those colors are RED and WHITE, respectively. mvtodo: also change them here eventually
+            continue
         report_piece = git_fetch.do_fetch(rp, remotes)
         for ri in report_piece:
             report.append(ri)
@@ -21,5 +27,10 @@ def visitor_fetch(repos, options):
     print("%s\n" % ORIGINAL_COLOR) # reset terminal color
 
 if __name__ == "__main__":
-    git_visitor_base.do_visit(None, None, visitor_fetch)
+
+    filters = None
+    if len(sys.argv) > 1:
+        filters = sys.argv[1:]
+
+    git_visitor_base.do_visit(None, filters, visitor_fetch)
 
