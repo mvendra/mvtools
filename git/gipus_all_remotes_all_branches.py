a4502881c2dbe9232dc3400b8b6180ab977a7467
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-08 16:49:30 -0200

    another gipus extension reusing visitor push

--- git/gipus_all_remotes_all_branches.py
+++ git/gipus_all_remotes_all_branches.py
@@ -5,6 +5,8 @@ import os
 import git_repo_query
 import git_push
 
+import git_visitor_push
+
 def puaq(): # print usage and quit
     print("Usage: %s repo_path (.git folder included)." % os.path.basename(__file__))
     sys.exit(1)
@@ -14,24 +16,10 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         puaq()
 
-    repo_path = sys.argv[1]
+    options = {}
+    repos = []
 
-    remotes = git_repo_query.get_remotes(repo_path)
-    branches = git_repo_query.get_branches(repo_path)
+    repos += [sys.argv[1]]
 
-    if branches is None:
-        print("No branches detected in %s. Aborting." % repo_path)
-        sys.exit(1)
-
-    if remotes is None:
-        print("No remotes detected in %s. Aborting." % repo_path)
-        sys.exit(1)
-
-    ORIGINAL_COLOR = "\033[0m" # mvtodo: would be better to try to detect the terminal's current standard color
-    report = git_push.do_push(repo_path, remotes, branches)
-
-    print("\nRESULTS:")
-    for p in report:
-        print(p)
-    print("%s" % ORIGINAL_COLOR) # reset terminal color
+    git_visitor_push.visitor_push(repos, options)
 
