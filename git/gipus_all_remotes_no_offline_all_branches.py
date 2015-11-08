f988632d765b300759ee5b85f7a59f6117cf2654
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-08 16:23:00 -0200

    now the gipus extension is reusing code from visitor pull.

--- git/gipus_all_remotes_no_offline_all_branches.py
+++ git/gipus_all_remotes_no_offline_all_branches.py
@@ -5,6 +5,8 @@ import os
 import git_repo_query
 import git_push
 
+import git_visitor_pull
+
 def puaq(): # print usage and quit
     print("Usage: %s repo_path (.git folder included)." % os.path.basename(__file__))
     sys.exit(1)
@@ -14,31 +16,11 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         puaq()
 
-    repo_path = sys.argv[1]
+    options = {}
+    repos = []
 
-    remotes = git_repo_query.get_remotes(repo_path)
-    branches = git_repo_query.get_branches(repo_path)
+    repos += [sys.argv[1]]
+    options["no-remote"] = ["offline"]
 
-    if branches is None:
-        print("No branches detected in %s. Aborting." % repo_path)
-        sys.exit(1)
-
-    if remotes is None:
-        print("No remotes detected in %s. Aborting." % repo_path)
-        sys.exit(1)
-
-    # removes the offline remote
-    aux = remotes
-    remotes = []
-    for r in aux:
-        if r != "offline":
-            remotes.append(r)
-
-    ORIGINAL_COLOR = "\033[0m" # mvtodo: would be better to try to detect the terminal's current standard color
-    report = git_push.do_push(repo_path, remotes, branches)
-
-    print("\nRESULTS:")
-    for p in report:
-        print(p)
-    print("%s" % ORIGINAL_COLOR) # reset terminal color
+    git_visitor_pull.visitor_pull(repos, options)
 
