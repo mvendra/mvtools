7ae19878eebd2432d3758827a2ae489aeaa56194
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-10 18:44:59 -0200

    fixing puaq msg, now using the -C option with git

--- git/gifet_all.py
+++ git/gifet_all.py
@@ -5,7 +5,7 @@ import os
 from subprocess import check_output
 
 def puaq(): # print usage and quit
-    print("Usage: %s repo_path (.git folder included)." % os.path.basename(__file__))
+    print("Usage: %s repo_path." % os.path.basename(__file__))
     sys.exit(1)
 
 if __name__ == "__main__":
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print("Fetching %s..." % repo_path)
 
     try:
-        out = check_output(["git", "--git-dir=%s" % repo_path, "--work-tree=%s" % os.path.dirname(repo_path), "fetch", "--all"])
+        out = check_output(["git", "-C", repo_path, "fetch", "--all"])
         out = "Done with success."
     except:
         out = "Failed."
