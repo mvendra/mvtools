45aa7dcde05f79d510d599770c7f9ba4c2a0cf27
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-10 18:57:15 -0200

    fixed visitor status, now using -C git options

--- git/git_visitor_status.py
+++ git/git_visitor_status.py
@@ -6,11 +6,11 @@ from subprocess import check_output
 
 def visitor_status(repos, options):
     for r in repos:
-        out = check_output(["git", "--git-dir=%s" % r, "--work-tree=%s" % os.path.dirname(r), "status", "-s"])
+        out = check_output(["git", "-C", r, "status", "-s"])
         if len(out) == 0:
             pass # clean HEAD
         else:
-            print("%s is dirty." % os.path.dirname(r))
+            print("%s is dirty." % r)
 
 if __name__ == "__main__":
     git_visitor_base.do_visit(None, None, visitor_status)
