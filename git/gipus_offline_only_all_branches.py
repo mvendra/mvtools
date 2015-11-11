02c4d6ba091b93e159d3c580bb2ad25698e31e95
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-11 18:08:11 -0200

    introducing gipusall and gipulall for the offline remote exclusive mode

---
+++ git/gipus_offline_only_all_branches.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+import git_visitor_push
+
+def puaq(): # print usage and quit
+    print("Usage: %s repo_path." % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    options = {}
+    repos = []
+
+    repos += [sys.argv[1]]
+    options["xor-remote"] = ["offline"]
+
+    git_visitor_push.visitor_push(repos, options)
+
