1a35d42db4e45758094182c411adb9586971998b
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-12 19:40:18 -0200

    introducing helper script to retrieve terminal colors

---
+++ terminal_colors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+TTY_WHITE = "\033[0m"
+TTY_RED = "\033[31m"
+TTY_GREEN = "\033[32m"
+
+def get_standard_color():
+    return TTY_WHITE # mvtodo: eventually this should dynamically read the env - LS_COLORS, likely
+
+if __name__ == "__main__":
+    print("A script to retrieve envionment's terminal colors.")
+
