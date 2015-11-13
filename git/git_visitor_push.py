52be435b77d5522c03f881baa13d50b08dfdd089
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-13 19:55:54 -0200

    visitor pull now also with improved report printing

--- git/git_visitor_push.py
+++ git/git_visitor_push.py
@@ -18,8 +18,8 @@ def visitor_push(repos, options):
         try:
             remotes, branches = git_visitor_base.apply_filters(rp, options)
         except git_visitor_base.gvbexcept as gvbex:
-            report.append("%s%s: %s.%s" % (terminal_colors.TTY_RED, rp, gvbex.message, ORIGINAL_COLOR)) 
             all_passed = False
+            report.append("%s%s: %s.%s" % (terminal_colors.TTY_RED, rp, gvbex.message, ORIGINAL_COLOR)) 
             continue
 
         op_piece, report_piece = git_push.do_push(rp, remotes, branches)
