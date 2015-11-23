1c328b4a0bbdb639d370eaa0ab8e0307bc191ec6
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-23 13:51:30 -0300

    furthering the mvtags commit wrapper development

--- git/test_mvtags_in_git_cache.py
+++ git/test_mvtags_in_git_cache.py
@@ -3,20 +3,45 @@
 import sys
 import os
 
+import git_repo_query
+
 def puaq():
     print("Usage: %s repo_path" % os.path.basename(__file__))
     sys.exit(1)
 
-def check_mvtags(repo):
+def check_mvtags_in_file(thefile):
 
-    """ check_mvtags
-    searches a repo for mvtags, but only on cached (staged) files
-    returns a list of staged files with mvtags, when any is found
-    or returns None when no mvtags are found on the given repository
+    """ check_mvtags_in_file
+    returns true if thefile has mvtags in it
+    returns false otherwise
+    returns None if thefile does not exist
     """
 
-    # mvtodo: implement
-    return None
+    if not os.path.exists(thefile):
+        return None
+
+    # mvtodo: get the diff of thefile and check it for mvtags
+    # mvtodo: if present, return True
+    
+    return False
+
+def check_mvtags_in_repo(repo):
+
+    """ check_mvtags_in_repo
+    searches a repo for mvtags, but only on cached (staged) files
+    and only in the introduced changes.
+    returns a list of staged files with mvtags, when any is found.
+    returns an empty list when none is found.
+    """
+
+    ret = []
+
+    files = git_repo_query.get_staged_files(repo)
+    for f in files:
+        if check_mvtags_in_file(f):
+            ret.append(f)
+
+    return ret
 
 if __name__ == "__main__":
 
@@ -25,7 +50,7 @@ if __name__ == "__main__":
 
     repo = sys.argv[1]
 
-    report = check_mvtags(repo)
+    report = check_mvtags_in_repo(repo)
     if report is None:
         exit(0)
     else:
