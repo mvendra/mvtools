ed4552f6b488114c279651712141cc45db4a8fbe
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-23 11:08:40 -0300

    adding basic docstring to the new mvtags/commit script

--- git/test_mvtags_in_git_cache.py
+++ git/test_mvtags_in_git_cache.py
@@ -8,6 +8,13 @@ def puaq():
     sys.exit(1)
 
 def check_mvtags(repo):
+
+    """ check_mvtags
+    searches a repo for mvtags, but only on cached (staged) files
+    returns a list of staged files with mvtags, when any is found
+    or returns None when no mvtags are found on the given repository
+    """
+
     # mvtodo: implement
     return None
 
