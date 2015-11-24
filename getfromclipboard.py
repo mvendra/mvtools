d9130d63d4a9f87bf9754fa2ee0ff7f33af3d17a
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-24 19:19:20 -0200

    safety check, getfromclipboard: wasnt catching exception

--- getfromclipboard.py
+++ getfromclipboard.py
@@ -2,10 +2,15 @@
 
 import sys
 import os
-import subprocess
+from subprocess import check_output
+from subprocess import CalledProcessError
 
 def getfromclipboards():
-    contents = subprocess.check_output(["xclip", "-sel", "clip", "-o"])
+    try:
+        contents = check_output(["xclip", "-sel", "clip", "-o"])
+    except CalledProcessError as cpe:
+        print("Failed to call xclip. Make sure it is installed.")
+        exit(1)
     return contents
 
 if __name__ == "__main__":
