4bb25dfe28bb3ca9ef3533af97365af172c5bd04
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-25 09:23:59 -0300

    trading system() to subproc.call(), sendtoclipboard

--- sendtoclipboard.py
+++ sendtoclipboard.py
@@ -3,12 +3,14 @@
 import sys
 import os
 
+from subprocess import call
+
 def puaq():
     print("Usage: %s contents" % os.path.basename(__file__))
     sys.exit(1)
 
 def sendtoclipboard(contents):
-    os.system("inline_echo.py '%s' | xclip -sel clip" % contents)
+    call("inline_echo.py '%s' | xclip -sel clip" % contents, shell=True)
 
 if __name__ == "__main__":
 
