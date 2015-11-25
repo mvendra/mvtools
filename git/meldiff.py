1022bef20c4e76c76364fae5d7b140c0568834ad
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-11-25 09:26:32 -0300

    trading system() with subproc.call(), meldiff script

--- git/meldiff.py
+++ git/meldiff.py
@@ -3,9 +3,11 @@
 import sys
 import os
 
+from subprocess import call
+
 if __name__ == "__main__":
     if (len(sys.argv) < 8):
         print("This script is not intended for standalone use - integrate with meld instead.")
         sys.exit(1)
-    os.system("meld %s %s" % (sys.argv[2], sys.argv[5]))
+    call("meld %s %s" % (sys.argv[2], sys.argv[5]), shell=True)
 
