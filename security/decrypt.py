ee1fb518d7c84ced048a50c12baaa7e27f712410
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-03-13 15:37:13 -0300

    parameterising decrypt

--- security/decrypt.py
+++ security/decrypt.py
@@ -12,6 +12,27 @@ def puaq():
     print("Usage: %s infile [outfile] [passphrase]" % os.path.basename(__file__))
     sys.exit(1)
 
+def symmetric_decrypt(infile, outfile, passphrase):
+
+    """
+    symmetric_decrypt
+    parameters are all mandatory here
+    """
+
+    if not os.path.isfile(infile):
+        print("%s does not exist. Aborting.")
+        sys.exit(1)
+
+    if outfile == "" or outfile == None:
+        print("Invalid output filename. Aborting")
+        sys.exit(1)
+
+    if passphrase == "" or passphrase == None:
+        print("Invalid passphrase. Aborting.")
+        sys.exit(1)
+
+    call(["openssl", "des3", "-d", "-salt", "-in", infile, "-out", outfile, "-k", passphrase])
+
 if __name__ == "__main__":
 
     infile = ""
@@ -39,5 +60,5 @@ if __name__ == "__main__":
     if (passphrase == ""):
         passphrase = getpass.getpass("Type in...\n")
 
-    call(["openssl", "des3", "-d", "-salt", "-in", infile, "-out", outfile, "-k", passphrase])
+    symmetric_decrypt(infile, outfile, passphrase)
 
