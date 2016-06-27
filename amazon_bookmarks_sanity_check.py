7264b40c805344334b0db055c1ffc7944d01dbea
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-06-27 11:21:00 -0300

    disabled execution of amazon bookmarks sanity script prototype

--- amazon_bookmarks_sanity_check.py
+++ amazon_bookmarks_sanity_check.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
+import sys
 import urllib
 
 if __name__ == "__main__":
+
+    print("script disabled, it's incomplete")
+    sys.exit(1)
+
     cont = urllib.urlopen("http://www.amazon.com/Templates-Complete-Guide-David-Vandevoorde/dp/0201734842/ref=sr_1_1?ie=UTF8&qid=1438170638&sr=8-1&keywords=c%2B%2B+templates")
     # mvtodo: extract all links from a bookmarks dump
     # mvtodo: extract isbn13 from each link
