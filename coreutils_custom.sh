d4c587e7fda13678b9c57baa15c11c0191e27026
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-07-20 09:54:53 -0300

    coreutils helper was breaking LS for mac

--- coreutils_custom.sh
+++ coreutils_custom.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-alias ls="ls --color --group-directories-first --sort=extension"
+unamestr=`uname`
+
+if [[ "$unamestr" == 'Linux' ]]; then
+  alias ls="ls --color --group-directories-first --sort=extension"
+fi
+
 alias la="ls -la"
 alias lash="ls -lash"
 alias mv="mv -i"
