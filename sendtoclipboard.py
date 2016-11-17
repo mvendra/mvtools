31de4add5ce181940419ee026c785f9ca944bd41
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-11-17 11:33:22 -0200

    support for windows clipboard

--- sendtoclipboard.py
+++ sendtoclipboard.py
@@ -11,10 +11,32 @@ def puaq():
     print("Usage: %s contents" % os.path.basename(__file__))
     sys.exit(1)
 
+def getplat():
+    ps = platform.system().lower()
+    if ps == "linux":
+        return "linux"
+    elif ps == "windows":
+        return "windows"
+    elif ps == "cygwin_nt-10.0":
+        return "cygwin"
+    elif ps == "darwin":
+        return "macosx"
+
 def sendtoclipboard(contents):
-    clipboard_app = "xclip -sel clip" # for linux
-    if platform.system() == "Darwin":
+
+    clipboard_app = ""
+    plat = getplat()
+
+    if plat == "linux":
+        clipboard_app = "xclip -sel clip" # for linux
+    elif plat == "windows" or plat == "cygwin":
+        clipboard_app = "clip"
+    elif plat == "macosx":
         clipboard_app = "pbcopy"
+    else:
+        print("UNsupported platform")
+        return
+
     call("inline_echo.py '%s' | %s" % (contents, clipboard_app), shell=True)
 
 if __name__ == "__main__":
@@ -28,4 +50,3 @@ if __name__ == "__main__":
     contents = contents.strip()
 
     sendtoclipboard(contents)
-
