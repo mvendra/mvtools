8f99e3a705bc901bbbe2a4697664feb26cc672cd
Mateus Vendramini <mateusvendramini21@gmail.com>
2016-11-18 12:53:24 -0200

    adapted copypwd for cygwin

--- copypwd.sh
+++ copypwd.sh
@@ -4,7 +4,8 @@ PWD_FIX=(`pwd -P`)
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
     inline_echo.py $PWD_FIX | xclip -sel clip
-else
+elif [[ "$unamestr" == 'Darwin' ]]; then
     inline_echo.py $PWD_FIX | pbcopy
+elif [[ "$unamestr" == 'CYGWIN_NT-10.0' ]]; then
+	inline_echo.py $PWD_FIX | clip
 fi
-
