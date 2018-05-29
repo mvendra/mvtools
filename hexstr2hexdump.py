c5df96773692401e5c6f4be95617b527e6179644
Mateus Vendramini <mateusvendramini21@gmail.com>
2018-05-28 22:29:10 -0300

    introducing hex str to hex dump converter script

---
+++ hexstr2hexdump.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+def hexstring_to_hexdump(hexstring):
+
+    result = ""
+
+    hexstring = hexstring.replace("\n", " ")
+    hexstring = hexstring.replace(" ", "")
+
+    if (len(hexstring) % 2 != 0):
+        print("Invalid hex string")
+        return ""
+
+    c = 0
+    offs = 0
+    result += "%04X   " % offs
+    for i in range(0, len(hexstring), 2):
+        if c == 16:
+            result += "\n"
+            offs += 16
+            result += "%04X   " % offs
+            c = 0
+
+        result += hexstring[i]
+        result += hexstring[i+1]
+        result += " "
+        c += 1
+    result += "\n"
+
+    return result
+
+def puaq():
+    print("Usage: %s file_with_hexstring.txt" % os.path.basename(__file__))
+    sys.exit(1)
+
+def make_output_filename(filename):
+    r = filename.find(".")
+    if (r == -1):
+        return "%s_out" % filename
+    f, e = filename.split(".")
+    return "%s_out.%s" % (f, e)
+
+def getcontents(filename):
+    cnt = ""
+    with open(filename) as f:
+        cnt = f.read()
+    return cnt
+
+def savecontents(filename, contents):
+    with open(filename, "w") as f:
+        f.write(contents)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    input_filename = os.path.basename(sys.argv[1])
+    output_filename = ""
+    if len(sys.argv) > 2:
+        output_filename = sys.argv[2]
+    else:
+        output_filename = make_output_filename(input_filename)
+
+    if not os.path.exists(input_filename):
+        print("%s does not exist. Aborting." % input_filename)
+        sys.exit(1)
+
+    if os.path.exists(output_filename):
+        print("%s already exists. Aborting." % output_filename)
+        sys.exit(1)
+
+    contents = getcontents(input_filename)
+    contents = hexstring_to_hexdump(contents)
+    savecontents(output_filename, contents)
