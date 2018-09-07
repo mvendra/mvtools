2d723d6aa294922a9dc52fa5ce50872f672f9524
Mateus Vendramini <mateusvendramini21@gmail.com>
2018-09-07 14:11:01 -0300

    fixed copypwd for paths with spaces

--- copypwd.sh
+++ copypwd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PWD_FIX=(`pwd -P`)
+PWD_FIX=`pwd -P`
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-    inline_echo.py $PWD_FIX | xclip -sel clip
+    inline_echo.py "$PWD_FIX" | xclip -sel clip
 elif [[ "$unamestr" == 'Darwin' ]]; then
-    inline_echo.py $PWD_FIX | pbcopy
+    inline_echo.py "$PWD_FIX" | pbcopy
 elif [[ "$unamestr" == 'CYGWIN_NT-10.0' ]]; then
-	inline_echo.py $PWD_FIX | clip
+	inline_echo.py "$PWD_FIX" | clip
 fi
