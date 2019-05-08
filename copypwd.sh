c1e29b72b10a54bba41ca03638de6b1688cd2ed2
Mateus Vendramini <mateusvendramini21@gmail.com>
2019-05-08 17:00:17 +0200

    copypwd now uses sendtoclipboard. more skin in the game.

--- copypwd.sh
+++ copypwd.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 PWD_FIX=`pwd -P`
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-    inline_echo.py "$PWD_FIX" | xclip -sel clip
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    inline_echo.py "$PWD_FIX" | pbcopy
-elif [[ "$unamestr" == 'CYGWIN_NT-10.0' ]]; then
-	inline_echo.py "$PWD_FIX" | clip
-fi
+sendtoclipboard.py $PWD_FIX
