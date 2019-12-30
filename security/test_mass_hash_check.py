47ab7d8dafe3e4d7ca1bf6baff6c9ad414053180
Mateus Vendramini <mateusvendramini21@gmail.com>
2019-12-30 10:56:18 +0100

    small refactors, test_mass_hash_check

--- security/test_mass_hash_check.py
+++ security/test_mass_hash_check.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
@@ -14,17 +14,11 @@ def puaq(): # print usage and quit
     print("Usage: %s path_to_operate files_extension" % os.path.basename(__file__))
     sys.exit(1)
 
-if __name__ == "__main__":
-
-    if len(sys.argv) < 3:
-        puaq()
-
-    path_files = sys.argv[1]
-    extension = sys.argv[2]
+def test_mass_hash_check(path_files, extension):
 
     if not os.path.exists(path_files):
         print("%s does not exist. Aborting." % path_files)
-        sys.exit(1)
+        return False, None
 
     ext_list_aux = []
     ext_list_aux.append(extension)
@@ -38,11 +32,27 @@ if __name__ == "__main__":
         elif not hashcheck.sha256sum_check(f, hash_file):
             report.append(f + " check FAILED.")
 
-    print("\nWill print the report...:")
-    for r in report:
-        print(r)
+    return True, report
+
+def print_report(report):
+    for l in report:
+        print(l)
     if len(report) == 0:
         print("All passed!")
     else:
         print("\nThere were %s errors." % len(report))
 
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        puaq()
+
+    path_files = sys.argv[1]
+    extension = sys.argv[2]
+
+    r, v = test_mass_hash_check(path_files, extension)
+    if not r:
+        sys.exit(1)
+    else:
+        print("\nWill print the report...:")
+        print_report(v)
