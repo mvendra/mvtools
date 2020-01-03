f15e76afec09c7d037f673dfe13f20ae147af49c
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-03 22:54:39 +0100

    porting parsedap to py3

--- inputfixes/parsedap.py
+++ inputfixes/parsedap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Parse Device Accel Profile
 
@@ -6,20 +6,26 @@ import re
 import sys
 import os
 
-if (len(sys.argv) > 1):
+def parsed_accel_profile(STR):
+
+    m = re.search("Device Accel Profile ([0-9]+):", STR)
+    if m == None:
+        return ""
+
+    m = re.search("[0-9]+", m.group(0))
+    if m == None:
+        return ""
+
+    return m.group(0) 
+
+def puaq():
+    print("Usage: %s search-string" % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
     STR=" ".join(sys.argv[1:])
-else:
-    STR=sys.stdin.read().strip()
-
-m = re.search("Device Accel Profile ([0-9]+):", STR)
-if m == None:
-    print("")
-    sys.exit(1)
-
-m = re.search("[0-9]+", m.group(0))
-if m == None:
-    print("")
-    sys.exit(1)
-
-print(m.group(0))
-
+    print(parsed_accel_profile(STR))
