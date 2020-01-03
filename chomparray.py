e5cfe16895c4bfc28c360342860ae9448f6e5f36
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-03 20:42:40 +0100

    porting chomparray to py3 and making it import-friendly

--- chomparray.py
+++ chomparray.py
@@ -1,29 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
+import os
 
-# chomparray.py
-# sample usage:
-# chomparray.py r1 aaa bbb ccc -> yields "aaa bbb"
-# chomparray.py l1 aaa bbb ccc -> yields "bbb ccc"
+def chomparray(option, inputs):
 
-bucket=[]
-for args in sys.argv[2:]:
-    bucket.append(args)
+    # sample usage:
+    # chomparray.py r1 aaa bbb ccc -> yields "aaa bbb"
+    # chomparray.py l1 aaa bbb ccc -> yields "bbb ccc"
 
-option=sys.argv[1]
-d=option[0]
-m=int(option[1:])
+    bucket=[]
+    for args in inputs:
+        bucket.append(args)
 
-ret=None
-if d=="l": # left
-    ret=bucket[m:]
-elif d=="r": # right
-    ret=bucket[:len(bucket)-m]
+    d=option[0]
+    m=int(option[1:])
 
-toline=""
-for p in ret:
-    toline+=p+" "
-toline=toline[:len(toline)-1] # removes trailing blank space
-print(toline)
+    ret=None
+    if d=="l": # left
+        ret=bucket[m:]
+    elif d=="r": # right
+        ret=bucket[:len(bucket)-m]
 
+    toline=""
+    for p in ret:
+        toline+=p+" "
+    toline=toline[:len(toline)-1] # removes trailing blank space
+    print(toline)
+
+def puaq():
+    print("Usage: %s {r1|l1} [string] ..." % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    option = sys.argv[1]
+    inputs = sys.argv[2:]
+
+    chomparray(option, inputs)
