34e7e0e8b5e0c08d37dd70b2b0f920d3826b057d
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-04 13:15:30 +0100

    introducing create_pass_hash script

---
+++ security/create_pass_hash.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import stat
+
+import getpass
+from subprocess import run, PIPE
+
+def create_pass_hash(filename):
+
+    if os.path.exists(hash_fn_full):
+        print("[%s] already exists. Aborting." % hash_fn_full)
+        return False
+
+    # ask passphrase
+    passphrase = getpass.getpass("Type in...\n")
+
+    # generate hash
+    p = run(["sha512sum"], stdout=PIPE, input=passphrase, encoding="ascii")
+    pp_hash_calc = p.stdout[0:128]
+
+    # write out to file
+    with open(filename, "w") as f:
+        f.write(pp_hash_calc)
+
+    # set user-read-only permissions
+    os.chmod(filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+    return True
+
+if __name__ == "__main__":
+
+    filename_base = os.path.expanduser("~/")
+    hash_filename = ".private_passphrase_hash"
+
+    if len(sys.argv) == 2:
+        filename_base = os.getcwd()
+        hash_filename = sys.argv[1]
+
+    hash_fn_full = os.path.join(filename_base, hash_filename)
+    if not create_pass_hash(hash_fn_full):
+        print("Failed creating pass hash file")
+        sys.exit(1)
+    else:
+        print("Successfuly created hash file [%s]." % hash_fn_full)
