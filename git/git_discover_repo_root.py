3ac3bdc874caa8139ac97c819eb58e91a107143e
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-04 15:39:40 +0100

    making git_discover_repo_root more import-friendly

--- git/git_discover_repo_root.py
+++ git/git_discover_repo_root.py
@@ -4,8 +4,6 @@ import os
 import sys
 import path_utils
 
-import inline_echo
-
 def is_repo_root(path):
     if path is None:
         return False
@@ -14,16 +12,23 @@ def is_repo_root(path):
     if path.endswith(".git") or path.endswith(".git" + os.sep):
         return True
 
-if __name__ == "__main__":
-
-    curpath = os.getcwd() # uses current working dir by default
-    if len(sys.argv) > 1:
-        # override with cmdline arg
-        curpath = sys.argv[1]
-
+def git_discover_repo_root(repo_path):
+    curpath = repo_path
     while not is_repo_root(os.path.join(curpath, ".git")):
         curpath = path_utils.backpedal_path(curpath)
         if curpath is None:
-            sys.exit(1)
+            return None
+    return curpath
 
-    inline_echo.inline_echo(curpath)
+if __name__ == "__main__":
+
+    startpath = os.getcwd() # uses current working dir by default
+    if len(sys.argv) > 1:
+        # override with cmdline arg
+        startpath = sys.argv[1]
+
+    r = git_discover_repo_root(startpath)
+    if r is None:
+        print("Failed getting root directory for [%s]." % startpath)
+        sys.exit(1)
+    print(r)
