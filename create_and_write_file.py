1d7b7ca0552bab82b962d0c0b37ff6e73b960d74
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-05 15:57:34 +0100

    introducing create_and_write_file script

---
+++ create_and_write_file.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+def create_file_contents(filename, contents):
+
+    if os.path.exists(filename):
+        print("[%s] already exists. Will not proceed." % filename)
+        return False
+
+    with open(filename, "w+") as f:
+        f.write(contents)
+    return True
+
+def puaq():
+    print("Usage: %s file contents" % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        puaq()
+
+    filename = sys.argv[1]
+    contents = sys.argv[2]
+
+    r = create_file_contents(filename, contents)
+    if not r:
+        print("Failed creating new file.")
+        sys.exit(1)
