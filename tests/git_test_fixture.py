72bf2a95e2f78d6fdd6073afbaae9bab23e88379
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-06 12:46:34 +0100

    created test fixture for git_visitor backends, improved its uts a bit

---
+++ tests/git_test_fixture.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os
+import generic_run
+
+def git_initRepo(repo_base, repo_name, bare):
+    bare_cmd = ""
+    if bare:
+        bare_cmd = " --bare"
+    if not generic_run.run_cmd("git -C %s init %s%s" % (repo_base, repo_name, bare_cmd) ):
+        return False, "Git command failed. Can't proceed."
+    return True, ""
+
+def git_cloneRepo(repo_source, repo_target, remotename):
+    if not generic_run.run_cmd("git clone %s %s -o %s" % (repo_source, repo_target, remotename)):
+        return False, "Git command failed. Can't proceed."
+    return True, ""
+
+def git_createAndCommit(repo, filename, content, commitmsg):
+    if not generic_run.run_cmd("create_and_write_file.py %s %s" % (os.path.join(repo, filename), content) ):
+        return False, "create_and_write_file command failed. Can't proceed."
+
+    if not generic_run.run_cmd("git -C %s add ." % repo):
+        return False, "Git command failed. Can't proceed."
+
+    if not generic_run.run_cmd("git -C %s commit -m %s" % (repo, commitmsg) ):
+        return False, "Git command failed. Can't proceed."
+
+    return True, ""
+
+def git_addRemote(repo, remotename, remotepath):
+    if not generic_run.run_cmd("git -C %s remote add %s %s" % (repo, remotename, remotepath)):
+        return False, "Git command failed. Can't proceed."
+    return True, ""
+
+def git_pushToRemote(repo, remotename, branchname):
+    if not generic_run.run_cmd("git -C %s push %s %s" % (repo, remotename, branchname)):
+        return False, "Git command failed. Can't proceed."
+    return True, ""
+
+def git_pullFromRemote(repo, remotename, branchname):
+    if not generic_run.run_cmd("git -C %s pull %s %s" % (repo, remotename, branchname)):
+        return False, "Git command failed. Can't proceed."
+    return True, ""
+
+def git_mergeWithRemote(repo, remotename, branchname):
+    if not generic_run.run_cmd("git -C %s merge %s/%s" % (repo, remotename, branchname)):
+        return False, "Git command failed. Can't proceed."
+    return True, ""
--- tests/git_visitor_backends_test.py
+++ tests/git_visitor_backends_test.py
@@ -5,7 +5,7 @@ import os
 import shutil
 import unittest
 
-import generic_run
+import git_test_fixture
 
 import git_pull
 import git_push
@@ -24,57 +24,26 @@ class GitVisitorBackendsTest(unittest.TestCase):
         content = "rubbish_content_%s" % self.internal_counter
         return content
 
-    def git_initRepo(self, repo_base, repo_name, bare):
-        bare_cmd = ""
-        if bare:
-            bare_cmd = " --bare"
-        if not generic_run.run_cmd("git -C %s init %s%s" % (repo_base, repo_name, bare_cmd) ):
-            self.fail("Git command failed. Can't proceed.")
-
-    def git_cloneRepo(self, repo_source, repo_target, remotename):
-        if not generic_run.run_cmd("git clone %s %s -o %s" % (repo_source, repo_target, remotename)):
-            self.fail("Git command failed. Can't proceed.")
-
-    def git_createAndCommit(self, repo, filename, content, commitmsg):
-        if not generic_run.run_cmd("create_and_write_file.py %s %s" % (os.path.join(repo, filename), content) ):
-            self.fail("create_and_write_file command failed. Can't proceed.")
-
-        if not generic_run.run_cmd("git -C %s add ." % repo):
-            self.fail("Git command failed. Can't proceed.")
-
-        if not generic_run.run_cmd("git -C %s commit -m %s" % (repo, commitmsg) ):
-            self.fail("Git command failed. Can't proceed.")
-
-    def git_addRemote(self, repo, remotename, remotepath):
-        if not generic_run.run_cmd("git -C %s remote add %s %s" % (repo, remotename, remotepath)):
-            self.fail("Git command failed. Can't proceed.")
-
-    def git_pushToRemote(self, repo, remotename, branchname):
-        if not generic_run.run_cmd("git -C %s push %s %s" % (repo, remotename, branchname)):
-            self.fail("Git command failed. Can't proceed.")
-
-    def git_pullFromRemote(self, repo, remotename, branchname):
-        if not generic_run.run_cmd("git -C %s pull %s %s" % (repo, remotename, branchname)):
-            self.fail("Git command failed. Can't proceed.")
-
-    def git_mergeWithRemote(self, repo, remotename, branchname):
-        if not generic_run.run_cmd("git -C %s merge %s/%s" % (repo, remotename, branchname)):
-            self.fail("Git command failed. Can't proceed.")
-
     def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
 
         self.internal_counter = 0
 
         # must have a $home/nuke folder, for creating test repos
         self.nuke_dir = os.path.expanduser("~/nuke")
         if not os.path.exists(self.nuke_dir):
-            self.fail("[%s] doesn't exist. Can't proceed." % self.nuke_dir)
+            return False, "[%s] doesn't exist. Can't proceed." % self.nuke_dir
 
         # must *not* have a $home/nuke/git_visitor_backends_test_base so it can be created - it will
         # be deleted after each test
         self.test_dir = os.path.join(self.nuke_dir, "git_visitor_backends_test_base")
         if os.path.exists(self.test_dir):
-            self.fail("[%s] already exists. Can't proceed." % self.test_dir)
+            return False, "[%s] already exists. Can't proceed." % self.test_dir
 
         os.mkdir(self.test_dir)
         self.first_repo = os.path.join(self.test_dir, "first")
@@ -82,17 +51,34 @@ class GitVisitorBackendsTest(unittest.TestCase):
         self.third_repo = os.path.join(self.test_dir, "third")
 
         # creates test repos
-        self.git_initRepo(self.test_dir, "second", True)
-        self.git_cloneRepo(self.second_repo, self.first_repo, "origin")
-        self.git_cloneRepo(self.second_repo, self.third_repo, "origin")
+        v, r = git_test_fixture.git_initRepo(self.test_dir, "second", True)
+        if not v:
+            return v, r
+
+        v, r = git_test_fixture.git_cloneRepo(self.second_repo, self.first_repo, "origin")
+        if not v:
+            return v, r
+
+        v, r = git_test_fixture.git_cloneRepo(self.second_repo, self.third_repo, "origin")
+        if not v:
+            return v, r
 
         # create a file with rubbish on first, and push it to its remote (second)
         self.first_repo_first_file = self.makeFilename()
-        self.git_createAndCommit(self.first_repo, self.first_repo_first_file, self.makeContent(), "commit_msg")
-        self.git_pushToRemote(self.first_repo, "origin", "master")
+        v, r = git_test_fixture.git_createAndCommit(self.first_repo, self.first_repo_first_file, self.makeContent(), "commit_msg")
+        if not v:
+            return v, r
+
+        v, r = git_test_fixture.git_pushToRemote(self.first_repo, "origin", "master")
+        if not v:
+            return v, r
 
         # pull changes from first into third, through second
-        self.git_pullFromRemote(self.third_repo, "origin", "master")
+        v, r = git_test_fixture.git_pullFromRemote(self.third_repo, "origin", "master")
+        if not v:
+            return v, r
+
+        return True, ""
 
     def tearDown(self):
         shutil.rmtree(self.test_dir)
@@ -102,8 +88,8 @@ class GitVisitorBackendsTest(unittest.TestCase):
         # setup
         newfile = self.makeFilename()
         newfile_r1 = os.path.join(self.first_repo, newfile)
-        self.git_createAndCommit(self.third_repo, newfile, self.makeContent(), "commit_msg")
-        self.git_pushToRemote(self.third_repo, "origin", "master")
+        git_test_fixture.git_createAndCommit(self.third_repo, newfile, self.makeContent(), "commit_msg")
+        git_test_fixture.git_pushToRemote(self.third_repo, "origin", "master")
 
         # test
         remotes = {}
@@ -125,7 +111,7 @@ class GitVisitorBackendsTest(unittest.TestCase):
         # setup
         newfile = self.makeFilename()
         newfile_r1 = os.path.join(self.first_repo, newfile)
-        self.git_createAndCommit(self.third_repo, newfile, self.makeContent(), "commit_msg")
+        git_test_fixture.git_createAndCommit(self.third_repo, newfile, self.makeContent(), "commit_msg")
 
         # test
         remotes = {}
@@ -140,7 +126,7 @@ class GitVisitorBackendsTest(unittest.TestCase):
         # operation must have succeded without any failures
         self.assertFalse(v)
         # pull the file into first we just pushed from third
-        self.git_pullFromRemote(self.first_repo, "origin", "master")
+        git_test_fixture.git_pullFromRemote(self.first_repo, "origin", "master")
         # must exist now because it was just pulled
         self.assertTrue(os.path.exists( newfile_r1 ))
 
@@ -149,8 +135,8 @@ class GitVisitorBackendsTest(unittest.TestCase):
         # setup
         newfile = self.makeFilename()
         newfile_r1 = os.path.join(self.first_repo, newfile)
-        self.git_createAndCommit(self.third_repo, newfile, self.makeContent(), "commit_msg")
-        self.git_pushToRemote(self.third_repo, "origin", "master")
+        git_test_fixture.git_createAndCommit(self.third_repo, newfile, self.makeContent(), "commit_msg")
+        git_test_fixture.git_pushToRemote(self.third_repo, "origin", "master")
 
         # test
         remotes = {}
@@ -166,7 +152,7 @@ class GitVisitorBackendsTest(unittest.TestCase):
         self.assertFalse(v)
 
         # merges after the fetch
-        self.git_mergeWithRemote(self.first_repo, "origin", "master")
+        git_test_fixture.git_mergeWithRemote(self.first_repo, "origin", "master")
 
         # must exist now because it was just pulled
         self.assertTrue(os.path.exists( newfile_r1 ))
@@ -178,7 +164,7 @@ class GitVisitorBackendsTest(unittest.TestCase):
         operation = ""
         fourth_repo = os.path.join(self.test_dir, "fourth")
         fifth_repo = os.path.join(self.test_dir, "fifth")
-        self.git_initRepo(self.test_dir, "fourth", True)
+        git_test_fixture.git_initRepo(self.test_dir, "fourth", True)
 
         # fifth repo must not pre-exist
         self.assertFalse(os.path.exists(fifth_repo))
@@ -189,10 +175,10 @@ class GitVisitorBackendsTest(unittest.TestCase):
         self.assertFalse(v)
 
         # push to new remote
-        self.git_pushToRemote(self.first_repo, "origin", "master")
+        git_test_fixture.git_pushToRemote(self.first_repo, "origin", "master")
 
         # clone fourth into fifth to check for repo1's contents
-        self.git_cloneRepo(fourth_repo, fifth_repo, "origin")
+        git_test_fixture.git_cloneRepo(fourth_repo, fifth_repo, "origin")
         # file from repo1 must exist inside fifth repo
         self.assertTrue(os.path.exists( os.path.join(fifth_repo, self.first_repo_first_file) ))
 
