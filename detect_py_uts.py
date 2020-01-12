5bad97d0a65a11a42851eb3e7ccef75b3e4e6c1e
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-12 21:58:28 +0100

    introducing script to detect python unit test scripts in folders

---
+++ detect_py_uts.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import fsquery
+
+def puaq():
+    print("Usage: %s target_folder" % os.path.basename(__file__))
+    sys.exit(1)
+
+def get_content(file):
+    content = ""
+    with open(file) as f:
+        content = f.read()
+    return content
+
+def is_file_py_ut(path):
+
+    content = get_content(path)
+    if "unittest.TestCase" in content and "unittest.main()" in content:
+        return True
+    return False
+
+def detect_py_uts(path):
+
+    ret = []
+    qr = fsquery.makecontentlist(path, False, True, False, False, False, "py")
+
+    for f in qr:
+        if is_file_py_ut(f):
+            ret.append(f)
+
+    return ret
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    path = sys.argv[1]
+    r = detect_py_uts(path)
+    for e in r:
+        print(e)
