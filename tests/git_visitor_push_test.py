46a0ac5489331b943fb26f98f9e96ac6a021bf76
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-18 12:36:03 +0100

    porting git_visitor_push to py3, adding uts for it as well

---
+++ tests/git_visitor_push_test.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import git_test_fixture
+import mvtools_test_fixture
+
+import git_visitor_push
+
+class GitVisitorPushTest(unittest.TestCase):
+
+    def makeFilename(self):
+        self.internal_counter += 1
+        filename = "testfile_%s.txt" % self.internal_counter
+        return filename
+
+    def makeContent(self):
+        self.internal_counter += 1
+        content = "rubbish_content_%s" % self.internal_counter
+        return content
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        self.internal_counter = 0
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("git_visitor_push_test_base")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # test repos paths
+        self.first_repo = os.path.join(self.test_dir, "first")
+        self.second_repo = os.path.join(self.test_dir, "second")
+
+        # creates test repos
+        v, r = git_test_fixture.git_initRepo(self.test_dir, "first", True)
+        if not v:
+            return v, r
+
+        # creates test repos
+        v, r = git_test_fixture.git_initRepo(self.test_dir, "second", True)
+        if not v:
+            return v, r
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testVisitorPush(self):
+
+        # clone first and second into third and fourth, respectively
+        third_repo = os.path.join(self.test_dir, "third")
+        v, r = git_test_fixture.git_cloneRepo(self.first_repo, third_repo, "origin")
+        if not v:
+            self.fail(r)
+
+        fourth_repo = os.path.join(self.test_dir, "fourth")
+        v, r = git_test_fixture.git_cloneRepo(self.second_repo, fourth_repo, "origin")
+        if not v:
+            self.fail(r)
+
+        # commit new file onto third
+        file1 = self.makeFilename()
+        v, r = git_test_fixture.git_createAndCommit(third_repo, file1, self.makeContent(), "commit_msg")
+        if not v:
+            return v, r
+
+        # commit new file onto fourth
+        file2 = self.makeFilename()
+        v, r = git_test_fixture.git_createAndCommit(fourth_repo, file2, self.makeContent(), "commit_msg")
+        if not v:
+            return v, r
+
+        # call visitor_push on third and fourth
+        repos = [third_repo, fourth_repo]
+        opts = {"xor-remotename": "origin"}
+
+        r = git_visitor_push.visitor_push(repos, opts)
+
+        self.assertTrue(r)
+
+        # checkout fifth and sixth, from first and second, and check if newfiles were pushed (from 3rd and 4th)
+        fifth_repo = os.path.join(self.test_dir, "fifth")
+        fifth_file1 = os.path.join(fifth_repo, file1)
+        v, r = git_test_fixture.git_cloneRepo(self.first_repo, fifth_repo, "origin")
+        if not v:
+            self.fail(r)
+
+        sixth_repo = os.path.join(self.test_dir, "sixth")
+        sixth_file2 = os.path.join(sixth_repo, file2)
+        v, r = git_test_fixture.git_cloneRepo(self.second_repo, sixth_repo, "origin")
+        if not v:
+            self.fail(r)
+
+        self.assertTrue(os.path.exists(fifth_file1))
+        self.assertTrue(os.path.exists(sixth_file2))
+
+if __name__ == '__main__':
+    unittest.main()
