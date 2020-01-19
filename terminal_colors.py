3b22efa68f1162f0b87eabfe4c92db86567cc4a3
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-19 14:07:00 +0100

    add blue to terminal colors

--- terminal_colors.py
+++ terminal_colors.py
@@ -3,6 +3,7 @@
 TTY_WHITE = "\033[0m"
 TTY_RED = "\033[31m"
 TTY_GREEN = "\033[32m"
+TTY_BLUE = "\033[94m"
 
 def get_standard_color():
     return TTY_WHITE # mvtodo: eventually this should dynamically read the env - LS_COLORS, likely
