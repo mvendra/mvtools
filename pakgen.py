15a12be79ae31a662f5a383022643e0c4455279c
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-20 21:11:18 +0100

    updating pakgen to use hash_algos and generic_run, plus a few additional tidy-ups

--- pakgen.py
+++ pakgen.py
@@ -3,6 +3,10 @@
 import sys
 import os
 
+import generic_run
+import create_and_write_file
+import hash_algos
+
 def puaq():
     print("Usage: %s [--hash] file_or_folder" % os.path.basename(__file__))
     sys.exit(1)
@@ -26,14 +30,21 @@ def pakgen(files):
         TARGET=TARGET[0:len(TARGET)-1]
 
     FILENAME = os.path.basename(TARGET)
-    FILENAME = FILENAME.replace(" ", "\\ ")
-    FILENAME += ".tar"
     TARGET = TARGET.replace(" ", "\\ ")
+    FILENAME = FILENAME.replace(" ", "\\ ")
+    FILENAME_TAR = "%s.tar" % FILENAME
+    FILENAME_TAR_BZ2 = "%s.bz2" % FILENAME_TAR
+    HASH_FILENAME = "%s.sha256"  % FILENAME_TAR_BZ2
 
-    os.system("tar -cf %s %s" % (FILENAME, TARGET))
-    os.system("bzip2 %s" % FILENAME)
+    generic_run.run_cmd("tar -cf %s %s" % (FILENAME_TAR, TARGET))
+    generic_run.run_cmd("bzip2 %s" % FILENAME_TAR)
+
     if DO_HASH:
-        os.system("sha256sum %s.bz2 > %s.bz2.sha256sum" % (FILENAME, FILENAME))
+        v, r = hash_algos.hash_sha_256_app_file(FILENAME_TAR_BZ2)
+        if v:
+            create_and_write_file.create_file_contents(HASH_FILENAME, r)
+        else:
+            print("Failed generating hash for %s" % FILENAME_TAR_BZ2)
 
 if __name__ == "__main__":
 
