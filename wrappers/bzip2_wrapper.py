53da5b73a4b99ddabc7e444dae9b7f87d576b34d
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-30 21:45:14 +0100

    introducing bzip2_wrapper

---
+++ wrappers/bzip2_wrapper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import generic_run
+
+def compress(file_to_compress):
+
+    # file_to_compress: tar file to compress
+
+    # prechecks
+    if not os.path.exists(file_to_compress):
+        return False, "%s does not exist." % file_to_compress
+
+    # actual command
+    tar_cmd = "bzip2 %s" % file_to_compress
+    tar_cmd = tar_cmd.strip()
+
+    v, r = generic_run.run_cmd(tar_cmd)
+    return v, None
+
+def puaq():
+    print("Usage: %s file_to_compress.tar" % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    file_to_compress = sys.argv[1]
+    compress(file_to_compress)
