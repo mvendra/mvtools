34aa1e36d1b5f377e7ba4e1daec7c9046696fbb6
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-01-30 22:01:51 +0100

    introducing bunzip2_wrapper

---
+++ wrappers/bunzip2_wrapper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import generic_run
+
+def decompress(file_to_decompress):
+
+    # file_to_decompress: file to decompress
+
+    # prechecks
+    if not os.path.exists(file_to_decompress):
+        return False, "%s does not exist." % file_to_decompress
+
+    # actual command
+    cmd = "bunzip2 %s" % file_to_decompress
+    cmd = cmd.strip()
+
+    v, r = generic_run.run_cmd(cmd)
+    return v, None
+
+def puaq():
+    print("Usage: %s file_to_decompress.bz2" % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    file_to_decompress = sys.argv[1]
+    decompress(file_to_decompress)
