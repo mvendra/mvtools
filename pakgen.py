a74356a61c0c98bf81d4515b79941b7bda3290c9
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-02-01 16:58:57 +0100

    retrofitting tar_wrapper into pakgen - wip

--- pakgen.py
+++ pakgen.py
@@ -3,51 +3,86 @@
 import sys
 import os
 
-import generic_run
+import tar_wrapper
+import bzip2_wrapper
 import create_and_write_file
 import sha256_wrapper
 
-def puaq():
-    print("Usage: %s [--hash] file_or_folder" % os.path.basename(__file__))
-    sys.exit(1)
+def pakgen(filename, dohash, files):
 
-def pakgen(files):
+    for i in range(len(files)):
+        if not os.path.exists(i):
+            print("%s does not exist." % i)
+            return False
 
-    DO_HASH=False
-    TARGET=""
-
-    if files[0] == "--hash":
-        DO_HASH=True
-        TARGET=files[1]
-    else:
-        TARGET=files[0]
-
-    if not os.path.exists(TARGET):
-        print("Does %s even exist?" % TARGET)
-        sys.exit(1)
-
-    if TARGET[len(TARGET)-1] == os.sep:
-        TARGET=TARGET[0:len(TARGET)-1]
-
-    FILENAME = os.path.basename(TARGET)
-    TARGET = TARGET.replace(" ", "\\ ")
-    FILENAME = FILENAME.replace(" ", "\\ ")
-    FILENAME_TAR = "%s.tar" % FILENAME
+    FILENAME_TAR = "%s.tar" % filename
     FILENAME_TAR_BZ2 = "%s.bz2" % FILENAME_TAR
-    HASH_FILENAME = "%s.sha256"  % FILENAME_TAR_BZ2
 
-    generic_run.run_cmd("tar -cf %s %s" % (FILENAME_TAR, TARGET))
-    generic_run.run_cmd("bzip2 %s" % FILENAME_TAR)
+    if os.path.exists(FILENAME_TAR):
+        print("%s already exists." % FILENAME_TAR)
+        return False
 
-    if DO_HASH:
+    if os.path.exists(FILENAME_TAR_BZ2):
+        print("%s already exists." % FILENAME_TAR_BZ2)
+        return False
+
+    v, r = tar_wrapper.make_pack(FILENAME_TAR, files)
+    if not v:
+        print(r)
+        return False
+
+    v, r = bzip2_wrapper.compress(FILENAME_TAR)
+    if not v:
+        print(r)
+        return False
+
+    if dohash:
+        HASH_FILENAME = "%s.sha256"  % FILENAME_TAR_BZ2
+        if os.path.exists(HASH_FILENAME):
+            print("%s already exists." % HASH_FILENAME)
+            return False
         v, r = sha256_wrapper.hash_sha_256_app_file(FILENAME_TAR_BZ2)
         if v:
             create_and_write_file.create_file_contents(HASH_FILENAME, r)
         else:
             print("Failed generating hash for %s" % FILENAME_TAR_BZ2)
+            return False
+
+    return True
+
+def puaq():
+    print("Usage: %s [--file pack_filename] [--hash] (files_and_folders)" % os.path.basename(__file__))
+    sys.exit(1)
 
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         puaq()
-    pakgen(sys.argv[1:])
+
+    params = sys.argv[1:]
+    files = []
+    filename = None
+    dohash = False
+
+    capture_next = False
+    for i in params:
+        if capture_next:
+            capture_next = False
+            filename = i
+        elif i == "--hash":
+            dohash = True
+        elif i == "--file":
+            capture_next = True
+        else:
+            files.append(i)
+
+    if len(files) < 1:
+        puaq
+    elif len(files) == 1 and filename is None:
+        filename = os.path.basename(files[0])
+    elif filename is None:
+        filename = "newpack"
+
+    if not pakgen(filename, dohash, files):
+        print("Generating pak failed.")
+        sys.exit(1)
