1104beb676d9e393e0b2f5293f3c0d9cc9acd7e5
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-02-15 20:19:19 +0100

    introducing audio tool: quick convert flac file to mp3, using ffmpeg

---
+++ audio/flac_to_mp3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import generic_run
+
+def puaq():
+    print("Usage: %s input_file.flac" % os.path.basename(__file__))
+    sys.exit(1)
+
+def convert_flac_to_mp3(input_file, output_file, bitrate):
+    cmd = ["ffmpeg", "-i", input_file, "-ab", bitrate, "-map_metadata", "0", "-id3v2_version", "3", output_file]
+    v, r = generic_run.run_cmd_l(cmd)
+    if not v:
+        print("Failed to convert file from flac to mp3")
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    input_file = sys.argv[1]
+    output_file = ""
+
+    if len(sys.argv) == 3:
+        output_file = sys.argv[2]
+    else:
+        output_file = (input_file.rsplit(".", 1)[0]) + ".mp3"
+
+    bitrate = "192k"
+
+    convert_flac_to_mp3(input_file, output_file, bitrate)
