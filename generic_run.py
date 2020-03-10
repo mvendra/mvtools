fa7ed153df687830a7fa219c9876daa3bce8369e
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-03-10 20:54:13 +0100

    added optional encoding parameter when calling generic_run commands (asc and utf8)

--- generic_run.py
+++ generic_run.py
@@ -4,17 +4,23 @@ import sys
 import os
 import subprocess
 
-def run_cmd_l(cmd_list, std_input=None):
+def run_cmd_l(cmd_list, std_input=None, use_encoding="ascii"):
     try:
         if std_input is not None:
-            process = subprocess.run(cmd_list, check=False, input=std_input, stdout=subprocess.PIPE, encoding="ascii")
+            process = subprocess.run(cmd_list, check=False, input=std_input, stdout=subprocess.PIPE, encoding=use_encoding)
         else:
-            process = subprocess.run(cmd_list, check=False, stdout=subprocess.PIPE, encoding="ascii")
+            process = subprocess.run(cmd_list, check=False, stdout=subprocess.PIPE, encoding=use_encoding)
         if process.returncode == 0:
             return True, process.stdout
     except:
         pass
     return False, None
+
+def run_cmd_l_asc(cmd_list, std_input=None):
+    return run_cmd_l(cmd_list, std_input, "ascii")
+
+def run_cmd_l_utf8(cmd_list, std_input=None):
+    return run_cmd_l(cmd_list, std_input, "utf8")
 
 def run_cmd(cmd, std_input=None):
     cmd_list = []
