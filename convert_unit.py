ac459378878625bce125828695cda2654b753801
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-03-21 11:25:48 +0100

    separated convert_to_bytes into its own script (from backup_preparation) so it can be reused

---
+++ convert_unit.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+def convert_to_bytes(size_string):
+
+    try:
+        s_number = ""
+        s_unit = ""
+        multiplier = 1
+
+        if len(size_string) < 3:
+            return (True, int(size_string))
+
+        s_number = size_string[0:len(size_string)-2]
+        s_unit = size_string[len(size_string)-2:]
+        s_unit = s_unit.lower()
+
+        if s_unit == "kb":
+            multiplier = 1024
+        elif s_unit == "mb":
+            multiplier = 1024*1024
+        elif s_unit == "gb":
+            multiplier = 1024*1024*1024
+        elif s_unit == "tb":
+            multiplier = 1024*1024*1024*1024
+        else:
+            s_number = size_string
+
+        final_num = int(s_number) * multiplier
+        return (True, final_num)
+    except:
+        return (False, None)
+
+def puaq():
+    print("Usage: %s size_string" % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+    size_string = sys.argv[1]
+
+    v, r = convert_to_bytes(size_string)
+    if not v:
+        print("Failed converting %s" % size_string)
+        sys.exit(1)
+    print(r)
---
+++ tests/convert_unit_wrapper_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import mvtools_test_fixture
+
+import convert_unit
+
+class ConvertUnitTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("convert_unit_test_base")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testConvertToByteFail(self):
+        self.assertEqual(convert_unit.convert_to_bytes("1b"), (False, None))
+        self.assertEqual(convert_unit.convert_to_bytes("50bb"), (False, None))
+        self.assertEqual(convert_unit.convert_to_bytes("abc"), (False, None))
+        self.assertEqual(convert_unit.convert_to_bytes(""), (False, None))
+
+    def testConvertToByteSuccess(self):
+        self.assertEqual(convert_unit.convert_to_bytes("50"), (True, 50))
+        self.assertEqual(convert_unit.convert_to_bytes("1024"), (True, 1024))
+        self.assertEqual(convert_unit.convert_to_bytes("1kb"), (True, 1024))
+        self.assertEqual(convert_unit.convert_to_bytes("20kb"), (True, 20480))
+        self.assertEqual(convert_unit.convert_to_bytes("1mb"), (True, 1048576))
+        self.assertEqual(convert_unit.convert_to_bytes("20mb"), (True, 20971520))
+        self.assertEqual(convert_unit.convert_to_bytes("1gb"), (True, 1073741824))
+        self.assertEqual(convert_unit.convert_to_bytes("1tb"), (True, 1073741824*1024))
+
+if __name__ == '__main__':
+    unittest.main()
