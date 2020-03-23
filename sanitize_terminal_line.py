9abd764ab2643174a5a38946689983b7b92d2281
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-03-23 20:09:24 +0100

    introducing terminal line sanitizer

---
+++ sanitize_terminal_line.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+def sanitize_next(input_line):
+
+    input_line_local = input_line
+
+    p = input_line_local.find("\x1b")
+    if p == -1:
+        return False, None
+
+    n = input_line_local.find("m", p)
+    if n == -1:
+        return False, None
+
+    input_line_local = input_line_local[0:p] + input_line_local[n+1:]
+    return True, input_line_local
+
+def sanitize_terminal_line(input_line):
+
+    input_line_local = input_line
+    while True:
+        v, r = sanitize_next(input_line_local)
+        if not v:
+            break
+        else:
+            input_line_local = r
+
+    return input_line_local
+
+def puaq():
+    print("Usage: %s input_line" % os.path.basename(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+    input_lines = sys.argv[1:]
+    for i in input_lines:
+        print(sanitize_terminal_line(i))
---
+++ tests/sanitize_terminal_line_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import unittest
+
+import sanitize_terminal_line
+
+class SanitizeTerminalLineTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+        return True, ""
+
+    def tearDown(self):
+        pass
+
+    def testSanitizeLine(self):
+        self.assertEqual(sanitize_terminal_line.sanitize_terminal_line("\x1b[0m\x1b[01;34m__pycache__\x1b[0m/"), "__pycache__/")
+        self.assertEqual(sanitize_terminal_line.sanitize_terminal_line("\x1b[01;34m__pycache__\x1b[0m/"), "__pycache__/")
+        self.assertEqual(sanitize_terminal_line.sanitize_terminal_line("\x1b[01;34m__pycache__/"), "__pycache__/")
+        self.assertEqual(sanitize_terminal_line.sanitize_terminal_line("__pycache__/"), "__pycache__/")
+
+if __name__ == '__main__':
+    unittest.main()
