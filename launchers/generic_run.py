8f779d480147e952c2133c742362cd738b12b6cf
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-04-19 16:19:47 +0200

    first version of the new run_cmd + uts for it

--- launchers/generic_run.py
+++ launchers/generic_run.py
@@ -4,6 +4,32 @@ import sys
 import os
 import subprocess
 
+class run_cmd_result:
+    def __init__(self, _success, _returncode, _stdout, _stderr):
+        self.success = _success
+        self.returncode = _returncode
+        self.stdout = _stdout
+        self.stderr = _stderr
+
+def run_cmd(cmd_list, use_input=None, use_cwd=None, use_env=None, use_encoding="utf8"):
+
+    # return format: bool, string, run_cmd_result
+    # the first (bool) is the return of this API call only
+    # the second (string) is a message about the API call only
+    # the third (run_cmd_result) is the result of the called command itself
+
+    if not isinstance(cmd_list, list):
+        return False, "[%s] is not a list" % cmd_list, None
+    if len(cmd_list) == 0:
+        return False, "Nothing to run", None
+
+    try:
+        process = subprocess.run(cmd_list, input=use_input, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=use_cwd, check=False, encoding=use_encoding, env=use_env)
+        return True, "OK", run_cmd_result( (process.returncode==0), process.returncode, process.stdout, process.stderr )
+    except Exception as ex:
+        return False, str(ex), None
+    return False, "run_cmd NOTREACHED", None
+
 def run_cmd_l(cmd_list, std_input=None, use_cwd=None, use_encoding="utf8"):
     try:
         if std_input is not None:
---
+++ tests/generic_run_test.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import stat
+import shutil
+import unittest
+
+import path_utils
+import mvtools_test_fixture
+import create_and_write_file
+import generic_run
+
+class GenericRunTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("generic_run_test_base")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # where to store the test scripts
+        self.scripts_folder = path_utils.concat_path(self.test_dir, "scripts_folder")
+        os.mkdir(self.scripts_folder)
+
+        # secondary test folder, for testing using different cwds
+        self.secondary_folder = path_utils.concat_path(self.test_dir, "secondary_folder")
+        os.mkdir(self.secondary_folder)
+
+        # test file, not pre-created
+        self.file_test_filename = "test_file.txt"
+        self.file_test_content = "the test file contents"
+
+        self.reserved_test_env_var_1 = "MVTOOLS_TEST_GENERIC_RUN_ENVVAR_RESERVED_1"
+        if self.reserved_test_env_var_1 in os.environ:
+            return False, "Environment variable [%s] is in use. This test requires it to be undefined." % (self.reserved_test_env_var_1)
+
+        # first script
+        self.test_script_ret_0_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_ret_0_content += "import sys" + os.linesep
+        self.test_script_ret_0_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_ret_0_content += "    sys.exit(0)"
+
+        self.test_script_ret_0_filename = path_utils.concat_path(self.scripts_folder, "test_ret_0.py")
+        if not create_and_write_file.create_file_contents(self.test_script_ret_0_filename, self.test_script_ret_0_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_ret_0_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # second script
+        self.test_script_ret_1_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_ret_1_content += "import sys" + os.linesep
+        self.test_script_ret_1_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_ret_1_content += "    sys.exit(1)"
+
+        self.test_script_ret_1_filename = path_utils.concat_path(self.scripts_folder, "test_ret_1.py")
+        if not create_and_write_file.create_file_contents(self.test_script_ret_1_filename, self.test_script_ret_1_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_ret_1_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # third script
+        self.test_script_fail_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_fail_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_fail_content += "    print..." # malformed instruction
+
+        self.test_script_fail_filename = path_utils.concat_path(self.scripts_folder, "test_fail.py")
+        if not create_and_write_file.create_file_contents(self.test_script_fail_filename, self.test_script_fail_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_fail_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # fourth script
+        self.test_script_print_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_print_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_print_content += "    print(\"the test output\")"
+
+        self.test_script_print_filename = path_utils.concat_path(self.scripts_folder, "test_print.py")
+        if not create_and_write_file.create_file_contents(self.test_script_print_filename, self.test_script_print_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_print_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # fifth script
+        self.test_script_print_err_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_print_err_content += "import sys" + os.linesep
+        self.test_script_print_err_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_print_err_content += "    sys.stderr.write(\"the test error\")"
+
+        self.test_script_print_err_filename = path_utils.concat_path(self.scripts_folder, "test_print_err.py")
+        if not create_and_write_file.create_file_contents(self.test_script_print_err_filename, self.test_script_print_err_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_print_err_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # sixth script
+        self.test_script_print_input_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_print_input_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_print_input_content += "    p = input(\"asking for input\")" + os.linesep
+        self.test_script_print_input_content += "    print(\"echoing back: {%s}\" % p)"
+
+        self.test_script_print_input_filename = path_utils.concat_path(self.scripts_folder, "test_input_print.py")
+        if not create_and_write_file.create_file_contents(self.test_script_print_input_filename, self.test_script_print_input_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_print_input_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # seventh script
+        self.test_script_print_cmdlineargs_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_print_cmdlineargs_content += "import sys" + os.linesep
+        self.test_script_print_cmdlineargs_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_print_cmdlineargs_content += "    v = sys.argv[1]" + os.linesep
+        self.test_script_print_cmdlineargs_content += "    print(v)"
+
+        self.test_script_print_cmdlineargs_filename = path_utils.concat_path(self.scripts_folder, "test_print_cmdlineargs.py")
+        if not create_and_write_file.create_file_contents(self.test_script_print_cmdlineargs_filename, self.test_script_print_cmdlineargs_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_print_cmdlineargs_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # eight script
+        self.test_script_print_utf8_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_script_print_utf8_content += "import sys" + os.linesep
+        self.test_script_print_utf8_content += "if __name__ == '__main__':" + os.linesep
+        self.test_script_print_utf8_content += "    print(\"おはようございます\")" + os.linesep
+        self.test_script_print_utf8_content += "    sys.stderr.write(\"こんばんは\")"
+
+        self.test_script_print_utf8_filename = path_utils.concat_path(self.scripts_folder, "test_print_utf8.py")
+        if not create_and_write_file.create_file_contents(self.test_script_print_utf8_filename, self.test_script_print_utf8_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_script_print_utf8_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # ninth script
+        self.test_print_file_cwd_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_print_file_cwd_content += "if __name__ == '__main__':" + os.linesep
+        self.test_print_file_cwd_content += ("    fn = \"./%s\"" + os.linesep) % self.file_test_filename
+        self.test_print_file_cwd_content += "    with open(fn) as f:"  + os.linesep
+        self.test_print_file_cwd_content += "        print(f.read())"  + os.linesep
+
+        self.test_print_file_cwd_filename = path_utils.concat_path(self.scripts_folder, "test_print_file_cwd.py")
+        if not create_and_write_file.create_file_contents(self.test_print_file_cwd_filename, self.test_print_file_cwd_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_print_file_cwd_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        # tenth script
+        self.test_custom_env_content = "#!/usr/bin/env python3" + os.linesep
+        self.test_custom_env_content += "import os" + os.linesep
+        self.test_custom_env_content += "if __name__ == '__main__':" + os.linesep
+        self.test_custom_env_content += ("    print(os.environ[\"%s\"])" + os.linesep) % self.reserved_test_env_var_1
+
+        self.test_custom_env_filename = path_utils.concat_path(self.scripts_folder, "test_print_custom_env.py")
+        if not create_and_write_file.create_file_contents(self.test_custom_env_filename, self.test_custom_env_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+        os.chmod(self.test_custom_env_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testSanity(self):
+
+        ret = generic_run.run_cmd(None)
+        self.assertEqual(len(ret), 3)
+        self.assertFalse(ret[0])
+
+        ret = generic_run.run_cmd("")
+        self.assertEqual(len(ret), 3)
+        self.assertFalse(ret[0])
+
+        ret = generic_run.run_cmd([])
+        self.assertEqual(len(ret), 3)
+        self.assertFalse(ret[0])
+
+    def testBasic(self):
+
+        ret = generic_run.run_cmd(["echo", "ohce"])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertTrue("ohce" in cmd_ret.stdout)
+        self.assertEqual(cmd_ret.stderr, "")
+
+    def testBasicScriptRet0(self):
+
+        ret = generic_run.run_cmd([self.test_script_ret_0_filename])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertEqual(cmd_ret.stdout, "")
+        self.assertEqual(cmd_ret.stderr, "")
+
+    def testBasicScriptRet1(self):
+
+        ret = generic_run.run_cmd([self.test_script_ret_1_filename])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertFalse(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 1)
+        self.assertEqual(cmd_ret.stdout, "")
+        self.assertEqual(cmd_ret.stderr, "")
+
+    def testBasicScriptFail(self):
+
+        ret = generic_run.run_cmd([self.test_script_fail_filename])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertFalse(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 1)
+        self.assertEqual(cmd_ret.stdout, "")
+        self.assertTrue( "SyntaxError" in cmd_ret.stderr )
+
+    def testPrintStdout(self):
+
+        ret = generic_run.run_cmd([self.test_script_print_filename])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertEqual(cmd_ret.stdout, "the test output" + os.linesep)
+        self.assertEqual(cmd_ret.stderr, "")
+
+    def testPrintStderr(self):
+
+        ret = generic_run.run_cmd([self.test_script_print_err_filename])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertEqual(cmd_ret.stdout, "")
+        self.assertEqual(cmd_ret.stderr, "the test error")
+
+    def testPrintInput(self):
+
+        test_input = "input to be passed"
+
+        ret = generic_run.run_cmd([self.test_script_print_input_filename], test_input)
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertEqual(cmd_ret.stdout, ("asking for inputechoing back: {%s}" + os.linesep) % test_input)
+        self.assertEqual(cmd_ret.stderr, "")
+
+    def testPrintCmdlineargs(self):
+
+        test_input = "the cmdline argument"
+
+        ret = generic_run.run_cmd([self.test_script_print_cmdlineargs_filename, test_input])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertEqual(cmd_ret.stdout, test_input + os.linesep)
+        self.assertEqual(cmd_ret.stderr, "")
+
+    def testPrintUtf8(self):
+
+        ret = generic_run.run_cmd([self.test_script_print_utf8_filename])
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertEqual(cmd_ret.stdout, "おはようございます" + os.linesep)
+        self.assertEqual(cmd_ret.stderr, "こんばんは")
+
+    def testPrintFileCwd(self):
+
+        if not create_and_write_file.create_file_contents(path_utils.concat_path(self.secondary_folder, self.file_test_filename), self.file_test_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+
+        ret = generic_run.run_cmd([self.test_print_file_cwd_filename], use_cwd=self.secondary_folder)
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+        self.assertEqual(cmd_ret.stdout, self.file_test_content + os.linesep)
+        self.assertEqual(cmd_ret.stderr, "")
+
+    def testPrintFileCwdFail(self):
+
+        if not create_and_write_file.create_file_contents(path_utils.concat_path(self.scripts_folder, self.file_test_filename), self.file_test_content):
+            self.fail("create_and_write_file command failed. Can't proceed.")
+
+        ret = generic_run.run_cmd([self.test_print_file_cwd_filename], use_cwd=self.secondary_folder)
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertFalse(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 1)
+        self.assertEqual(cmd_ret.stdout, "")
+        self.assertTrue( "FileNotFoundError" in cmd_ret.stderr )
+
+    def testPrintCustomEnv(self):
+
+        custom_env_content = "test environment value"
+        custom_env = {}
+        custom_env[self.reserved_test_env_var_1] = custom_env_content
+
+        ret = generic_run.run_cmd([self.test_custom_env_filename], use_env=custom_env)
+        self.assertEqual(len(ret), 3)
+        self.assertTrue(ret[0])
+        self.assertTrue(ret[1], "OK")
+
+        cmd_ret = ret[2]
+        self.assertTrue(cmd_ret.success)
+        self.assertEqual(cmd_ret.returncode, 0)
+
+        self.assertEqual(cmd_ret.stdout, custom_env_content + os.linesep)
+        self.assertEqual(cmd_ret.stderr, "")
+
+if __name__ == '__main__':
+    unittest.main()
