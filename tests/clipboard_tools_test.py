3c75d096e767b80df5f77189daf980a452a88929
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-04-25 18:07:33 +0200

    new ut for getfromclipboard and sendtoclipboard (coalition/sanity check)

---
+++ tests/clipboard_tools_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import unittest
+
+import getfromclipboard
+import sendtoclipboard
+
+class ClipboardToolsTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testSanityCheck(self):
+
+        test_string = "parabola"
+        sendtoclipboard.sendtoclipboard(test_string)
+        self.assertEqual(getfromclipboard.getfromclipboard(), test_string)
+
+if __name__ == '__main__':
+    unittest.main()
