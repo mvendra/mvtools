a5b3ac4d291fabedbe93906d558523c6b76d4ef1
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-08-27 19:56:23 +0200

    introducing trylock - for locking files for writing

---
+++ trylock.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import fcntl
+
+def try_lock_file(file_handle):
+
+    try:
+        fcntl.lockf(file_handle, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        return False
+    return True
+
+def try_unlock_file(file_handle):
+
+    try:
+        fcntl.lockf(file_handle, fcntl.LOCK_UN)
+    except IOError:
+        return False
+    return True
