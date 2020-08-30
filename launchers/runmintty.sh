e3e63bd1aa6ff27a7d010b61c2c4d2cc95ebebca
Mateus Vendramini <mateusvendramini21@gmail.com>
2020-08-30 21:34:44 +0200

    updated the runmintty launchers to use toolbus

--- launchers/runmintty.sh
+++ launchers/runmintty.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 CMD=$1
-CMD_FINAL="runmintty_adapter.sh $CMD"
+TOOLBUS_SIGNAME="runmintty_result_${BASHPID}"
+CMD_FINAL="runmintty_adapter.sh $CMD $TOOLBUS_SIGNAME"
+
+# test if the toolbus signal is clear
+toolbus.py --get-signal $TOOLBUS_SIGNAME 1>/dev/null # under normal conditions, this results in an error message on stdout so we can suppress it
+TOOLBUS_GETSIG_RET=$?
+
+if [ $TOOLBUS_GETSIG_RET -eq 0 ]; then
+    echo "Unable to continue with the execution of [$CMD] - the Toolbus signal [$TOOLBUS_SIGNAME] is already in use." >&2
+    exit 1
+fi
 
 mintty.exe $CMD_FINAL
 
-LAST_RUN=$(cat ~/nuke/last_run) # mvtodo: in the future, should use the toolbus instead
-rm ~/nuke/last_run # mvtodo: in the future, should use the toolbus instead
-exit $LAST_RUN
+TOOLBUS_SIGVAL=$(toolbus.py --get-signal $TOOLBUS_SIGNAME)
+TOOLBUS_GETSIG_RET=$?
+
+if [ ! $TOOLBUS_GETSIG_RET -eq 0 ]; then
+    echo "Unable to get the result of the command [$CMD] - the Toolbus signal [$TOOLBUS_SIGNAME] can't be retrieved." >&2
+    exit 1
+fi
+
+exit $TOOLBUS_SIGVAL
--- launchers/runmintty_adapter.sh
+++ launchers/runmintty_adapter.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 CMD=$1
+TOOLBUS_SIGNAME=$2
 
-rm ~/nuke/last_run # mvtodo: in the future, should use the toolbus instead
 $CMD
-echo $? > ~/nuke/last_run # mvtodo: in the future, should use the toolbus instead
+RUN_RESULT=$?
+
+# send the result over to the caller via toolbus
+toolbus.py --set-signal $TOOLBUS_SIGNAME $RUN_RESULT
+TOOLBUS_SETSIG_RET=$?
+
+if [ ! $TOOLBUS_SETSIG_RET -eq 0 ]; then
+    echo "Unable to submit command result to Toolbus: the signal [$TOOLBUS_SIGNAME] is already in use." >&2
+    exit 1
+fi
