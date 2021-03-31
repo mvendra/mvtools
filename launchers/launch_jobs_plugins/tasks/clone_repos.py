70d6ec6bf63a7f02032539076c6ad6224e6fc346
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-03-31 20:13:49 +0200

    introducing: clone_repos plugin for recipe_processor

---
+++ launchers/launch_jobs_plugins/tasks/clone_repos.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import os
+
+import launch_jobs
+import fsquery
+import fsquery_adv_filter
+import path_utils
+import detect_repo_type
+import git_wrapper
+
+class CustomTask(launch_jobs.BaseTask):
+    def get_desc(self):
+        return "clone_repos"
+    def run_task(self, execution_name=None):
+
+        # read out required parameters
+        # source_path
+        try:
+            src_path = self.params["source_path"]
+        except KeyError:
+            return False, "clone_repos failed - source_path is a required parameter"
+
+        # dest_path
+        try:
+            dst_path = self.params["dest_path"]
+        except KeyError:
+            return False, "clone_repos failed - dest_path is a required parameter"
+
+        # accepted_repo_type
+        try:
+            accepted_repo_type = self.params["accepted_repo_type"]
+        except KeyError:
+            return False, "clone_repos failed - accepted_repo_type is a required parameter"
+
+        # bare_clone
+        try:
+            bare_clone_str = self.params["bare_clone"]
+        except KeyError:
+            return False, "clone_repos failed - bare_clone is a required parameter"
+        bare_clone = None
+        if bare_clone_str == "yes":
+            bare_clone = True
+        elif bare_clone_str == "no":
+            bare_clone = False
+        else:
+            return False, "clone_repos failed - bare_clone has an invalid value: [%s]. Accepted values are: yes/no." % bare_clone_str
+
+        # remote_name (only relevant if bare_clone is False)
+        if not bare_clone:
+            try:
+                remote_name = self.params["remote_name"]
+            except KeyError:
+                return False, "clone_repos failed - remote_name is a required parameter"
+
+        v, r = path_utils.check_if_paths_not_exist_stop_first([src_path, dst_path])
+        if not v:
+            return False, "clone_repos failed - path does not exist: [%s]" % r
+
+        items = []
+        items = fsquery.makecontentlist(src_path, True, False, True, False, True, True, None)
+
+        items_filtered_struct = []
+        items_filtered_struct_pre = []
+        filters_struct = []
+        filters_struct.append( (fsquery_adv_filter.filter_has_not_middle_repos, "not-used") )
+        filters_struct.append( (fsquery_adv_filter.filter_is_not_repo, "not-used") )
+
+        items_filtered_clone = []
+        filters_clone = []
+        filters_clone.append( (fsquery_adv_filter.filter_is_repo, "not-used") )
+
+        items_filtered_struct_pre = fsquery_adv_filter.filter_path_list_and(items, filters_struct)
+        items_filtered_clone = fsquery_adv_filter.filter_path_list_and(items, filters_clone)
+
+        for it in items_filtered_struct_pre:
+            middle_path_parts = path_utils.find_middle_path_parts(src_path, it)
+            target_struct = path_utils.concat_path(dst_path, middle_path_parts, os.path.basename(it))
+            items_filtered_struct.append(target_struct)
+
+        # check if any of the plain struct target folders already exist
+        v, r = path_utils.check_if_paths_exist_stop_first(items_filtered_struct)
+        if not v:
+            return False, "clone_repos failed - path already exists: [%s] (failed while creating plain folders structure)" % r
+
+        # create structure (plain folders only)
+        for it in items_filtered_struct:
+            os.mkdir(it)
+
+        items_tuple_final = []
+
+        # pre-assemble final source and target repo paths
+        for it in items:
+
+            v, r = detect_repo_type.detect_repo_type(it)
+            if not v:
+                return False, r
+            repotype_detected = r
+
+            if repotype_detected == accepted_repo_type:
+                middle_path_parts = path_utils.find_middle_path_parts(src_path, it)
+                if middle_path_parts is None:
+                    return False, "clone_repos failed - cannot find middle parts of path: [%s vs %s]" % (src_path, it)
+                target_repo = path_utils.concat_path( dst_path, middle_path_parts, os.path.basename(it) )
+                if os.path.exists(target_repo):
+                    return False, "clone_repos failed - target repo [%s] already exists." % target_repo
+                items_tuple_final.append( (it, target_repo) )
+
+        # create (clone) repos
+        for item_pair in items_tuple_final:
+            final_src = item_pair[0]
+            final_target = item_pair[1]
+            if bare_clone:
+                v, r = git_wrapper.clone_bare(final_src, final_target)
+                if not v:
+                    return False, "clone_repos failed - unable to bare-clone [%s] into [%s]." % (final_src, final_target)
+            else:
+                v, r = git_wrapper.clone(final_src, final_target, remote_name)
+                if not v:
+                    return False, "clone_repos failed - unable to clone [%s] into [%s]." % (final_src, final_target)
+
+        return True, None
---
+++ tests/clone_repos_test.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import create_and_write_file
+import mvtools_test_fixture
+import path_utils
+import git_wrapper
+
+import clone_repos
+
+class CloneReposTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("clone_repos_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        self.source_path = path_utils.concat_path(self.test_dir, "source_path")
+        self.dest_path = path_utils.concat_path(self.test_dir, "dest_path")
+        os.mkdir(self.source_path)
+        os.mkdir(self.dest_path)
+        if not os.path.exists(self.source_path):
+            return False, "Failed creating test folder (source_path): [%s]" % self.source_path
+        if not os.path.exists(self.dest_path):
+            return False, "Failed creating test folder (dest_path): [%s]" % self.dest_path
+
+        # first repo
+        self.first_repo = path_utils.concat_path(self.source_path, "first")
+        v, r = git_wrapper.init(self.source_path, "first", True)
+        if not v:
+            return v, r
+
+        # second repo
+        self.second_repo = path_utils.concat_path(self.source_path, "second")
+        v, r = git_wrapper.clone(self.first_repo, self.second_repo)
+        if not v:
+            return v, r
+        test_file = path_utils.concat_path(self.second_repo, "test_file.txt")
+        if not create_and_write_file.create_file_contents(test_file, "test-contents"):
+            return False, "Failed creating test file %s" % test_file
+        v, r = git_wrapper.stage(self.second_repo, [path_utils.basename_filtered(test_file)])
+        if not v:
+            return False, r
+        v, r = git_wrapper.commit(self.second_repo, "test commit msg1")
+        if not v:
+            return False, r
+        v, r = git_wrapper.push(self.second_repo, "origin", "master")
+        if not v:
+            return False, r
+
+        # third repo
+        self.source_sub1 = path_utils.concat_path(self.source_path, "sub1")
+        os.mkdir(self.source_sub1)
+        if not os.path.exists(self.source_sub1):
+            return False, "Failed creating test folder %s" % self.source_sub1
+        self.source_sub2 = path_utils.concat_path(self.source_sub1, "sub2")
+        os.mkdir(self.source_sub2)
+        if not os.path.exists(self.source_sub2):
+            return False, "Failed creating test folder %s" % self.source_sub2
+        self.third_repo = path_utils.concat_path(self.source_sub2, "   third   ")
+        v, r = git_wrapper.init(self.source_sub2, "   third   ", True)
+        if not v:
+            return v, r
+
+        # the test task
+        self.clone_repo_task = clone_repos.CustomTask()
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testCloneReposEmptyFail1(self):
+
+        local_params = {}
+        self.clone_repo_task.params = local_params
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testCloneReposEmptyFail2(self):
+
+        local_params = {}
+        local_params["source_path"] = self.source_path
+        self.clone_repo_task.params = local_params
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testCloneReposEmptyFail3(self):
+
+        local_params = {}
+        local_params["source_path"] = self.source_path
+        local_params["dest_path"] = self.dest_path
+        self.clone_repo_task.params = local_params
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testCloneReposEmptyFail4(self):
+
+        local_params = {}
+        local_params["source_path"] = self.source_path
+        local_params["dest_path"] = self.dest_path
+        local_params["accepted_repo_type"] = "git/bare"
+        self.clone_repo_task.params = local_params
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testCloneReposEmptyFail5(self):
+
+        local_params = {}
+        local_params["source_path"] = self.source_path
+        local_params["dest_path"] = self.dest_path
+        local_params["accepted_repo_type"] = "git/bare"
+        local_params["bare_clone"] = "no"
+        self.clone_repo_task.params = local_params
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testCloneReposVanilla1(self):
+
+        local_params = {}
+        local_params["source_path"] = self.source_path
+        local_params["dest_path"] = self.dest_path
+        local_params["accepted_repo_type"] = "git/bare"
+        local_params["bare_clone"] = "yes"
+        self.clone_repo_task.params = local_params
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertTrue(v)
+        dest_first_objects = path_utils.concat_path(self.dest_path, path_utils.basename_filtered(self.first_repo), "objects")
+        dest_third_objects = path_utils.concat_path(self.dest_path, path_utils.basename_filtered(self.source_sub1), path_utils.basename_filtered(self.source_sub2), path_utils.basename_filtered(self.third_repo), "objects")
+        self.assertTrue( os.path.exists(dest_first_objects) )
+        self.assertTrue( os.path.exists(dest_third_objects) )
+
+    def testCloneReposVanilla2(self):
+
+        local_params = {}
+        local_params["source_path"] = self.source_path
+        local_params["dest_path"] = self.dest_path
+        local_params["accepted_repo_type"] = "git/bare"
+        local_params["bare_clone"] = "no"
+        local_params["remote_name"] = "test_remote"
+        self.clone_repo_task.params = local_params
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertTrue(v)
+        test_file_first_repo = path_utils.concat_path(self.dest_path, path_utils.basename_filtered(self.first_repo), "test_file.txt")
+        print(test_file_first_repo)
+        self.assertTrue( os.path.exists(test_file_first_repo) )
+
+    def testCloneReposCheckFail1(self):
+
+        local_params = {}
+        local_params["source_path"] = self.source_path
+        local_params["dest_path"] = self.dest_path
+        local_params["accepted_repo_type"] = "git/bare"
+        local_params["bare_clone"] = "yes"
+        self.clone_repo_task.params = local_params
+
+        dest_third_sub1 = path_utils.concat_path(self.dest_path, path_utils.basename_filtered(self.source_sub1))
+        self.assertFalse( os.path.exists(dest_third_sub1))
+        os.mkdir(dest_third_sub1)
+        self.assertTrue( os.path.exists(dest_third_sub1))
+
+        v, r = self.clone_repo_task.run_task("exe_name")
+        self.assertFalse(v)
+
+if __name__ == '__main__':
+    unittest.main()
