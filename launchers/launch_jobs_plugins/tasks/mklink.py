cb4bc5849b33a422d9578062178e63f1d1895067
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-04-03 19:01:42 +0200

    introducing new launch_jobs plugin: mklink

---
+++ launchers/launch_jobs_plugins/tasks/mklink.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+
+import launch_jobs
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "mklink"
+
+    def run_task(self, execution_name=None):
+
+        # source_path
+        try:
+            source_path = self.params["source_path"]
+        except KeyError:
+            return False, "mklink failed - source_path is a required parameter"
+
+        # target_path
+        try:
+            target_path = self.params["target_path"]
+        except KeyError:
+            return False, "mklink failed - target_path is a required parameter"
+
+        # pre-checks
+        if not os.path.exists(source_path):
+            return False, "mkdir failed - source path [%s] does not exist" % source_path
+        if os.path.exists(target_path):
+            return False, "mkdir failed - target path [%s] already exists" % target_path
+
+        # actual execution
+        os.symlink(source_path, target_path)
+
+        # post-verification
+        if not os.path.exists(target_path):
+            return False, "mkdir failed - unable to create symlink from [%s] to [%s]" % (source_path, target_path)
+
+        # normal return
+        return True, None
---
+++ tests/mklink_test.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import mvtools_test_fixture
+
+import create_and_write_file
+import path_utils
+
+import mklink
+
+class MklinkTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("mklink_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        self.test_source_file = path_utils.concat_path(self.test_dir, "test_source_file")
+        if not create_and_write_file.create_file_contents(self.test_source_file, "test-contents"):
+            return False, "Unable to create test file [%s]" % self.test_source_file
+        if not os.path.exists(self.test_source_file):
+            return False, "Unable to create test file [%s]" % self.test_source_file
+
+        self.test_nonexistent_file = path_utils.concat_path(self.test_dir, "test_nonexistent_file")
+        if os.path.exists(self.test_nonexistent_file):
+            return False, "File [%s] already exists" % self.test_nonexistent_file
+
+        # the test task
+        self.mklink_task = mklink.CustomTask()
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testMklinkFail1(self):
+
+        local_params = {}
+        self.mklink_task.params = local_params
+
+        v, r = self.mklink_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testMklinkFail2(self):
+
+        local_params = {}
+        local_params["source_path"] = self.test_source_file
+        self.mklink_task.params = local_params
+
+        v, r = self.mklink_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testMklinkFail3(self):
+
+        local_params = {}
+        local_params["source_path"] = self.test_nonexistent_file
+        local_params["target_path"] = path_utils.concat_path(self.test_dir, "test_target_file")
+        self.mklink_task.params = local_params
+
+        v, r = self.mklink_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testMklinkFail4(self):
+
+        local_target_file = path_utils.concat_path(self.test_dir, "test_target_file")
+
+        local_params = {}
+        local_params["source_path"] = self.test_source_file
+        local_params["target_path"] = local_target_file
+        self.mklink_task.params = local_params
+
+        self.assertTrue(create_and_write_file.create_file_contents(local_target_file, "test-contents"))
+        self.assertTrue(os.path.exists(local_target_file))
+
+        v, r = self.mklink_task.run_task("exe_name")
+        self.assertFalse(v)
+
+    def testMklinkVanilla(self):
+
+        local_params = {}
+        local_params["source_path"] = self.test_source_file
+        local_params["target_path"] = path_utils.concat_path(self.test_dir, "test_target_file")
+        self.mklink_task.params = local_params
+
+        v, r = self.mklink_task.run_task("exe_name")
+        self.assertTrue(v)
+
+if __name__ == '__main__':
+    unittest.main()
