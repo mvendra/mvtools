fba80dc66373e9fddb0b61c5a7f7b996a8fbf8ea
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-04-19 20:05:21 +0200

    mklink - corrected typo

--- launchers/launch_jobs_plugins/tasks/mklink.py
+++ launchers/launch_jobs_plugins/tasks/mklink.py
@@ -25,19 +25,19 @@ class CustomTask(launch_jobs.BaseTask):
 
         # pre-checks
         if not os.path.exists(source_path):
-            return False, "mkdir failed - source path [%s] does not exist" % source_path
+            return False, "mklink failed - source path [%s] does not exist" % source_path
         if os.path.exists(target_path):
-            return False, "mkdir failed - target path [%s] already exists" % target_path
+            return False, "mklink failed - target path [%s] already exists" % target_path
 
         # actual execution
         try:
             os.symlink(source_path, target_path)
         except FileNotFoundError as ex:
-            return False, "mkdir failed - unable to create symlink from [%s] to [%s]" % (source_path, target_path)
+            return False, "mklink failed - unable to create symlink from [%s] to [%s]" % (source_path, target_path)
 
         # post-verification
         if not os.path.exists(target_path):
-            return False, "mkdir failed - unable to create symlink from [%s] to [%s]" % (source_path, target_path)
+            return False, "mklink failed - unable to create symlink from [%s] to [%s]" % (source_path, target_path)
 
         # normal return
         return True, None
