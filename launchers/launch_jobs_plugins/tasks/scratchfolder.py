bf8e8fa25b18bd9359ea4aee0f86b504bdb3180c
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-05-09 22:17:51 +0200

    new launch_jobs plugin: scratchfolder

---
+++ launchers/launch_jobs_plugins/tasks/scratchfolder.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+
+import path_utils
+import launch_jobs
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "scratchfolder"
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        try:
+            target_path = self.params["target_path"]
+        except KeyError:
+            return False, "scratchfolder failed - target_path is a required parameter"
+
+        r = path_utils.scratchfolder(target_path)
+        if not r:
+            return False, "scratchfolder for path [%s] failed" % target_path
+
+        return True, None
---
+++ tests/scratchfolder_test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import mvtools_test_fixture
+import path_utils
+
+import scratchfolder
+
+class ScratchfolderTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("scratchfolder_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        self.nonexistent = path_utils.concat_path(self.test_dir, "nonexistent")
+
+        self.test_target_folder = path_utils.concat_path(self.test_dir, "test_target_folder")
+        os.mkdir(self.test_target_folder)
+
+        # the test task
+        self.scratchfolder_task = scratchfolder.CustomTask()
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testScratchfolderFail1(self):
+
+        local_params = {}
+        self.scratchfolder_task.params = local_params
+
+        v, r = self.scratchfolder_task.run_task(print, "exe_name")
+        self.assertFalse(v)
+
+    def testScratchfolderVanilla1(self):
+
+        local_params = {}
+        local_params["target_path"] = self.nonexistent
+        self.scratchfolder_task.params = local_params
+
+        self.assertFalse(os.path.exists( self.nonexistent ))
+        v, r = self.scratchfolder_task.run_task(print, "exe_name")
+        self.assertTrue(v)
+        self.assertTrue(os.path.exists( self.nonexistent ))
+
+    def testScratchfolderVanilla2(self):
+
+        local_params = {}
+        local_params["target_path"] = self.test_target_folder
+        self.scratchfolder_task.params = local_params
+
+        self.assertTrue(os.path.exists( self.test_target_folder ))
+        v, r = self.scratchfolder_task.run_task(print, "exe_name")
+        self.assertTrue(v)
+        self.assertTrue(os.path.exists( self.test_target_folder ))
+
+if __name__ == '__main__':
+    unittest.main()
