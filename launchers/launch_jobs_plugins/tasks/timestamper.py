35d21ce6f2035fc2bcc2c2de8c0048142e1daa48
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-05-10 21:10:12 +0200

    introducing new launch_jobs plugin: the timestamper

---
+++ launchers/launch_jobs_plugins/tasks/timestamper.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+import time
+
+import launch_jobs
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "timestamper"
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        try:
+            target_filename = self.params["target_filename"]
+        except KeyError:
+            return False, "timestamper failed - target_filename is a required parameter"
+
+        message = None
+        try:
+            message = self.params["message"]
+        except KeyError:
+            pass # optional
+
+        if os.path.exists(target_filename):
+            return False, "timestamper failed - file [%s] already exists." % target_filename
+
+        timestamp_now = datetime.datetime.fromtimestamp(time.time()).strftime("%H:%M:%S - %d/%m/%Y")
+
+        full_contents = ""
+        full_contents += timestamp_now + os.linesep
+        if message is not None:
+            full_contents += message
+
+        with open(target_filename, "w") as f:
+            f.write(full_contents)
+
+        return True, None
---
+++ tests/timestamper_test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import mvtools_test_fixture
+import path_utils
+
+import timestamper
+
+class TimestamperTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("timestamper_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        self.test_file = path_utils.concat_path(self.test_dir, "test_file.txt")
+
+        # the test task
+        self.timestamper_task = timestamper.CustomTask()
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testTimestamperFail1(self):
+
+        local_params = {}
+        self.timestamper_task.params = local_params
+
+        v, r = self.timestamper_task.run_task(print, "exe_name")
+        self.assertFalse(v)
+
+    def testTimestamperFail2(self):
+
+        local_params = {}
+        local_params["target_filename"] = self.test_file
+        self.timestamper_task.params = local_params
+
+        # create dummy file
+        with open(self.test_file, "w") as f:
+            f.write("dummy")
+
+        v, r = self.timestamper_task.run_task(print, "exe_name")
+        self.assertFalse(v)
+
+    def testTimestamperVanilla(self):
+
+        local_params = {}
+        local_params["target_filename"] = self.test_file
+        self.timestamper_task.params = local_params
+
+        self.assertFalse(os.path.exists(self.test_file))
+        v, r = self.timestamper_task.run_task(print, "exe_name")
+        self.assertTrue(v)
+        self.assertTrue(os.path.exists(self.test_file))
+
+    def testTimestamperCustomMessage(self):
+
+        LOCAL_CUSTOM_MESSAGE = "sun dogs fire on the horizon"
+
+        local_params = {}
+        local_params["target_filename"] = self.test_file
+        local_params["message"] = LOCAL_CUSTOM_MESSAGE
+        self.timestamper_task.params = local_params
+
+        self.assertFalse(os.path.exists(self.test_file))
+        v, r = self.timestamper_task.run_task(print, "exe_name")
+        self.assertTrue(v)
+        self.assertTrue(os.path.exists(self.test_file))
+
+        read_contents = ""
+        with open(self.test_file, "r") as f:
+            read_contents = f.read()
+        self.assertTrue(LOCAL_CUSTOM_MESSAGE in read_contents)
+
+if __name__ == '__main__':
+    unittest.main()
