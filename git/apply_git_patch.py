3542b2cf51c0e4df4c502307f6b73d44c083f3c5
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-05-25 21:27:54 +0200

    apply_git_patch: scope reduced to accepting files as parameters only. a new module will be introduced to cover for porting patches between repos.

--- git/apply_git_patch.py
+++ git/apply_git_patch.py
@@ -4,10 +4,8 @@ import sys
 import os
 import shutil
 
-import mvtools_envvars
 import path_utils
 import detect_repo_type
-import collect_git_patch
 import git_lib
 
 def _test_repo_path(path):
@@ -19,115 +17,52 @@ def _test_repo_path(path):
         return False, "Path [%s] does not point to a supported repository." % path
     return True, None
 
-def apply_git_patch_stash(temp_path, source_repo, target_repo):
+def apply_git_patch_stash(target_repo, source_files):
 
-    stash_files = None
-    v, r = collect_git_patch.collect_git_patch_stash(source_repo, temp_path)
-    if not v:
-        return False, r
-    stash_files = r
-
-    for sf in reversed(stash_files):
+    for sf in source_files:
         v, r = git_lib.patch_as_stash(target_repo, sf, True, True)
         if not v:
             return False, r
 
     return True, None
 
-def apply_git_patch_previous(temp_path, source_repo, target_repo, previous_count):
+def apply_git_patch_staged(target_repo, source_files):
 
-    previous_files = None
-    v, r = collect_git_patch.collect_git_patch_previous(source_repo, temp_path, previous_count)
-    if not v:
-        return False, r
-    previous_files = r
-
-    # previous commits will be stacked up ontop of head - no autocommitting is available (on purpose)
-    for pf in reversed(previous_files):
-        v, r = git_lib.patch_as_head(target_repo, pf, True)
+    for sf in source_files:
+        v, r = git_lib.patch_as_staged(target_repo, sf, True)
         if not v:
             return False, r
 
     return True, None
 
-def apply_git_patch_staged(temp_path, source_repo, target_repo):
+def apply_git_patch_head(target_repo, source_files):
 
-    staged_file = None
-    v, r = collect_git_patch.collect_git_patch_staged(source_repo, temp_path)
-    if not v:
-        return False, r
-    staged_file = r
-
-    v, r = git_lib.patch_as_staged(target_repo, staged_file, True)
-    if not v:
-        return False, r
+    for sf in source_files:
+        v, r = git_lib.patch_as_head(target_repo, sf, True)
+        if not v:
+            return False, r
 
     return True, None
 
-def apply_git_patch_head(temp_path, source_repo, target_repo):
+def apply_git_patch_unversioned(target_repo, source_files):
 
-    head_file = None
-    v, r = collect_git_patch.collect_git_patch_head(source_repo, temp_path)
-    if not v:
-        return False, r
-    head_file = r
+    for sf in source_files:
 
-    v, r = git_lib.patch_as_head(target_repo, head_file, True)
-    if not v:
-        return False, r
+        sf_base = sf[0]
+        sf_file = sf[1]
+
+        if not path_utils.based_copy_to(sf_base, sf_file, target_repo):
+            return False, "Failed copying [%s] to [%s]" % (sf, target_repo)
 
     return True, None
 
-def apply_git_patch_unversioned(temp_path, source_repo, target_repo):
+def apply_git_patch(target_repo, head_patches, staged_patches, stash_patches, unversioned_patches):
 
-    unversioned_files = None
-    v, r = collect_git_patch.collect_git_patch_unversioned(source_repo, temp_path)
-    if not v:
-        return False, r
-    unversioned_files = r
-
-    combined_base = path_utils.concat_path(temp_path, source_repo, "unversioned")
-
-    for uf in unversioned_files:
-        if not path_utils.based_copy_to(combined_base, uf, target_repo):
-            return False, "Failed copying [%s] to [%s]" % (uf, target_repo)
-
-    return True, None
-
-def apply_git_patch(source_repo, target_repo, head, staged, stash, unversioned, previous):
-
-    v, r = mvtools_envvars.mvtools_envvar_read_temp_path()
-    if not v:
-        return False, r
-    temp_path = r
-    if not os.path.exists(temp_path):
-        return False, ["Can't apply patches. MVTOOLS_TEMP_PATH envvar is not defined or the path does not exist."]
-
-    temp_path_patches = path_utils.concat_path(temp_path, "temp_path_patches")
-    if os.path.exists(temp_path_patches):
-        return False, ["Can't apply patches. Temporary path [%s] already exists." % temp_path_patches]
-    os.mkdir(temp_path_patches)
-
-    v, r = _apply_git_patch_delegate(temp_path_patches, source_repo, target_repo, head, staged, stash, unversioned, previous)
-    shutil.rmtree(temp_path_patches)
-    return v, r
-
-def _apply_git_patch_delegate(temp_path, source_repo, target_repo, head, staged, stash, unversioned, previous):
-
-    source_repo = path_utils.filter_remove_trailing_sep(source_repo)
-    source_repo = os.path.abspath(source_repo)
     target_repo = path_utils.filter_remove_trailing_sep(target_repo)
     target_repo = os.path.abspath(target_repo)
 
-    if not os.path.exists(source_repo):
-        return False, ["Source repository %s does not exist" % source_repo]
-
     if not os.path.exists(target_repo):
         return False, ["Target repository %s does not exist" % target_repo]
-
-    v, r = _test_repo_path(source_repo)
-    if not v:
-        return False, r
 
     v, r = _test_repo_path(target_repo)
     if not v:
@@ -137,36 +72,29 @@ def _apply_git_patch_delegate(temp_path, source_repo, target_repo, head, staged,
     has_any_failed = False
 
     # stash
-    if stash:
-        v, r = apply_git_patch_stash(temp_path, source_repo, target_repo)
+    if len(stash_patches) > 0:
+        v, r = apply_git_patch_stash(target_repo, stash_patches)
         if not v:
             has_any_failed = True
             report.append("apply_git_patch_stash: [%s]" % r)
 
-    # previous
-    if previous > 0:
-        v, r = apply_git_patch_previous(temp_path, source_repo, target_repo, previous)
-        if not v:
-            has_any_failed = True
-            report.append("apply_git_patch_previous: [%s]" % r)
-
     # staged
-    if staged:
-        v, r = apply_git_patch_staged(temp_path, source_repo, target_repo)
+    if len(staged_patches) > 0:
+        v, r = apply_git_patch_staged(target_repo, staged_patches)
         if not v:
             has_any_failed = True
             report.append("apply_git_patch_staged: [%s]" % r)
 
     # head
-    if head:
-        v, r = apply_git_patch_head(temp_path, source_repo, target_repo)
+    if len(head_patches) > 0:
+        v, r = apply_git_patch_head(target_repo, head_patches)
         if not v:
             has_any_failed = True
             report.append("apply_git_patch_head: [%s]" % r)
 
     # unversioned
-    if unversioned:
-        v, r = apply_git_patch_unversioned(temp_path, source_repo, target_repo)
+    if len(unversioned_patches) > 0:
+        v, r = apply_git_patch_unversioned(target_repo, unversioned_patches)
         if not v:
             has_any_failed = True
             report.append("apply_git_patch_unversioned: [%s]" % r)
@@ -174,44 +102,66 @@ def _apply_git_patch_delegate(temp_path, source_repo, target_repo, head, staged,
     return (not has_any_failed), report
 
 def puaq():
-    print("Usage: %s source_repo target_repo [--head] [--staged] [--stash] [--unversioned] [--previous]" % os.path.basename(__file__))
+    print("Usage: %s target_repo [--head patch-file] [--staged patch-file] [--stash patch-file] [--unversioned file-base patch-file]" % os.path.basename(__file__))
     sys.exit(1)
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         puaq()
 
-    source_repo = sys.argv[1]
-    target_repo = sys.argv[2]
-    params = sys.argv[3:]
+    target_repo = sys.argv[1]
+    params = sys.argv[2:]
 
-    head = False
-    staged = False
-    stash = False
-    unversioned = False
-    previous = 0
-    previous_parse_next = False
+    head = []
+    staged = []
+    stash = []
+    unversioned = []
+
+    head_parse_next = False
+    staged_parse_next = False
+    stash_parse_next = False
+    unversioned_parse_next = 0
+    unversioned_base_buffer = None
 
     for p in params:
 
-        if previous_parse_next:
-            previous = int(p)
-            previous_parse_next = False
+        if head_parse_next:
+            head_parse_next = False
+            head.append(p)
+            continue
+
+        if staged_parse_next:
+            staged_parse_next = False
+            staged.append(p)
+            continue
+
+        if stash_parse_next:
+            stash_parse_next = False
+            stash.append(p)
+            continue
+
+        if unversioned_parse_next == 1:
+            unversioned_parse_next = 2
+            unversioned_base_buffer = p
+            continue
+
+        if unversioned_parse_next == 2:
+            unversioned_parse_next = 0
+            unversioned.append( (unversioned_base_buffer, p) )
+            unversioned_base_buffer = None
             continue
 
         if p == "--head":
-            head = True
+            head_parse_next = True
         elif p == "--staged":
-            staged = True
+            staged_parse_next = True
         elif p == "--stash":
-            stash = True
+            stash_parse_next = True
         elif p == "--unversioned":
-            unversioned = True
-        elif p == "--previous":
-            previous_parse_next = True
+            unversioned_parse_next = 1
 
-    v, r = apply_git_patch(source_repo, target_repo, head, staged, stash, unversioned, previous)
+    v, r = apply_git_patch(target_repo, head, staged, stash, unversioned)
     if not v:
         for i in r:
             print("Failed: %s" % i)
--- tests/apply_git_patch_test.py
+++ tests/apply_git_patch_test.py
@@ -12,6 +12,7 @@ import path_utils
 
 import git_wrapper
 import git_lib
+import collect_git_patch
 import apply_git_patch
 
 class ApplyGitPatchTest(unittest.TestCase):
@@ -73,42 +74,130 @@ class ApplyGitPatchTest(unittest.TestCase):
 
     def testApplyGitPatchBasicChecks(self):
 
-        v, r = apply_git_patch.apply_git_patch(self.nonexistent, self.second_repo, False, False, False, False, 0)
+        v, r = apply_git_patch.apply_git_patch(self.nonexistent, [], [], [], [])
         self.assertFalse(v)
 
-        v, r = apply_git_patch.apply_git_patch(self.first_repo, self.nonexistent, False, False, False, False, 0)
+        v, r = apply_git_patch.apply_git_patch(self.nonrepo, [], [], [], [])
         self.assertFalse(v)
 
-        v, r = apply_git_patch.apply_git_patch(self.nonrepo, self.second_repo, False, False, False, False, 0)
+    def testApplyGitApplyUnversionedFail(self):
+
+        first_sub = path_utils.concat_path(self.first_repo, "sub")
+        os.mkdir(first_sub)
+        self.assertTrue(os.path.exists(first_sub))
+        second_sub = path_utils.concat_path(self.second_repo, "sub")
+        os.mkdir(second_sub)
+        self.assertTrue(os.path.exists(second_sub))
+
+        first_sub_file4 = path_utils.concat_path(first_sub, "file4.txt")
+        self.assertTrue(create_and_write_file.create_file_contents(first_sub_file4, "first, sub, file4, contents"))
+        second_sub_file4 = path_utils.concat_path(second_sub, "file4.txt")
+        self.assertTrue(create_and_write_file.create_file_contents(second_sub_file4, "second, sub, file4, contents"))
+
+        first_file5 = path_utils.concat_path(self.first_repo, "file5.txt")
+        self.assertTrue(create_and_write_file.create_file_contents(first_file5, "first, file5, contents"))
+        second_file5 = path_utils.concat_path(self.second_repo, "file5.txt")
+        self.assertFalse( os.path.exists(second_file5) )
+
+        v, r = collect_git_patch.collect_git_patch_unversioned(self.first_repo, self.storage_path)
+        self.assertTrue(v)
+        generated_patches = r
+
+        unversioned_param = []
+        unversioned_base = path_utils.concat_path(self.storage_path, self.first_repo, "unversioned")
+        for gp in generated_patches:
+            unversioned_param.append( (unversioned_base, gp) )
+        v, r = apply_git_patch.apply_git_patch_unversioned(self.second_repo, unversioned_param)
         self.assertFalse(v)
 
-        v, r = apply_git_patch.apply_git_patch(self.first_repo, self.nonrepo, False, False, False, False, 0)
-        self.assertFalse(v)
+    def testApplyGitApplyUnversioned(self):
 
-        v, r = apply_git_patch.apply_git_patch(self.first_repo, self.second_repo, False, False, False, False, 0)
+        first_sub = path_utils.concat_path(self.first_repo, "sub")
+        os.mkdir(first_sub)
+        self.assertTrue(os.path.exists(first_sub))
+        second_sub = path_utils.concat_path(self.second_repo, "sub")
+        self.assertFalse(os.path.exists(second_sub))
+
+        first_sub_file4 = path_utils.concat_path(first_sub, "file4.txt")
+        self.assertTrue(create_and_write_file.create_file_contents(first_sub_file4, "first, sub, file4, contents"))
+        second_sub_file4 = path_utils.concat_path(second_sub, "file4.txt")
+        self.assertFalse(os.path.exists(second_sub_file4))
+
+        first_file5 = path_utils.concat_path(self.first_repo, "file5.txt")
+        self.assertTrue(create_and_write_file.create_file_contents(first_file5, "first, file5, contents"))
+        second_file5 = path_utils.concat_path(self.second_repo, "file5.txt")
+        self.assertFalse( os.path.exists(second_file5) )
+
+        v, r = collect_git_patch.collect_git_patch_unversioned(self.first_repo, self.storage_path)
+        self.assertTrue(v)
+        generated_patches = r
+
+        unversioned_param = []
+        unversioned_base = path_utils.concat_path(self.storage_path, self.first_repo, "unversioned")
+        for gp in generated_patches:
+            unversioned_param.append( (unversioned_base, gp) )
+        v, r = apply_git_patch.apply_git_patch_unversioned(self.second_repo, unversioned_param)
         self.assertTrue(v)
 
-    def testApplyGitPatchStashFail(self):
+        self.assertTrue(os.path.exists(second_sub))
+        self.assertTrue(os.path.exists(second_sub_file4))
+        self.assertTrue( os.path.exists(second_file5) )
+
+    def testApplyGitPatchHead(self):
+
+        second_file1 = path_utils.concat_path(self.second_repo, "file1.txt")
+        self.assertTrue(os.path.exists(second_file1))
+
+        with open(self.first_file1, "a") as f:
+            f.write("more stuff")
+
+        v, r = collect_git_patch.collect_git_patch_head(self.first_repo, self.storage_path)
+        self.assertTrue(v)
+        generated_patches = [r]
+
+        v, r = apply_git_patch.apply_git_patch_head(self.second_repo, generated_patches)
+        self.assertTrue(v)
+
+        contents = ""
+        with open(second_file1, "r") as f:
+            contents = f.read()
+        self.assertTrue("more stuff" in contents)
+
+    def testApplyGitPatchStaged(self):
+
+        second_file1 = path_utils.concat_path(self.second_repo, "file1.txt")
+        self.assertTrue(os.path.exists(second_file1))
+
+        with open(self.first_file1, "a") as f:
+            f.write("more stuff")
+
+        v, r = git_wrapper.stage(self.first_repo)
+        self.assertTrue(v)
+
+        v, r = collect_git_patch.collect_git_patch_staged(self.first_repo, self.storage_path)
+        self.assertTrue(v)
+        generated_patches = [r]
+
+        v, r = apply_git_patch.apply_git_patch_staged(self.second_repo, generated_patches)
+        self.assertTrue(v)
+
+        contents = ""
+        with open(second_file1, "r") as f:
+            contents = f.read()
+        self.assertTrue("more stuff" in contents)
+
+        sf = git_lib.get_staged_files(self.second_repo)
+        self.assertTrue(sf is not None)
+        self.assertTrue(second_file1 in sf)
+
+    def testApplyGitPatchStash(self):
+
+        with open(self.first_file1, "a") as f:
+            f.write("more stuff")
 
         v, r = git_lib.get_stash_list(self.first_repo)
         self.assertTrue(v)
         self.assertEqual(len(r), 0)
-
-        v, r = git_lib.get_stash_list(self.second_repo)
-        self.assertTrue(v)
-        self.assertEqual(len(r), 0)
-
-        v, r = apply_git_patch.apply_git_patch_stash(self.storage_path, self.first_repo, self.second_repo)
-        self.assertFalse(v)
-
-        v, r = git_lib.get_stash_list(self.second_repo)
-        self.assertTrue(v)
-        self.assertEqual(len(r), 0)
-
-    def testApplyGitPatchStash1(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
 
         v, r = git_wrapper.stash(self.first_repo)
         self.assertTrue(v)
@@ -121,329 +210,16 @@ class ApplyGitPatchTest(unittest.TestCase):
         self.assertTrue(v)
         self.assertEqual(len(r), 0)
 
-        v, r = apply_git_patch.apply_git_patch_stash(self.storage_path, self.first_repo, self.second_repo)
+        v, r = collect_git_patch.collect_git_patch_stash(self.first_repo, self.storage_path)
+        self.assertTrue(v)
+        generated_patches = r
+
+        v, r = apply_git_patch.apply_git_patch_stash(self.second_repo, generated_patches)
         self.assertTrue(v)
 
         v, r = git_lib.get_stash_list(self.second_repo)
         self.assertTrue(v)
         self.assertEqual(len(r), 1)
-
-    def testApplyGitPatchStash2(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
-
-        v, r = git_wrapper.stash(self.first_repo)
-        self.assertTrue(v)
-
-        with open(self.first_file2, "a") as f:
-            f.write("latest2")
-
-        v, r = git_wrapper.stash(self.first_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.get_stash_list(self.first_repo)
-        self.assertTrue(v)
-        self.assertEqual(len(r), 2)
-
-        v, r = git_lib.get_stash_list(self.second_repo)
-        self.assertTrue(v)
-        self.assertEqual(len(r), 0)
-
-        v, r = apply_git_patch.apply_git_patch_stash(self.storage_path, self.first_repo, self.second_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.get_stash_list(self.second_repo)
-        self.assertTrue(v)
-        self.assertEqual(len(r), 2)
-
-    def testApplyGitPatchPreviousFail1(self):
-
-        v, r = git_lib.is_head_clear(self.first_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_previous(self.storage_path, self.first_repo, self.second_repo, 4)
-        self.assertFalse(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-    def testApplyGitPatchPreviousFail2(self):
-
-        empty_repo = path_utils.concat_path(self.test_dir, "empty_repo")
-        v, r = git_wrapper.init(self.test_dir, "empty_repo", False)
-        self.assertTrue(v)
-
-        empty_repo_clone = path_utils.concat_path(self.test_dir, "empty_repo_clone")
-        v, r = git_wrapper.clone(empty_repo, empty_repo_clone, "origin")
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(empty_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = git_lib.is_head_clear(empty_repo_clone)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_previous(self.storage_path, empty_repo, empty_repo_clone, 1)
-        self.assertFalse(v)
-
-        v, r = git_lib.is_head_clear(empty_repo_clone)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-    def testApplyGitPatchPrevious1(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
-
-        v, r = git_wrapper.stage(self.first_repo)
-        self.assertTrue(v)
-
-        v, r = git_wrapper.commit(self.first_repo, "newmsg1")
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.first_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_previous(self.storage_path, self.first_repo, self.second_repo, 1)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-    def testApplyGitPatchPrevious2(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
-
-        v, r = git_wrapper.stage(self.first_repo)
-        self.assertTrue(v)
-
-        v, r = git_wrapper.commit(self.first_repo, "newmsg1")
-        self.assertTrue(v)
-
-        with open(self.first_file2, "a") as f:
-            f.write("latest2")
-
-        v, r = git_wrapper.stage(self.first_repo)
-        self.assertTrue(v)
-
-        v, r = git_wrapper.commit(self.first_repo, "newmsg2")
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.first_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_previous(self.storage_path, self.first_repo, self.second_repo, 2)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-    def testApplyGitPatchStagedFail(self):
-
-        v, r = git_lib.is_head_clear(self.first_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_staged(self.storage_path, self.first_repo, self.second_repo)
-        self.assertFalse(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-    def testApplyGitPatchStaged1(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
-
-        v, r = git_wrapper.stage(self.first_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_staged(self.storage_path, self.first_repo, self.second_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-    def testApplyGitPatchStaged2(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
-
-        v, r = git_wrapper.stage(self.first_repo)
-        self.assertTrue(v)
-
-        with open(self.first_file2, "a") as f:
-            f.write("latest2")
-
-        v, r = git_wrapper.stage(self.first_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_staged(self.storage_path, self.first_repo, self.second_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-    def testApplyGitPatchHeadFail(self):
-
-        v, r = git_lib.is_head_clear(self.first_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_staged(self.storage_path, self.first_repo, self.second_repo)
-        self.assertFalse(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-    def testApplyGitPatchHead1(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_head(self.storage_path, self.first_repo, self.second_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-    def testApplyGitPatchHead2(self):
-
-        with open(self.first_file1, "a") as f:
-            f.write("latest1")
-
-        with open(self.first_file2, "a") as f:
-            f.write("latest2")
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_head(self.storage_path, self.first_repo, self.second_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-    def testApplyGitPatchUnversionedFail(self):
-
-        first_file4 = path_utils.concat_path(self.first_repo, "file4.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(first_file4, "file4-contents"))
-
-        first_sub1 = path_utils.concat_path(self.first_repo, "sub1")
-        os.mkdir(first_sub1)
-
-        first_sub1_sub2 = path_utils.concat_path(first_sub1, "sub2")
-        os.mkdir(first_sub1_sub2)
-
-        first_sub1_sub2_file5 = path_utils.concat_path(first_sub1_sub2, "file5.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(first_sub1_sub2_file5, "file5-contents"))
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        second_file4 = path_utils.concat_path(self.second_repo, "file4.txt")
-        second_sub1 = path_utils.concat_path(self.second_repo, "sub1")
-        second_sub1_sub2 = path_utils.concat_path(second_sub1, "sub2")
-        second_sub1_sub2_file5 = path_utils.concat_path(second_sub1_sub2, "file5.txt")
-
-        self.assertTrue(create_and_write_file.create_file_contents(second_file4, "file4-contents-dupe"))
-
-        v, r = apply_git_patch.apply_git_patch_unversioned(self.storage_path, self.first_repo, self.second_repo)
-        self.assertFalse(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-        self.assertTrue(os.path.exists(second_file4))
-        self.assertFalse(os.path.exists(second_sub1))
-        self.assertFalse(os.path.exists(second_sub1_sub2))
-        self.assertFalse(os.path.exists(second_sub1_sub2_file5))
-
-    def testApplyGitPatchUnversioned(self):
-
-        first_file4 = path_utils.concat_path(self.first_repo, "file4.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(first_file4, "file4-contents"))
-
-        first_sub1 = path_utils.concat_path(self.first_repo, "sub1")
-        os.mkdir(first_sub1)
-
-        first_sub1_sub2 = path_utils.concat_path(first_sub1, "sub2")
-        os.mkdir(first_sub1_sub2)
-
-        first_sub1_sub2_file5 = path_utils.concat_path(first_sub1_sub2, "file5.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(first_sub1_sub2_file5, "file5-contents"))
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertTrue(r)
-
-        v, r = apply_git_patch.apply_git_patch_unversioned(self.storage_path, self.first_repo, self.second_repo)
-        self.assertTrue(v)
-
-        v, r = git_lib.is_head_clear(self.second_repo)
-        self.assertTrue(v)
-        self.assertFalse(r)
-
-        second_file4 = path_utils.concat_path(self.second_repo, "file4.txt")
-        second_sub1 = path_utils.concat_path(self.second_repo, "sub1")
-        second_sub1_sub2 = path_utils.concat_path(second_sub1, "sub2")
-        second_sub1_sub2_file5 = path_utils.concat_path(second_sub1_sub2, "file5.txt")
-
-        self.assertTrue(os.path.exists(second_file4))
-        self.assertTrue(os.path.exists(second_sub1))
-        self.assertTrue(os.path.exists(second_sub1_sub2))
-        self.assertTrue(os.path.exists(second_sub1_sub2_file5))
 
 if __name__ == '__main__':
     unittest.main()
