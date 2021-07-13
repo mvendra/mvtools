42dbf6fd1e29c414e515deb4db3be814ba77074d
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-07-13 19:24:36 +0200

    introducing: generic_run_plugin

---
+++ launchers/launch_jobs_plugins/tasks/generic_run_plugin.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import os
+
+import launch_jobs
+import path_utils
+
+import generic_run
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "generic_run"
+
+    def _read_params(self):
+
+        command = None
+        args = None
+
+        # command
+        try:
+            command = self.params["command"]
+        except KeyError:
+            return False, "command is a required parameter"
+
+        # args
+        try:
+            arg_read = self.params["arg"]
+            if isinstance(arg_read, list):
+                args = arg_read
+            else:
+                args = [arg_read]
+        except KeyError:
+            pass # optional
+
+        return True, (command, args)
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        v, r = self._read_params()
+        if not v:
+            return False, r
+        command, args = r
+
+        final_cmd = []
+        final_cmd.append(command)
+
+        if args is not None:
+            for a in args:
+                final_cmd.append(a)
+
+        v, r = generic_run.run_cmd_simple(final_cmd)
+        if not v:
+            return False, "Running command [%s] failed: [%s]" % (final_cmd, r)
+
+        return True, None
---
+++ tests/generic_run_plugin_test.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+from unittest import mock
+from unittest.mock import patch
+
+import mvtools_test_fixture
+import create_and_write_file
+import path_utils
+
+import generic_run_plugin
+
+class GenericRunPluginTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("generic_run_plugin_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # the test task
+        self.generic_run_task = generic_run_plugin.CustomTask()
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testGenericRunPluginReadParams1(self):
+
+        local_params = {}
+        self.generic_run_task.params = local_params
+
+        v, r = self.generic_run_task._read_params()
+        self.assertFalse(v)
+
+    def testGenericRunPluginReadParams2(self):
+
+        local_params = {}
+        local_params["command"] = "dummy_value1"
+        self.generic_run_task.params = local_params
+
+        v, r = self.generic_run_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, ("dummy_value1", None) )
+
+    def testGenericRunPluginReadParams3(self):
+
+        local_params = {}
+        local_params["command"] = "dummy_value1"
+        local_params["arg"] = "dummy_value2"
+        self.generic_run_task.params = local_params
+
+        v, r = self.generic_run_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, ("dummy_value1", ["dummy_value2"]) )
+
+    def testGenericRunPluginReadParams4(self):
+
+        local_params = {}
+        local_params["command"] = "dummy_value1"
+        local_params["arg"] = ["dummy_value2", "dummy_value3"]
+        self.generic_run_task.params = local_params
+
+        v, r = self.generic_run_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, ("dummy_value1", ["dummy_value2", "dummy_value3"]) )
+
+    def testGenericRunPluginRunTask1(self):
+
+        local_params = {}
+        local_params["command"] = "dummy_value1"
+        local_params["arg"] = "dummy_value2"
+        self.generic_run_task.params = local_params
+
+        with mock.patch("generic_run.run_cmd_simple", return_value=(True, None)) as dummy:
+            v, r = self.generic_run_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(["dummy_value1", "dummy_value2"])
+
+    def testGenericRunPluginRunTask2(self):
+
+        local_params = {}
+        local_params["command"] = "dummy_value1"
+        local_params["arg"] = ["dummy_value2", "dummy_value3"]
+        self.generic_run_task.params = local_params
+
+        with mock.patch("generic_run.run_cmd_simple", return_value=(True, None)) as dummy:
+            v, r = self.generic_run_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(["dummy_value1", "dummy_value2", "dummy_value3"])
+
+    def testGenericRunPluginRunTask3(self):
+
+        local_params = {}
+        local_params["command"] = "dummy_value1"
+        local_params["arg"] = "dummy_value2"
+        self.generic_run_task.params = local_params
+
+        with mock.patch("generic_run.run_cmd_simple", return_value=(False, None)) as dummy:
+            v, r = self.generic_run_task.run_task(print, "exe_name")
+            self.assertFalse(v)
+
+if __name__ == '__main__':
+    unittest.main()
