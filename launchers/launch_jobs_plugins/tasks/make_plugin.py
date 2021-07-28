d2afa1c266f0a9a9dec6925368cc256aff01d8e8
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-07-28 19:52:52 +0200

    make_plugin: new option: save_output, to save make's output to a file

--- launchers/launch_jobs_plugins/tasks/make_plugin.py
+++ launchers/launch_jobs_plugins/tasks/make_plugin.py
@@ -12,6 +12,11 @@ class CustomTask(launch_jobs.BaseTask):
 
     def run_task(self, feedback_object, execution_name=None):
 
+        work_dir = None
+        target = None
+        save_output = None
+        suppress_make_output = False
+
         # work_dir
         try:
             work_dir = self.params["work_dir"]
@@ -19,22 +24,35 @@ class CustomTask(launch_jobs.BaseTask):
             return False, "make failed - work_dir is a required parameter"
 
         # target
-        target = None
         try:
             target = self.params["target"]
         except KeyError:
             pass # optional
 
         # suppress_make_output
-        suppress_make_output = False
         if "suppress_make_output" in self.params:
             suppress_make_output = True
+
+        # save_output
+        try:
+            save_output = self.params["save_output"]
+        except KeyError:
+            pass # optional
 
         # pre-validate parameters
         if not os.path.exists(work_dir):
             return False, "make failed - work_dir [%s] does not exist." % work_dir
 
+        # save_output
+        if save_output is not None:
+            if os.path.exists(save_output):
+                return False, "make_plugin: save_output [%s] points to a preexisting path" % save_output
+
         v, r = make_wrapper.make(work_dir, suppress_make_output, target)
+        if save_output is not None:
+            with open(save_output, "w") as f:
+                f.write(r)
+            feedback_object("Make's output has been saved to: [%s]" % save_output)
         if r is not None and suppress_make_output:
             r = "make's output was suppressed"
         return v, r
