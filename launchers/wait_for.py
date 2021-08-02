b41629b325ca874eb539c092a870d54f52918a21
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-08-02 19:00:56 +0200

    introducing: wait_for

---
+++ launchers/wait_for.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import path_utils
+import minicron
+
+def wait_for(time_delay_string):
+
+    wait_duration = minicron.convert_time_string(time_delay_string)
+    if wait_duration is None:
+        return False, "Invalid time duration: [%s]" % time_delay_string
+
+    if not minicron.busy_wait(wait_duration):
+        return False, "Requested delay of [%s] failed to be performed." % (time_delay_string)
+
+    return True, None
+
+def puaq():
+    print("Usage: %s time_delay_string" % path_utils.basename_filtered(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        puaq()
+
+    time_delay_string = sys.argv[1]
+    v, r = wait_for(time_delay_string)
+    if not v:
+        print(r)
+    else:
+        print("Done")
