a5ad69031d7a4ebd0e088ce9e8d91043fd271a9e
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-10-13 17:45:10 +0200

    introducing: print_svn_repo_url

---
+++ svn/print_svn_repo_url.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import svn_lib
+
+def find_and_print_svn_repo_remote_link(local_repo):
+
+    v, r = svn_lib.get_remote_link(local_repo)
+    if not v:
+        print("Failed: [%s]" % r)
+    print(r)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        local_repo = os.getcwd()
+    else:
+        local_repo = sys.argv[1]
+
+    find_and_print_svn_repo_remote_link(local_repo)
