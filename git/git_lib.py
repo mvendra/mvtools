ae28789c52bf6e925cb8ecad74e0de0b81f01634
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-10-16 21:12:03 +0200

    git_lib: new function: stash_drop

--- git/git_lib.py
+++ git/git_lib.py
@@ -658,6 +658,12 @@ def stash_clear(repo):
     repo = os.path.abspath(repo)
     return git_wrapper.stash_clear(repo)
 
+def stash_drop(repo):
+    if repo is None:
+        return False, "No repo specified"
+    repo = os.path.abspath(repo)
+    return git_wrapper.stash_drop(repo)
+
 def show(repo, commit_id):
     if repo is None:
         return False, "No repo specified"
--- tests/git_lib_test.py
+++ tests/git_lib_test.py
@@ -3329,6 +3329,33 @@ class GitLibTest(unittest.TestCase):
         finally:
             os.chdir(saved_wd)
 
+    def testStashDrop(self):
+
+        with mock.patch("git_wrapper.stash_drop", return_value=(True, None)) as dummy:
+            v, r = git_lib.stash_drop(self.first_repo)
+            self.assertTrue(v)
+            self.assertEqual(r, None)
+            dummy.assert_called_with(self.first_repo)
+
+        with mock.patch("git_wrapper.stash_drop", return_value=(True, None)) as dummy:
+            v, r = git_lib.stash_drop(None)
+            self.assertFalse(v)
+            dummy.assert_not_called()
+
+        saved_wd = os.getcwd()
+        try:
+            os.chdir(self.test_dir)
+
+            first_rel_path = path_utils.concat_path("./", os.path.basename(self.first_repo))
+            with mock.patch("git_wrapper.stash_drop", return_value=(True, None)) as dummy:
+                v, r = git_lib.stash_drop(first_rel_path)
+                self.assertTrue(v)
+                self.assertEqual(r, None)
+                dummy.assert_called_with(self.first_repo)
+
+        finally:
+            os.chdir(saved_wd)
+
     def testRemoteChangeUrl(self):
 
         with mock.patch("git_wrapper.remote_change_url", return_value=(True, None)) as dummy:
