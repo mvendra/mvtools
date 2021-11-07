4cedb1fd23a2c8ec07fdda419c239bd0d9efd996
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-11-07 15:46:57 +0100

    git_sub_pull: updated after fsquery api change

--- git/visitor/git_sub_pull.py
+++ git/visitor/git_sub_pull.py
@@ -20,8 +20,8 @@ def filter_sub_files(sub_candidates):
 
 def pull_subs(path):
 
-    subs = fsquery.makecontentlist(path, True, False, False, False, True, False, True, None)
-    subs = filter_sub_files(subs)
+    v, r = fsquery.makecontentlist(path, True, False, False, False, True, False, True, None)
+    subs = filter_sub_files(r)
 
     report = []
     anyfailed = False
