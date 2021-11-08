1d11a515563b6cbdfc73197f7f600311315c3c7c
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-11-08 20:59:32 +0100

    shred_wrapper: extra ut coverage (blank filenames, broken symlinks)

--- tests/shred_wrapper_test.py
+++ tests/shred_wrapper_test.py
@@ -47,21 +47,75 @@ class ShredWrapperTest(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.test_base_dir)
 
-    def testShredFile(self):
+    def testShredFile1(self):
         self.assertTrue(os.path.exists(self.file3))
-        shred_wrapper.shred_target(self.file3)
+        v, r = shred_wrapper.shred_target(self.file3)
+        self.assertTrue(v)
         self.assertFalse(os.path.exists(self.file3))
 
-    def testShredFolder(self):
+    def testShredFile2(self):
+
+        blanksub = path_utils.concat_path(self.test_dir, " ")
+        self.assertFalse(os.path.exists(blanksub))
+        os.mkdir(blanksub)
+        self.assertTrue(os.path.exists(blanksub))
+
+        blanksub_blankfile = path_utils.concat_path(blanksub, " ")
+        self.assertFalse(os.path.exists(blanksub_blankfile))
+        create_and_write_file.create_file_contents(blanksub_blankfile, "abc")
+        self.assertTrue(os.path.exists(blanksub_blankfile))
+
+        v, r = shred_wrapper.shred_target(blanksub_blankfile)
+        self.assertTrue(v)
+        self.assertFalse(os.path.exists(blanksub_blankfile))
+
+    def testShredFile3(self):
+
+        test_source_file = path_utils.concat_path(self.test_dir, "source_file.txt")
+        self.assertFalse(os.path.exists(test_source_file))
+        create_and_write_file.create_file_contents(test_source_file, "abc")
+        self.assertTrue(os.path.exists(test_source_file))
+
+        test_source_link = path_utils.concat_path(self.test_dir, "source_link.txt")
+        self.assertFalse(os.path.exists(test_source_link))
+        os.symlink(test_source_file, test_source_link)
+        self.assertTrue(os.path.exists(test_source_link))
+
+        os.unlink(test_source_file)
+        self.assertFalse(os.path.exists(test_source_file))
+        self.assertTrue(path_utils.is_path_broken_symlink(test_source_link))
+
+        v, r = shred_wrapper.shred_target(test_source_link)
+        self.assertFalse(v) # shred (version 8.30, of L21) does not accept broken symlinks
+
+    def testShredFolder1(self):
         self.assertTrue(os.path.exists(self.folder1))
         self.assertTrue(os.path.exists(self.folder1_file1))
         self.assertTrue(os.path.exists(self.folder2))
         self.assertTrue(os.path.exists(self.folder2_file2))
-        shred_wrapper.shred_target(self.folder1)
+        v, r = shred_wrapper.shred_target(self.folder1)
+        self.assertTrue(v)
         self.assertFalse(os.path.exists(self.folder1))
         self.assertFalse(os.path.exists(self.folder1_file1))
         self.assertFalse(os.path.exists(self.folder2))
         self.assertFalse(os.path.exists(self.folder2_file2))
+
+    def testShredFolder2(self):
+
+        blanksub = path_utils.concat_path(self.test_dir, " ")
+        self.assertFalse(os.path.exists(blanksub))
+        os.mkdir(blanksub)
+        self.assertTrue(os.path.exists(blanksub))
+
+        blanksub_blankfile = path_utils.concat_path(blanksub, " ")
+        self.assertFalse(os.path.exists(blanksub_blankfile))
+        create_and_write_file.create_file_contents(blanksub_blankfile, "abc")
+        self.assertTrue(os.path.exists(blanksub_blankfile))
+
+        v, r = shred_wrapper.shred_target(blanksub)
+        self.assertTrue(v)
+        self.assertFalse(os.path.exists(blanksub_blankfile))
+        self.assertFalse(os.path.exists(blanksub))
 
 if __name__ == '__main__':
     unittest.main()
