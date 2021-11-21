37dd9b8887d9ee484b9865dce972fbeec22caa0a
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-11-21 20:28:26 +0100

    deprecated: randomfilenamegen.sh. introduced: randomfilenamegen.py

---
+++ randomfilenamegen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+from random import randrange
+
+import path_utils
+import maketimestamp
+import sha256_wrapper
+
+def randomfilenamegen():
+
+    timestamp_now = maketimestamp.get_timestamp_now_compact()
+
+    random_number = randrange(100000000)
+    v, r = sha256_wrapper.hash_sha_256_app_content(str(random_number))
+    if not v:
+        return None
+    hashed_random_number = r
+
+    final_fn = "%s_%s.tmp" % (timestamp_now, hashed_random_number)
+    return final_fn
+
+if __name__ == "__main__":
+
+    random_fn = randomfilenamegen()
+    print(random_fn)
