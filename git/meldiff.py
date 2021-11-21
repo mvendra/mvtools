157384fec91cc6aede793affb42fc32842bddcae
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-11-21 21:39:45 +0100

    meldiff: removed usage of subprocess.call

--- git/meldiff.py
+++ git/meldiff.py
@@ -3,11 +3,13 @@
 import sys
 import os
 
-from subprocess import call
+import generic_run
 
 if __name__ == "__main__":
     if (len(sys.argv) < 8):
         print("This script is not intended for standalone use - integrate with meld instead.")
         sys.exit(1)
-    call("meld %s %s" % (sys.argv[2], sys.argv[5]), shell=True)
-
+    v, r = generic_run.run_cmd_simple(["meld", sys.argv[2], sys.argv[5]])
+    if not v:
+        print(r)
+        sys.exit(1)
