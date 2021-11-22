90e67c0b4bc7c843a8a42b6e54d80c5b915074c3
Mateus Vendramini <mateusvendramini21@gmail.com>
2021-11-22 11:46:41 +0100

    sendtoclipboard: small improvements

--- sendtoclipboard.py
+++ sendtoclipboard.py
@@ -7,19 +7,15 @@ from subprocess import call
 import get_platform
 import path_utils
 
-def puaq():
-    print("Usage: %s contents" % path_utils.basename_filtered(__file__))
-    sys.exit(1)
-
 def sendtoclipboard(contents):
 
-    clipboard_app = ""
     plat = get_platform.getplat()
 
     if plat == get_platform.PLAT_CYGWIN:
         call("inline_echo.py '%s' > /dev/clipboard" % (contents), shell=True)
-        return
+        return False
 
+    clipboard_app = ""
     if plat == get_platform.PLAT_LINUX:
         clipboard_app = "xclip -sel clip" # for linux
     elif plat == get_platform.PLAT_WINDOWS:
@@ -33,14 +29,14 @@ def sendtoclipboard(contents):
     call("inline_echo.py '%s' | %s" % (contents, clipboard_app), shell=True)
     return True
 
+def puaq():
+    print("Usage: %s contents" % path_utils.basename_filtered(__file__))
+    sys.exit(1)
+
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         puaq()
 
-    contents = ""
-    for c in sys.argv[1:]:
-        contents += c + " "
-    contents = contents.strip()
-
-    sendtoclipboard(contents)
+    if not sendtoclipboard(sys.argv[1]):
+        sys.exit(1)
