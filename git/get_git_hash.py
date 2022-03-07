d2f3f4d3bac940f4c32cb94e2059f05aee2c5b85
Mateus Vendramini <mateusvendramini21@gmail.com>
2022-03-07 20:21:47 +0100

    introducing get_git_hash: new applet for probing the head hash

---
+++ git/get_git_hash.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import path_utils
+import git_lib
+
+def get_git_hash(target_repo):
+    return git_lib.get_head_hash(target_repo)
+
+if __name__ == "__main__":
+
+    target_repo = os.getcwd()
+    v, r = get_git_hash(target_repo)
+    print(r)
+    if not v:
+        sys.exit(1)
