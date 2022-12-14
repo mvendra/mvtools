631db5ae16bf23c5ef93aed4f5ff7e2e2cb90304
Mateus Vendramini <mateusvendramini21@gmail.com>
2022-12-14 21:19:50 +0100

    msvc_proj_gen: updated msvc15 generation (c and cpp) to harness the common_gen refactors

--- codegen/msvc_proj_gen.py
+++ codegen/msvc_proj_gen.py
@@ -476,29 +476,7 @@ def generate_msvc15_c(target_dir, project_name):
     # base folders / base structure
     prj_fullname_base = path_utils.concat_path(target_dir, project_name)
     base_prj = path_utils.concat_path(prj_fullname_base, "proj")
-
-    base_build = path_utils.concat_path(prj_fullname_base, "build")
-    base_build_windows_x64_debug = path_utils.concat_path(base_build, "windows_x64_debug")
-    base_build_windows_x64_release = path_utils.concat_path(base_build, "windows_x64_release")
-
-    base_run = path_utils.concat_path(prj_fullname_base, "run")
-    base_run_windows_x64_debug = path_utils.concat_path(base_run, "windows_x64_debug")
-    base_run_windows_x64_release = path_utils.concat_path(base_run, "windows_x64_release")
-
     base_src = path_utils.concat_path(prj_fullname_base, "src")
-
-    prjboot_util.makedir_if_needed(prj_fullname_base)
-    prjboot_util.makedir_if_needed(base_prj)
-
-    prjboot_util.makedir_if_needed(base_build)
-    prjboot_util.makedir_if_needed(base_build_windows_x64_debug)
-    prjboot_util.makedir_if_needed(base_build_windows_x64_release)
-
-    prjboot_util.makedir_if_needed(base_run)
-    prjboot_util.makedir_if_needed(base_run_windows_x64_debug)
-    prjboot_util.makedir_if_needed(base_run_windows_x64_release)
-
-    prjboot_util.makedir_if_needed(base_src)
 
     # generate the actual Visual Studio project and solution files
     project_hex_id = msvc15_prj_hex_id()
@@ -512,8 +490,6 @@ def generate_msvc15_c(target_dir, project_name):
 
     # gitignore
     gitignore_filename = path_utils.concat_path(prj_fullname_base, ".gitignore")
-    prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "build/")
-    prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "run/")
     prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "proj/msvc15_c/.vs")
     prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "proj/msvc15_c/%s.VC.db" % project_name)
     prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "proj/msvc15_c/%s.VC.VC.opendb" % project_name)
@@ -538,29 +514,7 @@ def generate_msvc15_cpp(target_dir, project_name):
     # base folders / base structure
     prj_fullname_base = path_utils.concat_path(target_dir, project_name)
     base_prj = path_utils.concat_path(prj_fullname_base, "proj")
-
-    base_build = path_utils.concat_path(prj_fullname_base, "build")
-    base_build_windows_x64_debug = path_utils.concat_path(base_build, "windows_x64_debug")
-    base_build_windows_x64_release = path_utils.concat_path(base_build, "windows_x64_release")
-
-    base_run = path_utils.concat_path(prj_fullname_base, "run")
-    base_run_windows_x64_debug = path_utils.concat_path(base_run, "windows_x64_debug")
-    base_run_windows_x64_release = path_utils.concat_path(base_run, "windows_x64_release")
-
     base_src = path_utils.concat_path(prj_fullname_base, "src")
-
-    prjboot_util.makedir_if_needed(prj_fullname_base)
-    prjboot_util.makedir_if_needed(base_prj)
-
-    prjboot_util.makedir_if_needed(base_build)
-    prjboot_util.makedir_if_needed(base_build_windows_x64_debug)
-    prjboot_util.makedir_if_needed(base_build_windows_x64_release)
-
-    prjboot_util.makedir_if_needed(base_run)
-    prjboot_util.makedir_if_needed(base_run_windows_x64_debug)
-    prjboot_util.makedir_if_needed(base_run_windows_x64_release)
-
-    prjboot_util.makedir_if_needed(base_src)
 
     # generate the actual Visual Studio project and solution files
     project_hex_id = msvc15_prj_hex_id()
@@ -574,8 +528,6 @@ def generate_msvc15_cpp(target_dir, project_name):
 
     # gitignore
     gitignore_filename = path_utils.concat_path(prj_fullname_base, ".gitignore")
-    prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "build/")
-    prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "run/")
     prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "proj/msvc15_cpp/.vs")
     prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "proj/msvc15_cpp/%s.VC.db" % project_name)
     prjboot_util.add_to_gitignore_if_needed(gitignore_filename, "proj/msvc15_cpp/%s.VC.VC.opendb" % project_name)
