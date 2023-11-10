4ca6bc23d2aa25ccc2f96c32e0a20e7ad52c1f39
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-11-10 18:26:06 +0100

    mvtools_main.sh: cosmetic changes

--- mvtools_main.sh
+++ mvtools_main.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 
 add2path(){
-  unamestr=`uname`
-  if [[ ! ${unamestr:0:14} == "CYGWIN_NT-10.0" ]]; then
-      if [ -z "$PATH" ]; then
-        # first use. dont add the comma at the beginning
-        export PATH=${1}
-        return
-      fi
-  fi
-  export PATH=${PATH}:${1}
+
+    unamestr=`uname`
+    if [[ ! ${unamestr:0:14} == "CYGWIN_NT-10.0" ]]; then
+        if [ -z "$PATH" ]; then
+            # first use. dont add the comma at the beginning
+            export PATH=${1}
+            return
+        fi
+    fi
+
+    export PATH=${PATH}:${1}
+
 }
 
 add2pythonpath(){
-  if [ -z $PYTHONPATH ]; then
-    # first use. dont add the comma at the beginning
-    export PYTHONPATH=${1}
-  else
-    export PYTHONPATH=${PYTHONPATH}:${1}
-  fi
+
+    if [ -z $PYTHONPATH ]; then
+        # first use. dont add the comma at the beginning
+        export PYTHONPATH=${1}
+    else
+        export PYTHONPATH=${PYTHONPATH}:${1}
+    fi
+
 }
 
 if [ -z $MVTOOLS_LINKS_PATH ]; then
-  MVTOOLS_LINKS_PATH=$MVTOOLS/links
+    MVTOOLS_LINKS_PATH=$MVTOOLS/links
 fi
 
 # this requires the MVTOOLS envvar to be defined in the environment
-
 if [[ ! -z $MVTOOLS && -d $MVTOOLS ]]; then
 
-  add2path $MVTOOLS_LINKS_PATH
-  add2pythonpath $MVTOOLS_LINKS_PATH
+    add2path $MVTOOLS_LINKS_PATH
+    add2pythonpath $MVTOOLS_LINKS_PATH
 
-  # coreutils customisations
-  source $MVTOOLS/coreutils_custom.sh
+    # coreutils customisations
+    source $MVTOOLS/coreutils_custom.sh
 
-  # it is preferrable to source other scripts last, because any of their contents may depend on previously added paths
-  source $MVTOOLS/git/git_aliases.sh
+    # it is preferrable to source other scripts last, because any of their contents may depend on previously added paths
+    source $MVTOOLS/git/git_aliases.sh
 
 else
-  echo "WARNING: MVTOOLS envvar is defined incorrectly."
+    echo "WARNING: MVTOOLS envvar is defined incorrectly."
 fi
