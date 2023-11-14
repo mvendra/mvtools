c1277aa236daec31ce8222d5bbabc83d556b385b
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-11-14 09:00:08 +0100

    bazel_plugin: improved coverage

--- tests/bazel_plugin_test.py
+++ tests/bazel_plugin_test.py
@@ -562,5 +562,27 @@ class BazelPluginTest(unittest.TestCase):
                 out_list = [("bazel_plugin_stdout", "", "Bazel's stdout"), ("bazel_plugin_stderr", "test-stderr", "Bazel's stderr")]
                 dummy2.assert_called_with(True, print, out_list)
 
+    def testBazelPluginTaskTest8(self):
+
+        with mock.patch("bazel_wrapper.test", return_value=(True, (False, "", ""))) as dummy1:
+            with mock.patch("output_backup_helper.dump_outputs_autobackup", return_value=None) as dummy2:
+                v, r = self.bazel_task.task_test(print, self.existent_path1, "dummy_value1", False, None, None, False)
+                self.assertTrue(v)
+                self.assertEqual(r, None)
+                dummy1.assert_called_with(self.existent_path1, "dummy_value1")
+                out_list = [("bazel_plugin_stdout", "", "Bazel's stdout"), ("bazel_plugin_stderr", "", "Bazel's stderr")]
+                dummy2.assert_called_with(False, print, out_list)
+
+    def testBazelPluginTaskTest9(self):
+
+        with mock.patch("bazel_wrapper.test", return_value=(True, (False, "", ""))) as dummy1:
+            with mock.patch("output_backup_helper.dump_outputs_autobackup", return_value=None) as dummy2:
+                v, r = self.bazel_task.task_test(print, self.existent_path1, "dummy_value1", True, None, None, False)
+                self.assertFalse(v)
+                self.assertEqual(r, None)
+                dummy1.assert_called_with(self.existent_path1, "dummy_value1")
+                out_list = [("bazel_plugin_stdout", "", "Bazel's stdout"), ("bazel_plugin_stderr", "", "Bazel's stderr")]
+                dummy2.assert_called_with(False, print, out_list)
+
 if __name__ == '__main__':
     unittest.main()
