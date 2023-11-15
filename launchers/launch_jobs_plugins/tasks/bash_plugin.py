96abaaa27324eff923c084e5b84df4437844ca25
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-11-15 19:23:34 +0100

    introducing: bash_plugin

---
+++ launchers/launch_jobs_plugins/tasks/bash_plugin.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+import os
+
+import path_utils
+import log_helper
+import output_backup_helper
+
+import launch_jobs
+import bash_wrapper
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "bash"
+
+    def _read_params(self):
+
+        script = None
+        cwd = None
+        args = []
+        save_output = None
+        save_error_output = None
+        suppress_stderr_warnings = None
+
+        # script
+        try:
+            script = self.params["script"]
+        except KeyError:
+            return False, "script is a required parameter"
+
+        # cwd
+        try:
+            cwd = self.params["cwd"]
+        except KeyError:
+            pass # optional
+
+        # args
+        try:
+            args_read = self.params["arg"]
+            if isinstance(args_read, list):
+                args = args_read
+            else:
+                args.append(args_read)
+        except KeyError:
+            pass # optional
+
+        # save_output
+        try:
+            save_output = self.params["save_output"]
+        except KeyError:
+            pass # optional
+
+        # save_error_output
+        try:
+            save_error_output = self.params["save_error_output"]
+        except KeyError:
+            pass # optional
+
+        # suppress_stderr_warnings
+        suppress_stderr_warnings = "suppress_stderr_warnings" in self.params
+
+        # validate script
+        if not os.path.exists(script):
+            return False, "script [%s] does not exist" % script
+
+        # validate cwd
+        if cwd is not None:
+            if not os.path.exists(cwd):
+                return False, "cwd [%s] does not exist" % cwd
+
+        # validate save_output
+        if save_output is not None:
+            if os.path.exists(save_output):
+                return False, "save_output [%s] points to a preexisting path" % save_output
+
+        # validate save_error_output
+        if save_error_output is not None:
+            if os.path.exists(save_error_output):
+                return False, "save_error_output [%s] points to a preexisting path" % save_error_output
+
+        return True, (script, cwd, args, save_output, save_error_output, suppress_stderr_warnings)
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        # read params
+        v, r = self._read_params()
+        if not v:
+            return False, r
+        script, cwd, args, save_output, save_error_output, suppress_stderr_warnings = r
+
+        # actual execution
+        v, r = bash_wrapper.exec(script, cwd, args)
+        if not v:
+            return False, r
+        proc_result = r[0]
+        proc_stdout = r[1]
+        proc_stderr = r[2]
+
+        warnings = None
+
+        # dump outputs
+        output_backup_helper.dump_output(feedback_object, save_output, proc_stdout, ("Bash's stdout has been saved to: [%s]" % save_output))
+        output_backup_helper.dump_output(feedback_object, save_error_output, proc_stderr, ("Bash's stderr has been saved to: [%s]" % save_error_output))
+
+        # autobackup outputs
+        output_list = [("bash_plugin_stdout", proc_stdout, "Bash's stdout"), ("bash_plugin_stderr", proc_stderr, "Bash's stderr")]
+        warnings = log_helper.add_to_warnings(warnings, output_backup_helper.dump_outputs_autobackup(proc_result, feedback_object, output_list))
+
+        # warnings
+        if len(proc_stderr) > 0:
+            if not suppress_stderr_warnings:
+                warnings = log_helper.add_to_warnings(warnings, proc_stderr)
+            else:
+                warnings = log_helper.add_to_warnings(warnings, "bash's stderr has been suppressed")
+        return True, warnings
---
+++ tests/bash_plugin_test.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+from unittest import mock
+from unittest.mock import patch
+from unittest.mock import call
+
+import mvtools_test_fixture
+import create_and_write_file
+import path_utils
+import output_backup_helper
+
+import bash_plugin
+
+def FileHasContents(filename, contents):
+    if not os.path.exists(filename):
+        return False
+    local_contents = ""
+    with open(filename, "r") as f:
+        local_contents = f.read()
+    if local_contents == contents:
+        return True
+    return False
+
+class BashPluginTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("bash_plugin_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # the test task
+        self.bash_task = bash_plugin.CustomTask()
+
+        # existent path 1
+        self.existent_path1 = path_utils.concat_path(self.test_dir, "existent_path1")
+        os.mkdir(self.existent_path1)
+
+        # existent path 2
+        self.existent_path2 = path_utils.concat_path(self.test_dir, "existent_path2")
+        os.mkdir(self.existent_path2)
+
+        # nonexistent path 1
+        self.nonexistent_path1 = path_utils.concat_path(self.test_dir, "nonexistent_path1")
+
+        # dumped_stdout_file
+        self.dumped_stdout_file = path_utils.concat_path(self.test_dir, "dumped_stdout_file")
+
+        # dumped_stderr_file
+        self.dumped_stderr_file = path_utils.concat_path(self.test_dir, "dumped_stderr_file")
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testBashPluginReadParams1(self):
+
+        local_params = {}
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertFalse(v)
+
+    def testBashPluginReadParams2(self):
+
+        local_params = {}
+        local_params["script"] = self.nonexistent_path1
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertFalse(v)
+
+    def testBashPluginReadParams3(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path1, None, [], None, None, False) )
+
+    def testBashPluginReadParams4(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.nonexistent_path1
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertFalse(v)
+
+    def testBashPluginReadParams5(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path1, self.existent_path2, [], None, None, False) )
+
+    def testBashPluginReadParams6(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = "dummy_value1"
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path1, self.existent_path2, ["dummy_value1"], None, None, False) )
+
+    def testBashPluginReadParams7(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = ["dummy_value1", "dummy_value2"]
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path1, self.existent_path2, ["dummy_value1", "dummy_value2"], None, None, False) )
+
+    def testBashPluginReadParams8(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = "dummy_value1"
+        local_params["save_output"] = "dummy_value2"
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path1, self.existent_path2, ["dummy_value1"], "dummy_value2", None, False) )
+
+    def testBashPluginReadParams9(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = "dummy_value1"
+        local_params["save_output"] = "dummy_value2"
+        local_params["save_error_output"] = "dummy_value3"
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path1, self.existent_path2, ["dummy_value1"], "dummy_value2", "dummy_value3", False) )
+
+    def testBashPluginReadParams10(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = "dummy_value1"
+        local_params["save_output"] = "dummy_value2"
+        local_params["save_error_output"] = "dummy_value3"
+        local_params["suppress_stderr_warnings"] = "dummy_value4"
+        self.bash_task.params = local_params
+
+        v, r = self.bash_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path1, self.existent_path2, ["dummy_value1"], "dummy_value2", "dummy_value3", True) )
+
+    def testBashPluginRunTask1(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        self.bash_task.params = local_params
+
+        with mock.patch("bash_wrapper.exec", return_value=(True, (True, "", ""))) as dummy:
+            v, r = self.bash_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(self.existent_path1, None, [])
+
+    def testBashPluginRunTask2(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        self.bash_task.params = local_params
+
+        with mock.patch("bash_wrapper.exec", return_value=(True, (True, "", ""))) as dummy:
+            v, r = self.bash_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(self.existent_path1, self.existent_path2, [])
+
+    def testBashPluginRunTask3(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = "dummy_value1"
+        self.bash_task.params = local_params
+
+        with mock.patch("bash_wrapper.exec", return_value=(True, (True, "", ""))) as dummy:
+            v, r = self.bash_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(self.existent_path1, self.existent_path2, ["dummy_value1"])
+
+    def testBashPluginRunTask4(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = "dummy_value1"
+        local_params["save_output"] = self.dumped_stdout_file
+        local_params["save_error_output"] = self.dumped_stderr_file
+        self.bash_task.params = local_params
+
+        with mock.patch("bash_wrapper.exec", return_value=(True, (True, "test-stdout", "test-stderr"))) as dummy1:
+            with mock.patch("output_backup_helper.dump_outputs_autobackup", return_value="somewarning") as dummy2:
+                v, r = self.bash_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertTrue("somewarning" in r)
+                self.assertTrue("test-stderr" in r)
+                self.assertTrue(FileHasContents(self.dumped_stdout_file, "test-stdout"))
+                self.assertTrue(FileHasContents(self.dumped_stderr_file, "test-stderr"))
+                dummy1.assert_called_with(self.existent_path1, self.existent_path2, ["dummy_value1"])
+
+    def testBashPluginRunTask5(self):
+
+        local_params = {}
+        local_params["script"] = self.existent_path1
+        local_params["cwd"] = self.existent_path2
+        local_params["arg"] = "dummy_value1"
+        local_params["save_output"] = self.dumped_stdout_file
+        local_params["save_error_output"] = self.dumped_stderr_file
+        local_params["suppress_stderr_warnings"] = True
+        self.bash_task.params = local_params
+
+        with mock.patch("bash_wrapper.exec", return_value=(True, (True, "test-stdout", "test-stderr"))) as dummy1:
+            with mock.patch("output_backup_helper.dump_outputs_autobackup", return_value="somewarning") as dummy2:
+                v, r = self.bash_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertTrue("somewarning" in r)
+                self.assertFalse("test-stderr" in r)
+                self.assertTrue(FileHasContents(self.dumped_stdout_file, "test-stdout"))
+                self.assertTrue(FileHasContents(self.dumped_stderr_file, "test-stderr"))
+                dummy1.assert_called_with(self.existent_path1, self.existent_path2, ["dummy_value1"])
+
+if __name__ == '__main__':
+    unittest.main()
