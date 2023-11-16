87db2ba6dcacf9b69450c3ff9bd8e4b777cd37f7
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-11-16 18:13:12 +0100

    recipe_plugin: more coverage

--- tests/recipe_plugin_test.py
+++ tests/recipe_plugin_test.py
@@ -38,6 +38,9 @@ class RecipePluginTest(unittest.TestCase):
         self.existent_path1 = path_utils.concat_path(self.test_dir, "existent_path1")
         os.mkdir(self.existent_path1)
 
+        # nonexistent path 1
+        self.nonexistent_path1 = path_utils.concat_path(self.test_dir, "nonexistent_path1")
+
         return True, ""
 
     def tearDown(self):
@@ -61,6 +64,85 @@ class RecipePluginTest(unittest.TestCase):
         v, r = self.recipe_task._read_params()
         self.assertTrue(v)
         self.assertEqual( r, ("run", self.existent_path1, None, []) )
+
+    def testRecipePluginReadParams3(self):
+
+        local_params = {}
+        local_params["test"] = "dummy_value1"
+        local_params["recipe"] = self.existent_path1
+        self.recipe_task.params = local_params
+
+        v, r = self.recipe_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, ("test", self.existent_path1, None, []) )
+
+    def testRecipePluginReadParams4(self):
+
+        local_params = {}
+        local_params["recipe"] = self.existent_path1
+        self.recipe_task.params = local_params
+
+        v, r = self.recipe_task._read_params()
+        self.assertFalse(v)
+
+    def testRecipePluginReadParams5(self):
+
+        local_params = {}
+        local_params["run"] = "dummy_value1"
+        local_params["test"] = "dummy_value2"
+        local_params["recipe"] = self.existent_path1
+        self.recipe_task.params = local_params
+
+        v, r = self.recipe_task._read_params()
+        self.assertFalse(v)
+
+    def testRecipePluginReadParams6(self):
+
+        local_params = {}
+        local_params["test"] = "dummy_value1"
+        local_params["recipe"] = self.nonexistent_path1
+        self.recipe_task.params = local_params
+
+        v, r = self.recipe_task._read_params()
+        self.assertFalse(v)
+
+    def testRecipePluginReadParams7(self):
+
+        local_params = {}
+        local_params["test"] = "dummy_value1"
+        local_params["recipe"] = self.existent_path1
+        local_params["exec_name"] = "dummy_value2"
+        self.recipe_task.params = local_params
+
+        v, r = self.recipe_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, ("test", self.existent_path1, "dummy_value2", []) )
+
+    def testRecipePluginReadParams8(self):
+
+        local_params = {}
+        local_params["test"] = "dummy_value1"
+        local_params["recipe"] = self.existent_path1
+        local_params["exec_name"] = "dummy_value2"
+        local_params["envvar"] = "dummy_value3"
+        self.recipe_task.params = local_params
+
+        v, r = self.recipe_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, ("test", self.existent_path1, "dummy_value2", ["dummy_value3"]) )
+
+    def testRecipePluginReadParams9(self):
+
+        local_params = {}
+        local_params["test"] = "dummy_value1"
+        local_params["recipe"] = self.existent_path1
+        local_params["exec_name"] = "dummy_value2"
+        local_params["envvar"] = ["dummy_value3", "dummy_value4"]
+        self.recipe_task.params = local_params
+
+        v, r = self.recipe_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, ("test", self.existent_path1, "dummy_value2", ["dummy_value3", "dummy_value4"]) )
 
 if __name__ == '__main__':
     unittest.main()
