ae381766f0d67b46ddad4bd737d2fd31a614d74f
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-11-25 16:09:19 +0100

    git_aliases: 4 spaces ident

--- git/git_aliases.sh
+++ git/git_aliases.sh
@@ -11,69 +11,71 @@ alias gidif_noext="gidif --no-ext"
 # SHOW
 alias gisho="git show"
 gishogui(){
-  # mvtodo: does not work on the first commit of a repository
-  HASH=$1
-  if [ -z $1 ]; then
-    HASH="HEAD"
-  fi
-  git difftool -y --no-prompt $HASH~1 $HASH
+
+    # mvtodo: does not work on the first commit of a repository
+    HASH=$1
+    if [ -z $1 ]; then
+      HASH="HEAD"
+    fi
+    git difftool -y --no-prompt $HASH~1 $HASH
+
 }
 
 gikill(){
 
-  # removes $1 commits from the repository
-  # saves a backup of each commit as a patch
-  # on this user's defined temp folder (MVTOOLS_TEMP_PATH)
-  # will not run if there is no pre-existing temp folder
-  # HEAD is patch-0, deepest ($1) commit
-  # is patch-$1
+    # removes $1 commits from the repository
+    # saves a backup of each commit as a patch
+    # on this user's defined temp folder (MVTOOLS_TEMP_PATH)
+    # will not run if there is no pre-existing temp folder
+    # HEAD is patch-0, deepest ($1) commit
+    # is patch-$1
 
-  R=`git status -s`
-  if [[ ! -z $R ]]; then
-    echo "HEAD is not clear. Aborting."
-    return
-  fi
+    R=`git status -s`
+    if [[ ! -z $R ]]; then
+        echo "HEAD is not clear. Aborting."
+        return
+    fi
 
-  TEMP_FOLDER="$MVTOOLS_TEMP_PATH"
-  if [ ! -d $TEMP_FOLDER ]; then
-    echo "No [$TEMP_FOLDER] (MVTOOLS_TEMP_PATH envvar) folder found. Aborting."
-    return
-  fi
+    TEMP_FOLDER="$MVTOOLS_TEMP_PATH"
+    if [ ! -d $TEMP_FOLDER ]; then
+        echo "No [$TEMP_FOLDER] (MVTOOLS_TEMP_PATH envvar) folder found. Aborting."
+        return
+    fi
 
-  NUM_COMMITS=`git log --oneline | wc -l`
+    NUM_COMMITS=`git log --oneline | wc -l`
 
-  RANGE=$1
-  if [ -z $1 ]; then
-    RANGE=1
-  fi
+    RANGE=$1
+    if [ -z $1 ]; then
+        RANGE=1
+    fi
 
-  #if ((RANGE > NUM_COMMITS)); then
-  if [ "$RANGE" -gt "$NUM_COMMITS" ]; then
-    echo "Requested deletion of more commits than there are. Aborting."
-    return
-  fi
+    #if ((RANGE > NUM_COMMITS)); then
+    if [ "$RANGE" -gt "$NUM_COMMITS" ]; then
+        echo "Requested deletion of more commits than there are. Aborting."
+        return
+    fi
 
-  # binary patches safety
-  gisho --oneline | egrep "Binary files (.)* differ"
-  if [ $? -eq 0 ]; then
-    echo "The last commit was binary. The backup patch will be ineffective. Press any key to proceed."
-    read
-  fi
+    # binary patches safety
+    gisho --oneline | egrep "Binary files (.)* differ"
+    if [ $? -eq 0 ]; then
+        echo "The last commit was binary. The backup patch will be ineffective. Press any key to proceed."
+        read
+    fi
 
-  # backs up all commits to be deleted
-  MAX=$RANGE
-  (( MAX-- ))
-  for i in `seq 0 $MAX`; do
-    HASH=`get_git_hash.py`
-    FN="$TEMP_FOLDER/gikill_backup_"
-    FN+=$HASH
-    FN+="_"
-    FN+="$i.patch"
-    git show HEAD~$i > $FN
-  done
+    # backs up all commits to be deleted
+    MAX=$RANGE
+    (( MAX-- ))
+    for i in `seq 0 $MAX`; do
+        HASH=`get_git_hash.py`
+        FN="$TEMP_FOLDER/gikill_backup_"
+        FN+=$HASH
+        FN+="_"
+        FN+="$i.patch"
+        git show HEAD~$i > $FN
+    done
 
-  # carries out the removal
-  git reset --hard HEAD~$RANGE
+    # carries out the removal
+    git reset --hard HEAD~$RANGE
 
 }
 
