bfc3bcd76b2e5b28891ab9817aac03e5689c596a
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-11-27 20:24:21 +0100

    mv_wrapper_test: more coverage

--- tests/mv_wrapper_test.py
+++ tests/mv_wrapper_test.py
@@ -9,6 +9,7 @@ from unittest.mock import patch
 
 import mvtools_test_fixture
 
+import path_utils
 import mv_wrapper
 import generic_run
 
@@ -30,6 +31,11 @@ class MvWrapperTest(unittest.TestCase):
 
         self.result_obj = generic_run.run_cmd_result(None, None, None, None)
 
+        self.path1 = path_utils.concat_path(self.test_dir, "path1")
+        self.path2 = path_utils.concat_path(self.test_dir, "path2")
+        self.rel_path1 = path_utils.concat_path(self.test_dir, "..", path_utils.basename_filtered(self.test_dir), "path1")
+        self.rel_path2 = path_utils.concat_path(self.test_dir, "..", path_utils.basename_filtered(self.test_dir), "path2")
+
         return True, ""
 
     def tearDown(self):
@@ -40,6 +46,30 @@ class MvWrapperTest(unittest.TestCase):
             v, r = mv_wrapper.move(None, None)
             self.assertFalse(v)
             dummy.assert_not_called()
+
+    def testMove2(self):
+        with mock.patch("generic_run.run_cmd", return_value=(True, self.result_obj)) as dummy:
+            v, r = mv_wrapper.move(self.path1, None)
+            self.assertFalse(v)
+            dummy.assert_not_called()
+
+    def testMove3(self):
+        with mock.patch("generic_run.run_cmd", return_value=(True, self.result_obj)) as dummy:
+            v, r = mv_wrapper.move(None, self.path2)
+            self.assertFalse(v)
+            dummy.assert_not_called()
+
+    def testMove4(self):
+        with mock.patch("generic_run.run_cmd", return_value=(True, self.result_obj)) as dummy:
+            v, r = mv_wrapper.move(self.path1, self.path2)
+            self.assertTrue(v)
+            dummy.assert_called_with(["mv", self.path1, self.path2])
+
+    def testMove5(self):
+        with mock.patch("generic_run.run_cmd", return_value=(True, self.result_obj)) as dummy:
+            v, r = mv_wrapper.move(self.rel_path1, self.rel_path2)
+            self.assertTrue(v)
+            dummy.assert_called_with(["mv", self.path1, self.path2])
 
 if __name__ == '__main__':
     unittest.main()
