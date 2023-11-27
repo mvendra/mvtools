e9aa8ca7851a01cc3a894c104b2ecf06d3753073
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-11-27 21:28:44 +0100

    introducing: mv_plugin

---
+++ launchers/launch_jobs_plugins/tasks/mv_plugin.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+
+import launch_jobs
+import path_utils
+import mv_wrapper
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "mv"
+
+    def _read_params(self):
+
+        source_path = None
+        target_path = None
+
+        # source_path
+        try:
+            source_path = self.params["source_path"]
+        except KeyError:
+            return False, "source_path is a required parameter"
+
+        # target_path
+        try:
+            target_path = self.params["target_path"]
+        except KeyError:
+            return False, "target_path is a required parameter"
+
+        # validations
+        if not os.path.exists(source_path):
+            return False, "Source path [%s] does not exist" % source_path
+
+        if not os.path.exists(target_path):
+            return False, "Target path [%s] does not exist" % target_path
+
+        final_path = path_utils.concat_path(target_path, path_utils.basename_filtered(source_path))
+        if os.path.exists(final_path):
+            return False, "Final path [%s] already exists" % final_path
+
+        return True, (source_path, target_path)
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        v, r = self._read_params()
+        if not v:
+            return False, r
+        source_path, target_path = r
+
+        v, r = mv_wrapper.move(source_path, target_path)
+        if not v:
+            return False, r
+        return True, None
---
+++ tests/mv_plugin_test.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import mvtools_test_fixture
+import path_utils
+
+import mv_plugin
+
+class MvPluginTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("mv_plugin_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        self.test_target_path = path_utils.concat_path(self.test_dir, "test_target_path")
+
+        self.nonexistent1 = path_utils.concat_path(self.test_dir, "nonexistent1")
+        self.nonexistent2 = path_utils.concat_path(self.test_dir, "nonexistent2")
+
+        self.path1 = path_utils.concat_path(self.test_dir, "path1")
+        self.path2 = path_utils.concat_path(self.test_dir, "path2")
+
+        os.mkdir(self.path1)
+        os.mkdir(self.path2)
+
+        # the test task
+        self.mv_task = mv_plugin.CustomTask()
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testMvPluginReadParams1(self):
+
+        local_params = {}
+        self.mv_task.params = local_params
+
+        v, r = self.mv_task._read_params()
+        self.assertFalse(v)
+
+    def testMvPluginReadParams2(self):
+
+        local_params = {}
+        local_params["source_path"] = "dummy_value1"
+        self.mv_task.params = local_params
+
+        v, r = self.mv_task._read_params()
+        self.assertFalse(v)
+
+    def testMvPluginReadParams3(self):
+
+        local_params = {}
+        local_params["source_path"] = "dummy_value1"
+        local_params["target_path"] = "dummy_value2"
+        self.mv_task.params = local_params
+
+        v, r = self.mv_task._read_params()
+        self.assertFalse(v)
+
+    def testMvPluginReadParams4(self):
+
+        local_params = {}
+        local_params["source_path"] = self.nonexistent1
+        local_params["target_path"] = self.path2
+        self.mv_task.params = local_params
+
+        v, r = self.mv_task._read_params()
+        self.assertFalse(v)
+
+    def testMvPluginReadParams5(self):
+
+        local_params = {}
+        local_params["source_path"] = self.path1
+        local_params["target_path"] = self.nonexistent1
+        self.mv_task.params = local_params
+
+        v, r = self.mv_task._read_params()
+        self.assertFalse(v)
+
+    def testMvPluginReadParams6(self):
+
+        local_params = {}
+        local_params["source_path"] = self.path1
+        local_params["target_path"] = self.path2
+        self.mv_task.params = local_params
+
+        v, r = self.mv_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual(r, (self.path1, self.path2))
+
+if __name__ == '__main__':
+    unittest.main()
