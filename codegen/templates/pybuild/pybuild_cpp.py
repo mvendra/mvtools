8240cb4f4a6798c0d061d98d6640f43d33246f19
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-12-03 21:51:43 +0100

    codegen: dropped get_cpp_compiler_flags_macosx_common_gcc

--- codegen/templates/pybuild/pybuild_cpp.py
+++ codegen/templates/pybuild/pybuild_cpp.py
@@ -117,10 +117,8 @@ class Builder():
     def standard_cpp_hook(self):
 
         # common compiler flags
-        if self.plat == get_platform.PLAT_LINUX and self.compiler == "g++":
+        if (self.plat == get_platform.PLAT_LINUX or self.plat == get_platform.PLAT_MACOSX) and self.compiler == "g++":
             self.compiler_flags_common += standard_cpp.get_cpp_compiler_flags_linux_common_gcc()
-        if self.plat == get_platform.PLAT_MACOSX and self.compiler == "g++":
-            self.compiler_flags_common += standard_cpp.get_cpp_compiler_flags_macosx_common_gcc()
 
         # debug compiler flags
         if self.mode == "debug" and self.compiler == "g++":
