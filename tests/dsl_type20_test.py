27434912e0f91590da8b3dad589257dddc28b537
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-12-23 18:49:32 +0100

    dsl_type20_test: cleaned up testDslType20_TestAddContext8

--- tests/dsl_type20_test.py
+++ tests/dsl_type20_test.py
@@ -1277,12 +1277,8 @@ class DSLType20Test(unittest.TestCase):
         self.assertTrue(v)
         v, r = dsl.add_context("ctx1", "ctx2", [("opt1", "val2")])
         self.assertTrue(v)
-        v, r = dsl.get_context_options("ctx1")
-        self.assertTrue(v)
-        self.assertEqual([ (x.get_name(), x.get_value()) for x in r], [ ("opt1", "val1") ])
-        v, r = dsl.get_context_options("ctx2")
-        self.assertTrue(v)
-        self.assertEqual([ (x.get_name(), x.get_value()) for x in r], [ ("opt1", "val2") ])
+        self.assertEqual(opt_fmt_helper(dsl.get_context_options("ctx1")), [ ("opt1", "val1") ])
+        self.assertEqual(opt_fmt_helper(dsl.get_context_options("ctx2")), [ ("opt1", "val2") ])
 
     def testDslType20_TestAddContext9(self):
         dsl = dsl_type20.DSLType20(dsl_type20.DSLType20_Config())
