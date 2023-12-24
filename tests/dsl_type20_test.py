2d03d94f6ec9d96feaacc38cc6b024d0242c2a82
Mateus Vendramini <mateusvendramini21@gmail.com>
2023-12-24 12:56:15 +0100

    dsl_type20_test: improved testDslType20_TestGetSubContext3

--- tests/dsl_type20_test.py
+++ tests/dsl_type20_test.py
@@ -1567,7 +1567,8 @@ class DSLType20Test(unittest.TestCase):
         v, r = dsl.get_sub_context("ctx1")
         self.assertTrue(v)
         self.assertTrue(isinstance(r, dsl_type20.DSLType20_Context))
-        self.assertEqual(r.get_name(), dsl.data.get_entries()[1].get_name())
+        self.assertEqual(r.get_name(), dsl.data.entries[1].get_name())
+        self.assertNotEqual(r, dsl.data.entries[1])
         self.assertEqual(len(r.get_entries()), 1)
         self.assertTrue("ctx2" in [x.get_name() for x in r.get_entries()] )
 
