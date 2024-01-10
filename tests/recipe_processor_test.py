d622a7914038b6f7f72249b463048e154996abd3
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-01-10 21:36:49 +0100

    recipe_processor_test: pre-adjusting indices

--- tests/recipe_processor_test.py
+++ tests/recipe_processor_test.py
@@ -313,12 +313,12 @@ class RecipeProcessorTest(unittest.TestCase):
         self.recipe_test_file23 = path_utils.concat_path(self.test_dir, "recipe_test23.t20")
         create_and_write_file.create_file_contents(self.recipe_test_file23, recipe_test_contents23)
 
-        recipe_test_contents24 = "[\n@%s\n" % recipe_processor.RECIPE_PROCESSOR_CONFIG_METAJOB
-        recipe_test_contents24 += "* recipe-namespace = \"%s\"\n" % self.namespace1
-        recipe_test_contents24 += "]\n"
-        recipe_test_contents24 += "[\n@test-job\n* task1 = \"sample_echo_true_plugin.py\"\n]"
-        self.recipe_test_file24 = path_utils.concat_path(self.test_dir, "recipe_test24.t20")
-        create_and_write_file.create_file_contents(self.recipe_test_file24, recipe_test_contents24)
+        recipe_test_contents25 = "[\n@%s\n" % recipe_processor.RECIPE_PROCESSOR_CONFIG_METAJOB
+        recipe_test_contents25 += "* recipe-namespace = \"%s\"\n" % self.namespace1
+        recipe_test_contents25 += "]\n"
+        recipe_test_contents25 += "[\n@test-job\n* task1 = \"sample_echo_true_plugin.py\"\n]"
+        self.recipe_test_file25 = path_utils.concat_path(self.test_dir, "recipe_test25.t20")
+        create_and_write_file.create_file_contents(self.recipe_test_file25, recipe_test_contents25)
 
         recipe_test_contents26 = "[\n@%s\n" % recipe_processor.RECIPE_PROCESSOR_CONFIG_METAJOB
         recipe_test_contents26 += "* recipe-namespace {inclusive} = \"%s\"\n" % self.namespace2
@@ -447,7 +447,7 @@ class RecipeProcessorTest(unittest.TestCase):
         self.assertTrue(v)
 
     def testRecipeProcessorNamespaceExclusiveNotFound(self):
-        v, r = recipe_processor.run_jobs_from_recipe_file(self.recipe_test_file24)
+        v, r = recipe_processor.run_jobs_from_recipe_file(self.recipe_test_file25)
         self.assertFalse(v)
 
     def testRecipeProcessorNamespaceInclusiveIsFoundAtBuiltInsFirst(self):
