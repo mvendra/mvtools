b01730405e10e69fcc35f7fe00f6a7273808bd66
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-01-10 21:58:35 +0100

    recipe_processor_test: pre-adjusting indices

--- tests/recipe_processor_test.py
+++ tests/recipe_processor_test.py
@@ -303,17 +303,17 @@ class RecipeProcessorTest(unittest.TestCase):
         self.recipe_test_file22 = path_utils.concat_path(self.test_dir, "recipe_test22.t20")
         create_and_write_file.create_file_contents(self.recipe_test_file22, recipe_test_contents22)
 
-        recipe_test_contents23 = "[\n@%s\n" % recipe_processor.RECIPE_PROCESSOR_CONFIG_METAJOB
-        recipe_test_contents23 += "* recipe-namespace = \"%s\"\n" % self.test_dir
-        recipe_test_contents23 += "]\n"
-        recipe_test_contents23 += "[\n@test-job-1\n"
-        recipe_test_contents23 += "* task1 = \"%s\"\n" % path_utils.basename_filtered(self.sample_custom_exe_name_contents_file1)
-        recipe_test_contents23 += "]\n"
-        recipe_test_contents23 += "[\n@test-job-2\n"
-        recipe_test_contents23 += "* task2 = \"%s\"\n" % path_utils.basename_filtered(self.sample_custom_exe_name_contents_file2)
-        recipe_test_contents23 += "]"
-        self.recipe_test_file23 = path_utils.concat_path(self.test_dir, "recipe_test23.t20")
-        create_and_write_file.create_file_contents(self.recipe_test_file23, recipe_test_contents23)
+        recipe_test_contents24 = "[\n@%s\n" % recipe_processor.RECIPE_PROCESSOR_CONFIG_METAJOB
+        recipe_test_contents24 += "* recipe-namespace = \"%s\"\n" % self.test_dir
+        recipe_test_contents24 += "]\n"
+        recipe_test_contents24 += "[\n@test-job-1\n"
+        recipe_test_contents24 += "* task1 = \"%s\"\n" % path_utils.basename_filtered(self.sample_custom_exe_name_contents_file1)
+        recipe_test_contents24 += "]\n"
+        recipe_test_contents24 += "[\n@test-job-2\n"
+        recipe_test_contents24 += "* task2 = \"%s\"\n" % path_utils.basename_filtered(self.sample_custom_exe_name_contents_file2)
+        recipe_test_contents24 += "]"
+        self.recipe_test_file24 = path_utils.concat_path(self.test_dir, "recipe_test24.t20")
+        create_and_write_file.create_file_contents(self.recipe_test_file24, recipe_test_contents24)
 
         recipe_test_contents25 = "[\n@%s\n" % recipe_processor.RECIPE_PROCESSOR_CONFIG_METAJOB
         recipe_test_contents25 += "* recipe-namespace = \"%s\"\n" % self.namespace1
@@ -452,7 +452,7 @@ class RecipeProcessorTest(unittest.TestCase):
         self.assertFalse(v)
 
     def testRecipeProcessorCustomTaskUseExecutionName(self):
-        v, r = recipe_processor.run_jobs_from_recipe_file(self.recipe_test_file23)
+        v, r = recipe_processor.run_jobs_from_recipe_file(self.recipe_test_file24)
         self.assertTrue(v)
 
     def testRecipeProcessorStringlistParam(self):
