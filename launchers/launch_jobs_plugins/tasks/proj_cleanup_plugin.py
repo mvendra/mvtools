f6b5eaf5befaa4b320d0654e5610c44776946755
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-06-08 11:02:29 +0200

    introducing: proj_cleanup_plugin

---
+++ launchers/launch_jobs_plugins/tasks/proj_cleanup_plugin.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+
+import launch_jobs
+import proj_cleanup
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "proj_cleanup"
+
+    def _read_params(self):
+
+        proj = None
+        dep = None
+        tmp = None
+        out = None
+
+        # proj
+        try:
+            proj = self.params["proj"]
+        except KeyError:
+            return False, "proj is a required parameter"
+
+        # dep
+        dep = "dep" in self.params
+
+        # tmp
+        tmp = "tmp" in self.params
+
+        # out
+        out = "out" in self.params
+
+        if not os.path.exists(proj):
+            return False, "proj [%s] points to a nonexistent path" % proj
+
+        return True, (proj, dep, tmp, out)
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        # read params
+        v, r = self._read_params()
+        if not v:
+            return False, r
+        proj, dep, tmp, out = r
+
+        v, r = proj_cleanup.proj_cleanup(proj, dep, tmp, out)
+        if not v:
+            return False, "Task failed: [%s]" % r
+        return True, None
---
+++ tests/proj_cleanup_plugin_test.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+from unittest import mock
+from unittest.mock import patch
+from unittest.mock import call
+
+import mvtools_test_fixture
+
+import path_utils
+import proj_cleanup_plugin
+
+class ProjCleanupPluginTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("proj_cleanup_plugin_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # the test task
+        self.proj_cleanup_task = proj_cleanup_plugin.CustomTask()
+
+        # existent path
+        self.existent_path = path_utils.concat_path(self.test_dir, "existent_path")
+        os.mkdir(self.existent_path)
+
+        # nonexistent path
+        self.nonexistent_path = path_utils.concat_path(self.test_dir, "nonexistent_path")
+
+        return True, None
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testProjCleanupPluginReadParams1(self):
+
+        local_params = {}
+        self.proj_cleanup_task.params = local_params
+
+        v, r = self.proj_cleanup_task._read_params()
+        self.assertFalse(v)
+
+    def testProjCleanupPluginReadParams2(self):
+
+        local_params = {}
+        local_params["proj"] = self.nonexistent_path
+        self.proj_cleanup_task.params = local_params
+
+        v, r = self.proj_cleanup_task._read_params()
+        self.assertFalse(v)
+
+    def testProjCleanupPluginReadParams3(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        self.proj_cleanup_task.params = local_params
+
+        v, r = self.proj_cleanup_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path, False, False, False) )
+
+    def testProjCleanupPluginReadParams4(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        local_params["dep"] = "dummy_value1"
+        self.proj_cleanup_task.params = local_params
+
+        v, r = self.proj_cleanup_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path, True, False, False) )
+
+    def testProjCleanupPluginReadParams5(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        local_params["dep"] = "dummy_value1"
+        local_params["tmp"] = "dummy_value2"
+        self.proj_cleanup_task.params = local_params
+
+        v, r = self.proj_cleanup_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path, True, True, False) )
+
+    def testProjCleanupPluginReadParams6(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        local_params["dep"] = "dummy_value1"
+        local_params["tmp"] = "dummy_value2"
+        local_params["out"] = "dummy_value3"
+        self.proj_cleanup_task.params = local_params
+
+        v, r = self.proj_cleanup_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual( r, (self.existent_path, True, True, True) )
+
+    def testProjCleanupPluginRunTask1(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        self.proj_cleanup_task.params = local_params
+
+        with mock.patch("proj_cleanup.proj_cleanup", return_value=(False, "test error message")) as dummy:
+            v, r = self.proj_cleanup_task.run_task(print, "exe_name")
+            self.assertFalse(v)
+            dummy.assert_called_with(self.existent_path, False, False, False)
+
+    def testProjCleanupPluginRunTask2(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        self.proj_cleanup_task.params = local_params
+
+        with mock.patch("proj_cleanup.proj_cleanup", return_value=(True, None)) as dummy:
+            v, r = self.proj_cleanup_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(self.existent_path, False, False, False)
+
+    def testProjCleanupPluginRunTask3(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        local_params["dep"] = "dummy_value1"
+        self.proj_cleanup_task.params = local_params
+
+        with mock.patch("proj_cleanup.proj_cleanup", return_value=(True, None)) as dummy:
+            v, r = self.proj_cleanup_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(self.existent_path, True, False, False)
+
+    def testProjCleanupPluginRunTask4(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        local_params["dep"] = "dummy_value1"
+        local_params["tmp"] = "dummy_value2"
+        self.proj_cleanup_task.params = local_params
+
+        with mock.patch("proj_cleanup.proj_cleanup", return_value=(True, None)) as dummy:
+            v, r = self.proj_cleanup_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(self.existent_path, True, True, False)
+
+    def testProjCleanupPluginRunTask5(self):
+
+        local_params = {}
+        local_params["proj"] = self.existent_path
+        local_params["dep"] = "dummy_value1"
+        local_params["tmp"] = "dummy_value2"
+        local_params["out"] = "dummy_value3"
+        self.proj_cleanup_task.params = local_params
+
+        with mock.patch("proj_cleanup.proj_cleanup", return_value=(True, None)) as dummy:
+            v, r = self.proj_cleanup_task.run_task(print, "exe_name")
+            self.assertTrue(v)
+            dummy.assert_called_with(self.existent_path, True, True, True)
+
+if __name__ == '__main__':
+    unittest.main()
