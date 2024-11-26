aff92903bafde86bbb419fcd7e830ea4c2921956
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-11-26 17:57:53 +0100

    filteredsearch: reusing generic_run

--- filteredsearch.py
+++ filteredsearch.py
@@ -2,12 +2,11 @@
 
 import os
 import sys
-from subprocess import check_output
-from subprocess import CalledProcessError
 
+import mvtools_exception
 import path_utils
 import fsquery
-import mvtools_exception
+import generic_run
 import terminal_colors
 
 def filteredsearch(path, search, extensions):
@@ -15,21 +14,14 @@ def filteredsearch(path, search, extensions):
     v, r = fsquery.makecontentlist(path, True, False, True, False, False, False, True, extensions)
     if not v:
         raise mvtools_exception.mvtools_exception(r)
-    ret = r
-    for r in ret:
-        out = ""
-        try:
-            out = check_output(["ag", search, r])
-            out = out.decode("utf8")
-        except OSError as oe:
-            print("Failed calling ag. Make sure silversearcher-ag is installed.")
-            exit(1)
-        except CalledProcessError:
-            pass # no match, most likely
-        if not len(out):
-            continue
-        print("%s%s" % (terminal_colors.TTY_BLUE, r))
-        print("%s%s" % (terminal_colors.TTY_WHITE, out))
+    targets = r
+
+    for tg in targets:
+
+        v, r = generic_run.run_cmd_simple(["ag", search, tg])
+        if v:
+            print("%s%s" % (terminal_colors.TTY_BLUE, tg))
+            print("%s%s" % (terminal_colors.TTY_WHITE, r))
 
 def puaq():
     print("Usage: %s path search extensions " % path_utils.basename_filtered(__file__))
