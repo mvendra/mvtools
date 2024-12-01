2e33ae097f6862f24a1bbe4c8146626fd6871246
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-12-01 20:24:00 +0100

    introducing: ffmpeg_wrapper

---
+++ wrappers/ffmpeg_wrapper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import path_utils
+import generic_run
+
+def convert(input_filename, output_filename, bitrate):
+
+    input_filename_filtered = path_utils.filter_remove_trailing_sep(input_filename)
+    output_filename_filtered = path_utils.filter_remove_trailing_sep(output_filename)
+
+    if not os.path.exists(input_filename_filtered):
+        return False, "Input filename [%s] does not exist." % input_filename_filtered
+
+    if os.path.exists(output_filename_filtered):
+        return False, "Output filename [%s] already exists." % output_filename_filtered
+
+    full_cmd = ["ffmpeg", "-i", input_filename_filtered, "-ab", bitrate, "-map_metadata", "0", "-id3v2_version", "3", output_filename_filtered]
+    v, r = generic_run.run_cmd_simple(full_cmd)
+    return v, r
+
+def puaq():
+    print("Usage: %s input_filename output_filename" % path_utils.basename_filtered(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        puaq()
+
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+
+    v, r = convert(input_filename, output_filename, "192k")
+    if not v:
+        print(r)
+        sys.exit(1)
