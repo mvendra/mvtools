ce317911de03c0274d0f1deedb916d6c9fb3d8fa
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-12-01 19:52:40 +0100

    introducing: gcc_wrapper

---
+++ tests/gcc_wrapper_test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import mvtools_test_fixture
+import create_and_write_file
+import path_utils
+import standard_c
+import generic_run
+
+import gcc_wrapper
+
+class GccWrapperTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("gcc_wrapper_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # prepare test content
+        self.main_c = "main.c"
+        self.main_c_full = path_utils.concat_path(self.test_dir, self.main_c)
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testExec1(self):
+
+        self.assertTrue(create_and_write_file.create_file_contents(self.main_c_full, standard_c.get_main_c_app()))
+
+        v, r = gcc_wrapper.exec(self.main_c_full, self.test_dir)
+        self.assertFalse(v)
+        self.assertEqual(r, "options_list must be a list")
+
+    def testExec2(self):
+
+        v, r = gcc_wrapper.exec([self.main_c_full], self.test_dir)
+        self.assertFalse(v)
+
+    def testExec3(self):
+
+        self.assertTrue(create_and_write_file.create_file_contents(self.main_c_full, standard_c.get_main_c_app()))
+
+        v, r = gcc_wrapper.exec([self.main_c_full], self.test_dir)
+        self.assertTrue(v)
+        self.assertEqual(r, "")
+
+        a_out = "a.out"
+        a_out_full = path_utils.concat_path(self.test_dir, a_out)
+
+        v, r = generic_run.run_cmd_simple([a_out_full], use_cwd=self.test_dir)
+        self.assertTrue(v)
+        self.assertEqual(r.strip(), "test for echo")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ wrappers/gcc_wrapper.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import path_utils
+import generic_run
+
+def exec(options_list, cwd = None):
+
+    if not isinstance(options_list, list):
+        return False, "options_list must be a list"
+
+    full_cmd = ["gcc"]
+    for opt in options_list:
+        full_cmd.append(opt)
+
+    v, r = generic_run.run_cmd_simple(full_cmd, use_cwd=cwd)
+    return v, r
+
+def puaq():
+    print("Usage: %s [options-list]" % path_utils.basename_filtered(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    options_list = sys.argv[1:]
+
+    v, r = exec(options_list)
+    if not v:
+        print(r)
+        sys.exit(1)
