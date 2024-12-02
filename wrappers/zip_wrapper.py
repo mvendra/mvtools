2f044742595c089ea63b65167f8f8c4ce6f084e1
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-12-02 17:18:02 +0100

    zip_wrapper: bugfixes

--- wrappers/zip_wrapper.py
+++ wrappers/zip_wrapper.py
@@ -19,12 +19,12 @@ def make_pack(file_to_create, incl_list):
     if len(incl_list) == 0:
         return False, "No files to add"
     if os.path.exists(file_to_create):
-        return False, "%s already exists." % file_to_create
+        return False, "[%s] already exists." % file_to_create
 
     for fi in range(len(incl_list)):
         incl_list[fi] = path_utils.filter_remove_trailing_sep(incl_list[fi])
         if not os.path.exists( incl_list[fi] ) and not path_utils.is_path_broken_symlink(incl_list[fi]):
-            return False, "%s does not exist." % incl_list(fi)
+            return False, "[%s] does not exist." % incl_list[fi]
 
     # main option
     zip_cmd = ["zip", file_to_create, "-r"] + incl_list
@@ -40,9 +40,9 @@ def extract(file_to_extract, target_path):
 
     # prechecks
     if not os.path.exists(file_to_extract):
-        return False, "%s does not exist." % file_to_extract
+        return False, "[%s] does not exist." % file_to_extract
     if not os.path.exists(target_path):
-        return False, "%s does not exist." % target_path
+        return False, "[%s] does not exist." % target_path
 
     # actual command
     zip_cmd = ["unzip", file_to_extract, "-d", target_path]
@@ -61,4 +61,7 @@ if __name__ == "__main__":
     file_to_create = sys.argv[1]
     incl_list = sys.argv[2:]
 
-    make_pack(file_to_create, incl_list)
+    v, r = make_pack(file_to_create, incl_list)
+    if not v:
+        print(r)
+        sys.exit(1)
