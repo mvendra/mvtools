da672a01a5cae87251e7c46448e8f9d3eaffd8ab
Mateus Vendramini <mateusvendramini21@gmail.com>
2024-12-02 19:07:10 +0100

    introducing: umount_wrapper

---
+++ wrappers/umount_wrapper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import path_utils
+import generic_run
+
+def umount(target_path):
+
+    if target_path is None:
+        return False, "target_path is mandatory"
+
+    full_cmd = ["umount", target_path]
+
+    v, r = generic_run.run_cmd_simple(full_cmd)
+    if not v:
+        return False, "Failed running umount command: [%s]" % r
+
+    return True, None
+
+def puaq():
+    print("Usage: %s target_path" % path_utils.basename_filtered(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        puaq()
+
+    target_path = sys.argv[1]
+
+    v, r = umount(target_path)
+    if not v:
+        print(r)
+        sys.exit(1)
