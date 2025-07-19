1051d29ce36a8b467bc1be74a598ce96b14bbda5
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-07-19 20:08:45 +0200

    tree_wrapper_test: updated after create_and_write_file api change

--- tests/tree_wrapper_test.py
+++ tests/tree_wrapper_test.py
@@ -30,37 +30,37 @@ class TreeWrapperTest(unittest.TestCase):
         # create test content
 
         self.file_with_space = path_utils.concat_path(self.test_dir, "fi le.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.file_with_space, "abc"))
+        create_and_write_file.create_file_contents(self.file_with_space, "abc")
         self.file_with_sep = path_utils.concat_path(self.test_dir, "file_with_sep.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.file_with_sep, "abc"))
+        create_and_write_file.create_file_contents(self.file_with_sep, "abc")
         self.file_with_sep += "/"
 
         self.folder_with_space = path_utils.concat_path(self.test_dir, "fol der")
         os.mkdir(self.folder_with_space)
         self.folder_with_space_filler = path_utils.concat_path(self.folder_with_space, "filler.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.folder_with_space_filler, "abc"))
+        create_and_write_file.create_file_contents(self.folder_with_space_filler, "abc")
 
         self.folder_with_sep = path_utils.concat_path(self.test_dir, "folder_with_sep")
         os.mkdir(self.folder_with_sep)
         self.folder_with_sep_filler = path_utils.concat_path(self.folder_with_sep, "filler.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.folder_with_sep_filler, "abc"))
+        create_and_write_file.create_file_contents(self.folder_with_sep_filler, "abc")
         self.folder_with_sep += "/"
 
         # base, files
         self.file1 = path_utils.concat_path(self.test_dir, "file1.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.file1, "abc"))
+        create_and_write_file.create_file_contents(self.file1, "abc")
 
         self.file_hidden = path_utils.concat_path(self.test_dir, ".file_hidden.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.file_hidden, "abc"))
+        create_and_write_file.create_file_contents(self.file_hidden, "abc")
 
         self.file_esp1 = path_utils.concat_path(self.test_dir, "   file_esp1.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.file_esp1, "abc"))
+        create_and_write_file.create_file_contents(self.file_esp1, "abc")
 
         self.file_esp2 = path_utils.concat_path(self.test_dir, "file_esp2.txt   ")
-        self.assertTrue(create_and_write_file.create_file_contents(self.file_esp2, "abc"))
+        create_and_write_file.create_file_contents(self.file_esp2, "abc")
 
         self.file2 = path_utils.concat_path(self.test_dir, "file2.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.file2, "abc"))
+        create_and_write_file.create_file_contents(self.file2, "abc")
 
         # base, folders
         self.folder1 = path_utils.concat_path(self.test_dir, "folder1")
@@ -73,23 +73,23 @@ class TreeWrapperTest(unittest.TestCase):
         os.mkdir(self.folder2_sub1)
 
         self.folder2_sub1_file1 = path_utils.concat_path(self.folder2_sub1, "file1.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.folder2_sub1_file1, "abc"))
+        create_and_write_file.create_file_contents(self.folder2_sub1_file1, "abc")
 
         # files in folders
 
         # folder1
         self.folder1_file1 = path_utils.concat_path(self.folder1, "file1.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.folder1_file1, "abc"))
+        create_and_write_file.create_file_contents(self.folder1_file1, "abc")
 
         self.folder1_file2 = path_utils.concat_path(self.folder1, "file2.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.folder1_file2, "abc"))
+        create_and_write_file.create_file_contents(self.folder1_file2, "abc")
 
         # folder2
         self.folder2_file1 = path_utils.concat_path(self.folder2, "file1.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.folder2_file1, "abc"))
+        create_and_write_file.create_file_contents(self.folder2_file1, "abc")
 
         self.folder2_file2 = path_utils.concat_path(self.folder2, "file2.txt")
-        self.assertTrue(create_and_write_file.create_file_contents(self.folder2_file2, "abc"))
+        create_and_write_file.create_file_contents(self.folder2_file2, "abc")
 
         return True, ""
 
