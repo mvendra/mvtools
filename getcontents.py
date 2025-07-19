38da54d6819981046c651d27ba48581f2ee54ab3
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-07-19 17:00:11 +0200

    getcontents: raise except instead of return None

--- getcontents.py
+++ getcontents.py
@@ -3,9 +3,11 @@
 import sys
 import os
 
+import mvtools_exception
+
 def getcontents(filename):
     if not os.path.exists(filename):
-        return None
+        raise mvtools_exception.mvtools_exception("filename [%s] does not exist" % filename)
     contents = ""
     with open(filename) as f:
         contents = f.read()
