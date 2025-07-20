905a6d26db7aa7960d04b1120fad9fb502e58fc2
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-07-20 09:29:51 +0200

    introducing: open_and_update_file

---
+++ open_and_update_file.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import binascii
+
+import mvtools_exception
+import path_utils
+
+def update_file_contents(filename, contents):
+
+    if not os.path.exists(filename):
+        raise mvtools_exception.mvtools_exception("[%s] does not exist. Will not proceed." % filename)
+
+    with open(filename, "a+") as f:
+        f.write(contents)
+
+def puaq():
+    print("Usage: %s file contents" % path_utils.basename_filtered(__file__))
+    sys.exit(1)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        puaq()
+
+    filename = sys.argv[1]
+    contents = sys.argv[2]
+
+    try:
+        update_file_contents(filename, contents)
+    except mvtools_exception.mvtools_exception as ex:
+        print(ex)
+        sys.exit(1)
---
+++ tests/open_and_update_file_test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+
+import mvtools_test_fixture
+import mvtools_exception
+import getcontents
+import path_utils
+
+import create_and_write_file
+import open_and_update_file
+
+class OpenAndUpdateFileTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("open_and_update_file_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # source files
+        self.content_file = path_utils.concat_path(self.test_dir, "test_contents.txt")
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testUpdateFileContentsDoesntExist(self):
+
+        self.assertFalse(os.path.exists(self.content_file))
+        self.assertTrue(mvtools_test_fixture.throwsExcept2(open_and_update_file.update_file_contents, self.content_file, "andthensomemore", mvtools_exception.mvtools_exception))
+        self.assertFalse(os.path.exists(self.content_file))
+
+    def testUpdateFileContentsVanilla(self):
+
+        self.assertFalse(os.path.exists(self.content_file))
+        create_and_write_file.create_file_contents(self.content_file, "abcdef123456")
+        self.assertTrue(os.path.exists(self.content_file))
+        self.assertEqual(getcontents.getcontents(self.content_file), "abcdef123456")
+        open_and_update_file.update_file_contents(self.content_file, "andthensomemore")
+        self.assertEqual(getcontents.getcontents(self.content_file), "abcdef123456andthensomemore")
+
+if __name__ == '__main__':
+    unittest.main()
