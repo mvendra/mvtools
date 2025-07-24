48d8b6fcd45f6bfcc001982c624d4c2d977f16df
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-07-24 13:20:59 +0200

    linc_c_integer_suffix: prototype progress

--- codelint/plugins/lint_c_integer_suffix.py
+++ codelint/plugins/lint_c_integer_suffix.py
@@ -6,47 +6,6 @@ import os
 import path_utils
 import string_utils
 
-def scan_largest_of(target_str, target_str_offset, source_str_list):
-
-    largest_found = 0
-    largest_available = 0
-    map_src_offsets = {}
-
-    for e in source_str_list:
-        map_src_offsets[e] = 0
-        if len(e) > largest_available:
-            largest_available = len(e)
-
-    scan_count = 0
-    for idx in range(target_str_offset, len(target_str)):
-        scan_count += 1
-
-        c = target_str[idx]
-
-        no_matches = True
-        for e in source_str_list:
-
-            if scan_count > len(e):
-                continue
-
-            if c == e[map_src_offsets[e]]:
-                map_src_offsets[e] += 1
-                no_matches = False
-            else:
-                map_src_offsets[e] = 0
-
-            if map_src_offsets[e] == len(e):
-                if len(e) > largest_found:
-                    largest_found = len(e)
-
-        if no_matches:
-            break
-
-        if largest_found == largest_available:
-            break
-
-    return largest_found
-
 def lint_name():
     return path_utils.basename_filtered(__file__)
 
@@ -70,9 +29,9 @@ def lint_cycle(plugins_params, filename, shared_state, line_index, content_line)
     bin_candidate = False
     parsing_hex = False
     parsing_fp = False
+    number_is_fp = False
     parsing_suffix = False
     mod_flag = False
-    skip_amt = 0
 
     for idx in range(len(content_line_local)):
 
@@ -102,6 +61,7 @@ def lint_cycle(plugins_params, filename, shared_state, line_index, content_line)
             if not parsing_fp:
                 if c == ".":
                     parsing_fp = True
+                    number_is_fp = True
                     corrected_line += c
                     continue
 
@@ -110,11 +70,10 @@ def lint_cycle(plugins_params, filename, shared_state, line_index, content_line)
                 continue
 
             # integer already ended here - its the suffix's turn
+            parsing_suffix = True
             parsing_number = False
             parsing_hex = False
-            parsing_fp = False # mvtodo: might need to be used first
-
-            # mvtodo: now its either a matter of knowing which suffixes are bad, or what followup chars are not bad suffixes but something esle (that is also valid)
+            parsing_fp = False
 
         else:
 
