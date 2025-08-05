4ede2cde769318ace1f2f3d7a48cb9e5e5641576
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-08-05 19:58:31 +0200

    introducing: cleanup_dir_plugin

---
+++ launchers/launch_jobs_plugins/tasks/cleanup_dir_plugin.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import os
+
+import launch_jobs
+import path_utils
+import fsquery
+
+class CustomTask(launch_jobs.BaseTask):
+
+    def get_desc(self):
+        return "cleanup_dir"
+
+    def _read_params(self):
+
+        target_path = None
+        keep_only = None
+        ditch = None
+
+        # target_path
+        try:
+            target_path = self.params["target_path"]
+        except KeyError:
+            return False, "target_path is a required parameter"
+
+        # keep_only
+        try:
+            keep_only = self.params["keep_only"]
+        except KeyError:
+            pass # optional
+
+        # ditch
+        try:
+            ditch_read = self.params["ditch"]
+            if isinstance(ditch_read, list):
+                ditch = ditch_read
+            else:
+                ditch = []
+                ditch.append(ditch_read)
+        except KeyError:
+            pass # optional
+
+        # pre-validate params
+        if not os.path.exists(target_path):
+            return False, "target path [%s] does not exist" % target_path
+
+        if not os.path.isdir(target_path):
+            return False, "target path [%s] is not a folder" % target_path
+
+        if (keep_only is None) and (ditch is None):
+            return False, "no operations selected"
+
+        if (keep_only is not None) and (ditch is not None):
+            return False, "keep_only cannot be selected with ditch"
+
+        return True, (target_path, keep_only, ditch)
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        v, r = self._read_params()
+        if not v:
+            return False, r
+        target_path, keep_only, ditch = r
+
+        report = []
+
+        v, r = fsquery.makecontentlist(target_path, False, False, True, True, True, True, True, None)
+        if not v:
+            return False, r
+        all_files = r
+
+        # keep_only
+        if keep_only is not None:
+
+            found_keeper = False
+            for f in all_files:
+                if path_utils.basename_filtered(f) == keep_only:
+                    found_keeper = True
+                    continue
+                if not path_utils.remove_path(f):
+                    return False, "could not remove [%s] (keep-only)" % f
+
+            if not found_keeper:
+                report.append("[%s] was not found on [%s] (keep-only)" % (keep_only, target_path))
+
+        else: # ditch
+
+            for fd in ditch:
+                found = False
+                for fa in all_files:
+                    if fd == path_utils.basename_filtered(fa):
+                        if not path_utils.remove_path(fa):
+                            return False, "could not remove [%s] (ditch)" % fa
+                        found = True
+                        break
+                if not found:
+                    report.append("[%s] was not found on [%s] (ditch)" % (fd, target_path))
+
+        final_msg = None
+        if len(report) > 0:
+            final_msg = report
+        return True, final_msg
---
+++ tests/cleanup_dir_plugin_test.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import unittest
+from unittest import mock
+from unittest.mock import patch
+from unittest.mock import call
+
+import mvtools_test_fixture
+import create_and_write_file
+import path_utils
+
+import cleanup_dir_plugin
+
+class CleanupDirPluginTest(unittest.TestCase):
+
+    def setUp(self):
+        v, r = self.delegate_setUp()
+        if not v:
+            self.tearDown()
+            self.fail(r)
+
+    def delegate_setUp(self):
+
+        v, r = mvtools_test_fixture.makeAndGetTestFolder("cleanup_dir_plugin_test")
+        if not v:
+            return v, r
+        self.test_base_dir = r[0] # base test folder. shared amongst other test cases
+        self.test_dir = r[1] # test folder, specific for each test case (i.e. one level above self.test_base_dir)
+
+        # test paths
+        self.nonexistent = path_utils.concat_path(self.test_dir, "nonexistent")
+        self.existent_dir = path_utils.concat_path(self.test_dir, "existent_dir")
+        self.existent_file = path_utils.concat_path(self.test_dir, "existent_file.txt")
+
+        os.mkdir(self.existent_dir)
+        create_and_write_file.create_file_contents(self.existent_file, "contents")
+
+        # the test task
+        self.cleanup_dir_task = cleanup_dir_plugin.CustomTask()
+
+        return True, ""
+
+    def tearDown(self):
+        shutil.rmtree(self.test_base_dir)
+
+    def testCleanupDirPluginReadParams1(self):
+
+        local_params = {}
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertFalse(v)
+        self.assertEqual(r, "target_path is a required parameter")
+
+    def testCleanupDirPluginReadParams2(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual(r, (self.existent_dir, "dummy_value1", None))
+
+    def testCleanupDirPluginReadParams3(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["ditch"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual(r, (self.existent_dir, None, ["dummy_value1"]))
+
+    def testCleanupDirPluginReadParams4(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["ditch"] = ["dummy_value1", "dummy_value2"]
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertTrue(v)
+        self.assertEqual(r, (self.existent_dir, None, ["dummy_value1", "dummy_value2"]))
+
+    def testCleanupDirPluginReadParams5(self):
+
+        local_params = {}
+        local_params["target_path"] = self.nonexistent
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertFalse(v)
+        self.assertEqual(r, "target path [%s] does not exist" % self.nonexistent)
+
+    def testCleanupDirPluginReadParams6(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_file
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertFalse(v)
+        self.assertEqual(r, "target path [%s] is not a folder" % self.existent_file)
+
+    def testCleanupDirPluginReadParams7(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertFalse(v)
+        self.assertEqual(r, "no operations selected")
+
+    def testCleanupDirPluginReadParams8(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["keep_only"] = "dummy_value1"
+        local_params["ditch"] = "dummy_value2"
+        self.cleanup_dir_task.params = local_params
+
+        v, r = self.cleanup_dir_task._read_params()
+        self.assertFalse(v)
+        self.assertEqual(r, "keep_only cannot be selected with ditch")
+
+    def testCleanupDirPluginRunTask1(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(False, "dummy_error_msg")) as dummy:
+            v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+            self.assertFalse(v)
+            self.assertEqual(r, "dummy_error_msg")
+            dummy.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+
+    def testCleanupDirPluginRunTask2(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value1"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(True)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertEqual(r, None)
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_not_called()
+
+    def testCleanupDirPluginRunTask3(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value2"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(True)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertEqual(r, ["[dummy_value1] was not found on [%s] (keep-only)" % self.existent_dir])
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_called_with("/base/dummy_value2")
+
+    def testCleanupDirPluginRunTask4(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value1", "/base/dummy_value2", "/base/dummy_value3"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(True)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertEqual(r, None)
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_has_calls([call("/base/dummy_value2"), call("/base/dummy_value3")])
+
+    def testCleanupDirPluginRunTask5(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["keep_only"] = "dummy_value1"
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value2"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(False)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertFalse(v)
+                self.assertEqual(r, "could not remove [/base/dummy_value2] (keep-only)")
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_called_with("/base/dummy_value2")
+
+    def testCleanupDirPluginRunTask6(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["ditch"] = ["dummy_value1"]
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value1"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(True)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertEqual(r, None)
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_called_with("/base/dummy_value1")
+
+    def testCleanupDirPluginRunTask7(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["ditch"] = ["dummy_value1"]
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value2"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(True)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertEqual(r, ["[dummy_value1] was not found on [%s] (ditch)" % self.existent_dir])
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_not_called()
+
+    def testCleanupDirPluginRunTask8(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["ditch"] = ["dummy_value1"]
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value1", "/base/dummy_value2"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(True)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertEqual(r, None)
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_called_with("/base/dummy_value1")
+
+    def testCleanupDirPluginRunTask9(self):
+
+        local_params = {}
+        local_params["target_path"] = self.existent_dir
+        local_params["ditch"] = ["dummy_value1"]
+        self.cleanup_dir_task.params = local_params
+
+        with mock.patch("fsquery.makecontentlist", return_value=(True, ["/base/dummy_value1"])) as dummy1:
+            with mock.patch("path_utils.remove_path", return_value=(False)) as dummy2:
+                v, r = self.cleanup_dir_task.run_task(print, "exe_name")
+                self.assertFalse(v)
+                self.assertEqual(r, "could not remove [/base/dummy_value1] (ditch)")
+                dummy1.assert_called_with(self.existent_dir, False, False, True, True, True, True, True, None)
+                dummy2.assert_called_with("/base/dummy_value1")
+
+if __name__ == "__main__":
+    unittest.main()
