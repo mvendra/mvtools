c638bd0852339aeefb204cb8aaac3faa6cb189f3
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-08-06 08:11:22 +0200

    mklink_plugin_test: improved coverage

--- launchers/launch_jobs_plugins/tasks/mklink_plugin.py
+++ launchers/launch_jobs_plugins/tasks/mklink_plugin.py
@@ -9,7 +9,10 @@ class CustomTask(launch_jobs.BaseTask):
     def get_desc(self):
         return "mklink"
 
-    def run_task(self, feedback_object, execution_name=None):
+    def _read_params(self):
+
+        source_path = None
+        target_path = None
 
         # source_path
         try:
@@ -26,18 +29,28 @@ class CustomTask(launch_jobs.BaseTask):
         # pre-checks
         if not os.path.exists(source_path):
             return False, "mklink failed - source path [%s] does not exist" % source_path
+
         if os.path.exists(target_path):
             return False, "mklink failed - target path [%s] already exists" % target_path
+
+        return True, (source_path, target_path)
+
+    def run_task(self, feedback_object, execution_name=None):
+
+        v, r = self._read_params()
+        if not v:
+            return False, r
+        source_path, target_path = r
 
         # actual execution
         try:
             os.symlink(source_path, target_path)
-        except FileNotFoundError as ex:
+        except:
             return False, "mklink failed - unable to create symlink from [%s] to [%s]" % (source_path, target_path)
 
         # post-verification
         if not os.path.exists(target_path):
-            return False, "mklink failed - unable to create symlink from [%s] to [%s]" % (source_path, target_path)
+            return False, "mklink failed - symlink from [%s] to [%s] was not created" % (source_path, target_path)
 
         # normal return
         return True, None
--- tests/mklink_plugin_test.py
+++ tests/mklink_plugin_test.py
@@ -4,11 +4,15 @@ import sys
 import os
 import shutil
 import unittest
+from unittest import mock
+from unittest.mock import patch
+from unittest.mock import call
 
 import mvtools_test_fixture
 
 import create_and_write_file
 import path_utils
+import mvtools_exception
 
 import mklink_plugin
 
@@ -33,9 +37,16 @@ class MklinkPluginTest(unittest.TestCase):
         if not os.path.exists(self.test_source_file):
             return False, "Unable to create test file [%s]" % self.test_source_file
 
-        self.test_nonexistent_file = path_utils.concat_path(self.test_dir, "test_nonexistent_file")
-        if os.path.exists(self.test_nonexistent_file):
-            return False, "File [%s] already exists" % self.test_nonexistent_file
+        self.test_nonexistent_file1 = path_utils.concat_path(self.test_dir, "test_nonexistent_file1")
+        if os.path.exists(self.test_nonexistent_file1):
+            return False, "File [%s] already exists" % self.test_nonexistent_file1
+
+        self.test_nonexistent_file2 = path_utils.concat_path(self.test_dir, "test_nonexistent_file2")
+        if os.path.exists(self.test_nonexistent_file2):
+            return False, "File [%s] already exists" % self.test_nonexistent_file2
+
+        self.helper_os_path_exists_counter = 0
+        self.helper_os_path_exists_fail_zero = False
 
         # the test task
         self.mklink_task = mklink_plugin.CustomTask()
@@ -45,67 +56,118 @@ class MklinkPluginTest(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.test_base_dir)
 
-    def testMklinkPluginFail1(self):
+    def _helper_os_path_exists(self, target_path):
+
+        if self.helper_os_path_exists_counter > 0:
+            self.helper_os_path_exists_counter -= 1
+        if self.helper_os_path_exists_fail_zero and self.helper_os_path_exists_counter == 0:
+            return False
+        return ((self.helper_os_path_exists_counter % 2) == 0)
+
+    def _helper_throw_except(self):
+
+        raise mvtools_exception.mvtools_exception("dummy exception")
+
+    def testMklinkPluginReadParams1(self):
 
         local_params = {}
+        local_params["target_path"] = self.test_nonexistent_file1
         self.mklink_task.params = local_params
 
-        v, r = self.mklink_task.run_task(print, "exe_name")
+        v, r = self.mklink_task._read_params()
         self.assertFalse(v)
 
-    def testMklinkPluginFail2(self):
+    def testMklinkPluginReadParams2(self):
+
+        local_params = {}
+        local_params["target_path"] = self.test_nonexistent_file1
+        self.mklink_task.params = local_params
+
+        v, r = self.mklink_task._read_params()
+        self.assertFalse(v)
+
+    def testMklinkPluginReadParams3(self):
+
+        local_params = {}
+        local_params["source_path"] = self.test_nonexistent_file1
+        local_params["target_path"] = self.test_nonexistent_file2
+        self.mklink_task.params = local_params
+
+        v, r = self.mklink_task._read_params()
+        self.assertFalse(v)
+
+    def testMklinkPluginReadParams4(self):
+
+        create_and_write_file.create_file_contents(self.test_nonexistent_file1, "test-contents")
+        self.assertTrue(os.path.exists(self.test_nonexistent_file1))
 
         local_params = {}
         local_params["source_path"] = self.test_source_file
+        local_params["target_path"] = self.test_nonexistent_file1
         self.mklink_task.params = local_params
 
-        v, r = self.mklink_task.run_task(print, "exe_name")
+        v, r = self.mklink_task._read_params()
         self.assertFalse(v)
 
-    def testMklinkPluginFail3(self):
-
-        local_params = {}
-        local_params["source_path"] = self.test_nonexistent_file
-        local_params["target_path"] = path_utils.concat_path(self.test_dir, "test_target_file")
-        self.mklink_task.params = local_params
-
-        v, r = self.mklink_task.run_task(print, "exe_name")
-        self.assertFalse(v)
-
-    def testMklinkPluginFail4(self):
-
-        local_target_file = path_utils.concat_path(self.test_dir, "test_target_file")
+    def testMklinkPluginReadParams5(self):
 
         local_params = {}
         local_params["source_path"] = self.test_source_file
-        local_params["target_path"] = local_target_file
+        local_params["target_path"] = self.test_nonexistent_file1
         self.mklink_task.params = local_params
 
-        create_and_write_file.create_file_contents(local_target_file, "test-contents")
-        self.assertTrue(os.path.exists(local_target_file))
-
-        v, r = self.mklink_task.run_task(print, "exe_name")
-        self.assertFalse(v)
-
-    def testMklinkPluginFail5(self):
-
-        local_params = {}
-        local_params["source_path"] = self.test_source_file
-        local_params["target_path"] = path_utils.concat_path(self.test_dir, "test_target_file", "second_level")
-        self.mklink_task.params = local_params
-
-        v, r = self.mklink_task.run_task(print, "exe_name")
-        self.assertFalse(v)
-
-    def testMklinkPluginVanilla(self):
-
-        local_params = {}
-        local_params["source_path"] = self.test_source_file
-        local_params["target_path"] = path_utils.concat_path(self.test_dir, "test_target_file")
-        self.mklink_task.params = local_params
-
-        v, r = self.mklink_task.run_task(print, "exe_name")
+        v, r = self.mklink_task._read_params()
         self.assertTrue(v)
+        self.assertEqual(r, (self.test_source_file, self.test_nonexistent_file1))
+
+    def testMklinkPluginRunTask1(self):
+
+        local_params = {}
+        local_params["source_path"] = self.test_source_file
+        local_params["target_path"] = self.test_nonexistent_file1
+        self.mklink_task.params = local_params
+
+        with mock.patch("os.symlink", return_value=(False)) as dummy:
+            dummy.side_effect = self._helper_throw_except
+            v, r = self.mklink_task.run_task(print, "exe_name")
+            self.assertFalse(v)
+            self.assertEqual(r, "mklink failed - unable to create symlink from [%s] to [%s]" % (self.test_source_file, self.test_nonexistent_file1))
+            dummy.assert_called_with(self.test_source_file, self.test_nonexistent_file1)
+
+    def testMklinkPluginRunTask2(self):
+
+        local_params = {}
+        local_params["source_path"] = self.test_source_file
+        local_params["target_path"] = self.test_nonexistent_file1
+        self.mklink_task.params = local_params
+
+        self.helper_os_path_exists_counter = 3
+        self.helper_os_path_exists_fail_zero = True
+        with mock.patch("os.symlink", return_value=(True)) as dummy1:
+            with mock.patch("os.path.exists", return_value=(True)) as dummy2:
+                dummy2.side_effect = self._helper_os_path_exists
+                v, r = self.mklink_task.run_task(print, "exe_name")
+                self.assertFalse(v)
+                self.assertEqual(r, "mklink failed - symlink from [%s] to [%s] was not created" % (self.test_source_file, self.test_nonexistent_file1))
+                dummy1.assert_called_with(self.test_source_file, self.test_nonexistent_file1)
+                dummy2.assert_has_calls([call(self.test_source_file), call(self.test_nonexistent_file1), call(self.test_nonexistent_file1)])
+
+    def testMklinkPluginRunTask3(self):
+
+        local_params = {}
+        local_params["source_path"] = self.test_source_file
+        local_params["target_path"] = self.test_nonexistent_file1
+        self.mklink_task.params = local_params
+
+        self.helper_os_path_exists_counter = 3
+        with mock.patch("os.symlink", return_value=(True)) as dummy1:
+            with mock.patch("os.path.exists", return_value=(True)) as dummy2:
+                dummy2.side_effect = self._helper_os_path_exists
+                v, r = self.mklink_task.run_task(print, "exe_name")
+                self.assertTrue(v)
+                self.assertEqual(r, None)
+                dummy1.assert_called_with(self.test_source_file, self.test_nonexistent_file1)
+                dummy2.assert_has_calls([call(self.test_source_file), call(self.test_nonexistent_file1), call(self.test_nonexistent_file1)])
 
 if __name__ == "__main__":
     unittest.main()
