9c84ae15dad20256b52e19decec8d3b32d4b53a5
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-08-08 17:29:53 +0200

    deploy_lib: improved and extended

--- deploy_lib.sh
+++ deploy_lib.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+quit_any_err(){
+    if [ $1 != 0 ]; then
+        exit 1
+    fi
+}
+
+quit_ok_if_defined(){
+    if [ ! -z $1 ]; then
+        exit 0
+    fi
+}
+
 deploy_lib(){
     mkdir_if_needed_and_possible.sh $4
     rm -rf $4/$3
     export $1=$4
     run_recipe.sh $2
-    if [ $? != 0 ]; then
-        exit 1
-    fi
+    quit_any_err $?
 }
