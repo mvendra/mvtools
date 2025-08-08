5d97424ac104ae0af82cf1649e09b8873e4b1e77
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-08-08 20:47:03 +0200

    deploy_lib: function qualifier

--- deploy_lib.sh
+++ deploy_lib.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-quit_fail_any_err(){
+function quit_fail_any_err(){
     if [ $1 != 0 ]; then
         exit 1
     fi
 }
 
-quit_ok_if_defined(){
+function quit_ok_if_defined(){
     if [ ! -z $1 ]; then
         exit 0
     fi
 }
 
-quit_combo(){
+function quit_combo(){
     quit_fail_any_err $1
     quit_ok_if_defined $2
 }
 
-deploy_lib(){
+function deploy_lib(){
     mkdir_if_needed_and_possible.sh $4
     rm -rf $4/$3
     export $1=$4
