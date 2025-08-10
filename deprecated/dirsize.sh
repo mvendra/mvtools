5f4031a1bff974cd8c439615ac67418d54957542
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-08-10 08:13:51 +0200

    project-wide: cleanups

--- deprecated/dirsize.sh
+++ deprecated/dirsize.sh
@@ -11,15 +11,15 @@ TARGET_DIR=${@}
 TARGET_DIR=`resolve_and_escape_path.py $TARGET_DIR`
 
 # resolve symlinks
-cd $TARGET_DIR
+cd "$TARGET_DIR"
 TARGET_DIR=`pwd -P`
 
-if [ -z $TARGET_DIR ]; then
+if [ -z "$TARGET_DIR" ]; then
     puaq
 fi
 
-if [ ! -d $TARGET_DIR ]; then
+if [ ! -d "$TARGET_DIR" ]; then
     echo "$TARGET_DIR does not exist or is not a directory"
 fi
 
-du -h $TARGET_DIR | tail -n 1
+du -h "$TARGET_DIR" | tail -n 1
