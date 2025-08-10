f2b81f1477ca30802a3469188364c45e36328610
Mateus Vendramini <mateusvendramini21@gmail.com>
2025-08-10 08:56:49 +0200

    project-wide: cleanups

--- mvtools_main.sh
+++ mvtools_main.sh
@@ -4,49 +4,49 @@ function add2path(){
 
     if [ -z "$PATH" ]; then
         # first use. dont add the comma at the beginning
-        export PATH=${1}
+        export PATH="${1}"
         return
     fi
 
-    if [[ $PATH =~ $1 ]]; then
+    if [[ "$PATH" =~ "$1" ]]; then
         echo "Warning: value [$1] is already in the envvar PATH - skipped adding"
         return
     fi
 
-    export PATH=${PATH}:${1}
+    export PATH="${PATH}:${1}"
 
 }
 
 function add2pythonpath(){
 
-    if [ -z $PYTHONPATH ]; then
+    if [ -z "$PYTHONPATH" ]; then
         # first use. dont add the comma at the beginning
-        export PYTHONPATH=${1}
+        export PYTHONPATH="${1}"
     else
-        if [[ $PYTHONPATH =~ $1 ]]; then
+        if [[ "$PYTHONPATH" =~ "$1" ]]; then
             echo "Warning: value [$1] is already in the envvar PYTHONPATH - skipped adding"
             return
         fi
-        export PYTHONPATH=${PYTHONPATH}:${1}
+        export PYTHONPATH="${PYTHONPATH}:${1}"
     fi
 
 }
 
-if [ -z $MVTOOLS_LINKS_PATH ]; then
-    MVTOOLS_LINKS_PATH=$MVTOOLS/links
+if [ -z "$MVTOOLS_LINKS_PATH" ]; then
+    MVTOOLS_LINKS_PATH="$MVTOOLS/links"
 fi
 
 # this requires the MVTOOLS envvar to be defined in the environment
-if [[ ! -z $MVTOOLS && -d $MVTOOLS ]]; then
+if [[ ! -z "$MVTOOLS" && -d "$MVTOOLS" ]]; then
 
-    add2path $MVTOOLS_LINKS_PATH
-    add2pythonpath $MVTOOLS_LINKS_PATH
+    add2path "$MVTOOLS_LINKS_PATH"
+    add2pythonpath "$MVTOOLS_LINKS_PATH"
 
     # coreutils customisations
-    source $MVTOOLS/coreutils_custom.sh
+    source "$MVTOOLS/coreutils_custom.sh"
 
     # it is preferrable to source other scripts last, because any of their contents may depend on previously added paths
-    source $MVTOOLS/git/git_aliases.sh
+    source "$MVTOOLS/git/git_aliases.sh"
 
 else
     echo "WARNING: MVTOOLS envvar is defined incorrectly."
