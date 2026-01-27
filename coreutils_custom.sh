0a96d937452ac69ccd25c74a31f03bd59605ccd7
Mateus Vendramini <mateusvendramini21@gmail.com>
2026-01-27 19:04:53 +0100

    coreutils_custom: platform compatibility fix

--- coreutils_custom.sh
+++ coreutils_custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-platform=`get_platform.py`
-if [[ "$platform" == "linux" || "$platform" == "cygwin" || "$platform" == "msys" || "$platform" == "mingw" ]]; then
+unamestr=`uname` # a bit at odds with mvtools's get_platform.py but trying to invoke python from here has caused headaches in the past (msys2)
+if [[ ${unamestr:0:5} == "Linux" || ${unamestr:0:9} == "CYGWIN_NT" || ${unamestr:0:7} == "MSYS_NT" || ${unamestr:0:10} == "MINGW64_NT" ]]; then
     alias ls="ls --color --group-directories-first --sort=extension"
 fi
 
