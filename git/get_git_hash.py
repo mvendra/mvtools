14cc2f03acbb07922e9b137116340ed8d225ef7b
Mateus Vendramini <mateusvendramini21@gmail.com>
2026-06-22 19:54:04 +0200

    get_git_hash: patched to accept prev hash param number

--- git/get_git_hash.py
+++ git/get_git_hash.py
@@ -6,13 +6,30 @@ import os
 import path_utils
 import git_lib
 
-def get_git_hash(target_repo):
-    return git_lib.get_head_hash(target_repo)
+def get_git_hash(target_repo, num_prev_hash):
+
+    if num_prev_hash is None:
+
+        return git_lib.get_head_hash(target_repo)
+
+    else:
+
+        if num_prev_hash == "0":
+            return git_lib.get_head_hash(target_repo)
+
+        v, r = git_lib.get_previous_hash_list(target_repo, int(num_prev_hash) + 1)
+        if not v:
+            return False, r
+        return True, r[len(r) - 1]
 
 if __name__ == "__main__":
 
+    num_prev_hash = None
+    if len(sys.argv) > 1:
+        num_prev_hash = sys.argv[1]
+
     target_repo = os.getcwd()
-    v, r = get_git_hash(target_repo)
+    v, r = get_git_hash(target_repo, num_prev_hash)
     print(r)
     if not v:
         sys.exit(1)
